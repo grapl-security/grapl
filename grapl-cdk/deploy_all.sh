4bab332b96709df1803ffc82b840226f3994bed1
Colin <insanitybit@gmail.com>
2019-05-23 12:25:31 -0700

    Update deploy_all.sh

--- grapl-cdk/deploy_all.sh
+++ grapl-cdk/deploy_all.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 npm run build &&
-cdk deploy grapl-event-emitters-stack && \
-cdk deploy graplvpcs-stack && \
-cdk deploy graplhistorydb-stack && \
-cdk deploy mastergraphcluster-stack && \
-cdk deploy engagementgraphcluster-stack && \
-cdk deploy grapl-generic-subgraph-generator-stack && \
-cdk deploy grapl-sysmon-subgraph-generator-stack && \
-cdk deploy grapl-node-identity-mapper-stack && \
-cdk deploy grapl-node-identifier-stack && \
-cdk deploy grapl-graph-merger-stack && \
-cdk deploy grapl-analyzer-dispatcher-stack && \
-cdk deploy grapl-analyzer-executor-stack && \
-cdk deploy grapl-engagement-creator-stack && \
-cdk deploy grapl-engagement-creator-stack;
+cdk deploy --require-approval=never grapl-event-emitters-stack && \
+cdk deploy --require-approval=never graplvpcs-stack && \
+cdk deploy --require-approval=never graplhistorydb-stack && \
+cdk deploy --require-approval=never mastergraphcluster-stack && \
+cdk deploy --require-approval=never engagementgraphcluster-stack && \
+cdk deploy --require-approval=never grapl-generic-subgraph-generator-stack && \
+cdk deploy --require-approval=never grapl-sysmon-subgraph-generator-stack && \
+cdk deploy --require-approval=never grapl-node-identity-mapper-stack && \
+cdk deploy --require-approval=never grapl-node-identifier-stack && \
+cdk deploy --require-approval=never grapl-graph-merger-stack && \
+cdk deploy --require-approval=never grapl-analyzer-dispatcher-stack && \
+cdk deploy --require-approval=never grapl-analyzer-executor-stack && \
+cdk deploy --require-approval=never grapl-engagement-creator-stack && \
+cdk deploy --require-approval=never grapl-engagement-creator-stack;
 
 # cdk deploy engagement-creation-service-stack
 
