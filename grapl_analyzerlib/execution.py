57ccefa38fe3c81d01c7028ed00f785bc1d74304
Insanitybit <insanitybit@gmail.com>
2019-06-04 23:21:03 -0700

    Serialize execution hit

--- grapl_analyzerlib/execution.py
+++ grapl_analyzerlib/execution.py
@@ -3,8 +3,8 @@ from grapl_analyzerlib.entities import NodeView
 
 class ExecutionHit(object):
     def __init__(self, analyzer_name: str, node_view: NodeView, risk_score: int = 50):
-        self.node_view = node_view.serialize()
         self.analyzer_name = analyzer_name
+        self.node_view = node_view.serialize()
         self.risk_score = risk_score
 
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="grapl_analyzerlib",
-    version="0.1.14",
+    version="0.1.15",
     description="Library for Grapl Analyzers",
     url="https://github.com/insanitybit/grapl/",
     author="insanitybit",
