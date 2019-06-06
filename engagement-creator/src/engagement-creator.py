13d4fbb6918b8043c5e245fd5d5ee4c173c2c109
Insanitybit <insanitybit@gmail.com>
2019-06-05 22:46:57 -0700

    Adopt new schema. New analyzer library. New engagement system.

--- engagement-creator/src/engagement-creator.py
+++ engagement-creator/src/engagement-creator.py
@@ -1,13 +1,11 @@
+import os
 import hashlib
-import time
-
 import boto3
 import json
 
-from typing import Any, List, Dict
+from typing import Any, List, Dict, Union, TypeVar, Optional
 
-import os
-import pydgraph
+from grapl_analyzerlib.entities import ProcessView, FileView
 from pydgraph import DgraphClient, DgraphClientStub
 
 
@@ -62,6 +60,8 @@ class NodeCopier(object):
             if not node:
                 node = props
             else:
+                # TODO: Merge lists of properties together
+
                 node = {**props, **node[0]}
 
             res = txn.mutate(set_obj=node, commit_now=True)
@@ -78,15 +78,21 @@ class NodeCopier(object):
         query = """
             query q0($a: string)
             {
-              q0(func: uid($a))
+            
+              q as var(func: uid($a)) {
+                  pred as _predicate_
+              }
+            
+              q0(func: uid(q))
               {
-                expand(_all_)
+                  expand(val(pred))
               }
             }
             """
 
-        res = json.loads(self.mg_client.query(query, variables={'$a': node_uid}).json)
-
+        res = json.loads(self.mg_client.txn(read_only=True)
+                         .query(query, variables={'$a': node_uid}).json)
+        print(f'res {res}')
         # We assume the node exists in the master graph
         node = res['q0'][0]
 
@@ -108,43 +114,140 @@ class NodeCopier(object):
         print('edge mutation result is: {}'.format(res))
 
 
-def create_process_schema(eg_client: DgraphClient):
-    schema = \
-        'node_key: string @index(hash) .\n' +\
-        'engagement_key: string @index(hash) .\n' +\
-        'children: uid @reverse .\n' +\
-        'pid: int @index(int) .\n'
+def get_engagement_key(label: str, root: Dict[str, Any]) -> str:
+    hasher = hashlib.sha1(label.encode('utf8'))
 
-    op = pydgraph.Operation(schema=schema)
-    eg_client.alter(op)
-
-
-def get_engagement_key(label: str, uids: List[str]) -> str:
-    bucket = int(time.time()) - (int(time.time()) % 7200)
-    hasher = hashlib.sha1(label)
-    hasher.update(str(bucket).encode())
-    [hasher.update(str(uid).encode()) for uid in sorted(uids)]
-
+    hasher.update(str(root['node_key']).encode())
     return str(hasher.hexdigest())
 
 
-# We need to whitelist by taking the uids, sorting, and hashing with the alert name
-# For now, we 'throttle' by hour, but this should be customizable later
+def get_root(nodes: List[Dict[str, Any]]) -> Dict[str, Any]:
+    root = None
+    for node in nodes:
+        if node.get('root'):
+            root = node
+            break
+    assert root, 'There must be a root node'
+    return root
+
+
 def should_throttle(engagement_key: str, dgraph_client: DgraphClient) -> bool:
     query = """
             query q0($a: string)
             {
-              q0(func: eq(engagement_key, $a), first: 1)
+              q0(func: eq(engagement_key, $a), first: 1) @cascade
               {
                 uid,
+                ~scope {
+                    uid
+                }
               }
             }
             """
 
-    res = json.loads(dgraph_client.query(query, variables={'$a': engagement_key}).json)
+    res = json.loads(dgraph_client.txn(read_only=True).query(query, variables={'$a': engagement_key}).json)
     if res['q0']:
         return True
     return False
+
+
+def into_process_views(raw_engagement) -> List[Union[ProcessView, FileView]]:
+    scope = []
+    for scoped_node in raw_engagement.get('scope', []):
+        if scoped_node.get('process_id'):
+            node = ProcessView(node_key=scoped_node['node_key'])
+        elif scoped_node.get('file_path'):
+            node = FileView(node_key=scoped_node['node_key'])
+        else:
+            raise Exception('fInvalid scoped node type: {scoped_node}')
+        scope.append(node)
+
+    return scope
+
+
+EV = TypeVar('EV', bound='EngagementView')
+
+
+class EngagementView(object):
+    def __init__(
+            self,
+            client: DgraphClient,
+            lense: str,
+            uid: str,
+            scope: Optional[List[Union[ProcessView, FileView]]] = None
+    ):
+        self.client = client
+        self.lense = lense
+        self.uid = uid
+        self.scope = scope or []
+
+    @staticmethod
+    def get_or_create(client, lense: str) -> EV:
+        query = """
+        query q0($a: string)
+        {
+          p as var(func: eq(lense, $a)) {
+            pred as _predicate_
+          }
+        
+          q0(func: uid(p))
+          {
+            uid,
+            expand(val(pred))
+            scope {
+                node_key,
+                uid,
+                process_id,
+                file_path
+            }
+          }
+        }
+        """
+
+        txn = client.txn(read_only=False)
+
+        try:
+            res = json.loads(txn.query(query, variables={'$a': lense}).json)
+            node = res['q0']
+            print(f'node is {node}')
+            if node:
+                return EngagementView(client,
+                                      lense=node[0]['lense'],
+                                      uid=node[0]['uid'],
+                                      scope=into_process_views(node[0]),
+                                      )
+            else:
+                node = {'lense': lense, 'score': 0}
+            res = txn.mutate(set_obj=node, commit_now=True)
+            uids = res.uids
+            print(uids)
+            uid = uids['blank-0']
+            return EngagementView(client,
+                                  uid=uid,
+                                  lense=lense,
+                                  scope=[],
+                                  )
+        finally:
+            txn.discard()
+
+    def attach_scope(self, root_node: Union[ProcessView, FileView]) -> EV:
+        txn = self.client.txn(read_only=False)
+
+        try:
+            mutation = {
+                "uid": self.uid,
+                "scope": {
+                    "uid": root_node.get_uid()
+                }
+            }
+
+            print(f"mutation: {mutation}")
+
+            txn.mutate(set_obj=mutation, commit_now=True)
+        finally:
+            txn.discard()
+
+        return self
 
 
 def lambda_handler(events: Any, context: Any) -> None:
@@ -157,36 +260,61 @@ def lambda_handler(events: Any, context: Any) -> None:
     eg_client = DgraphClient(*eg_client_stubs)
     mg_client = DgraphClient(*mg_client_stubs)
 
-    create_process_schema(eg_client)
-
     uid_map = {}
     for event in events['Records']:
         print('Copying engagement')
         data = parse_s3_event(event)
         incident_graph = json.loads(data)
 
-        label = incident_graph['label'].encode('utf-8')
-        node_refs = incident_graph['node_refs']
+        analyzer_name = incident_graph['analyzer_name']
+        nodes = incident_graph['nodes']
         edges = incident_graph['edges']
 
-        engagement_key = get_engagement_key(label, [n['uid'] for n in node_refs])
+        print(f'nodes {nodes}')
+        print(f'edges {edges}')
+        # Key is root node + analyzer_name
+        root = get_root(nodes.values())
+        engagement_key = get_engagement_key(analyzer_name, root)
 
         if should_throttle(engagement_key, eg_client):
-            print('Throttling: {}'.format(engagement_key))
+            print('Throttling: {}'.format(nodes))
             continue
 
-        print('Creating engagement: {}'.format(engagement_key))
+        # Upsert all of the nodes
+        # If nodes have a list field, merge it
+        # In particular, merge the 'analyzer_names' list
+
         copier = NodeCopier(engagement_key, mg_client, eg_client)
 
-        print('node_refs: {}'.format(node_refs))
+        print('node_refs: {}'.format(nodes))
         print('edges: {}'.format(edges))
-        for node_ref in node_refs:
-            new_uid = copier.copy_node(node_ref['uid'])
-            uid_map[node_ref['uid']] = new_uid
+        for node in nodes.values():
+            node.pop('root', None)
+            node.pop('type', None)
+
+            new_uid = copier.copy_node(node['uid'])
+            uid_map[node['node_key']] = new_uid
             print('new_uid: {}'.format(new_uid))
-        for edge in edges:
-            copier.copy_edge(uid_map[edge[0]], edge[1], uid_map[edge[2]])
+
+        for edge_list in edges.values():
+            for edge in edge_list:
+                copier.copy_edge(uid_map[edge['from']], edge['edge_name'], uid_map[edge['to']])
         print('Copied engagement successfully')
+
+        if root['node_type'] == 'Process':
+            root_view = ProcessView(dgraph_client=eg_client, node_key=root['node_key'])
+        elif root['node_type'] == 'File':
+            root_view = FileView(dgraph_client=eg_client, node_key=root['node_key'])
+        else:
+            raise Exception(f"Invalid root node. Missing 'type': {root}.")
+
+        asset_id = root_view.get_asset_id()
+
+        engagement = EngagementView.get_or_create(eg_client, lense=asset_id)
+
+        engagement.attach_scope(root_view)
+
+        # TODO: Recalculate risk for engagement
 
     print('Engagement creation was successful')
 
