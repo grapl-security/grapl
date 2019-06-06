759ec6fb21fb8f3112b915d24e8e0a826c261e19
Insanitybit <insanitybit@gmail.com>
2019-06-05 17:43:31 -0700

    Update in test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="grapl_analyzerlib",
-    version="0.1.19",
+    version="0.1.20",
     description="Library for Grapl Analyzers",
     url="https://github.com/insanitybit/grapl/",
     author="insanitybit",
