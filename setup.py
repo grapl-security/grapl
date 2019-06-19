b510409e556207c209ce2010a3ce1d72d3ef00fe
Insanitybit <insanitybit@gmail.com>
2019-06-18 19:41:32 -0700

    Don't use regex for contains. Add get_edges to FileQuery

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="grapl_analyzerlib",
-    version="0.1.35",
+    version="0.1.36",
     description="Library for Grapl Analyzers",
     url="https://github.com/insanitybit/grapl/",
     author="insanitybit",
