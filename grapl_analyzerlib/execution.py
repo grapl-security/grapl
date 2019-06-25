d5b37c82f8f24e2cc7fc60da56df3b38153b12f3
Insanitybit <insanitybit@gmail.com>
2019-06-24 19:06:29 -0700

    Internal changes

--- grapl_analyzerlib/execution.py
+++ grapl_analyzerlib/execution.py
@@ -5,6 +5,8 @@ from grapl_analyzerlib.entities import NodeView
 
 class ExecutionHit(object):
     def __init__(self, analyzer_name: str, node_view: NodeView, risk_score: int = 50):
+        self.root_node_key = node_view.node.node_key
+
         node_dict = node_view.to_adjacency_list()
         self.analyzer_name = analyzer_name
         self.nodes = json.dumps(node_dict['nodes'])
