7b3b6ee420f0e54eb35ad490fd1cc3bcf75be02b
Insanitybit <insanitybit@gmail.com>
2019-07-01 20:42:37 -0700

    Debuglogging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="grapl_analyzerlib",
-    version="0.1.51",
+    version="0.1.52",
     description="Library for Grapl Analyzers",
     url="https://github.com/insanitybit/grapl/",
     author="insanitybit",
