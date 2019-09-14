2a4677bb2b4ff2778d402ac2698a90a0d78d7e4c
Insanitybit <insanitybit@gmail.com>
2019-09-14 12:17:49 -0700

    Fix bug in get_descendents. Remove cached edges

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="grapl_analyzerlib",
-    version="0.1.125",
+    version="0.1.126",
     description="Library for Grapl Analyzers",
     url="https://github.com/insanitybit/grapl/",
     author="insanitybit",
