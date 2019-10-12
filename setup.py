d59568a179c651fbdb78df6e95c18559921fb069
Insanitybit <insanitybit@gmail.com>
2019-10-11 19:53:38 -0700

    Fix index errors. Add reverse edges for FileView

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="grapl_analyzerlib",
-    version="0.1.136",
+    version="0.1.137",
     description="Library for Grapl Analyzers",
     url="https://github.com/insanitybit/grapl/",
     author="insanitybit",
