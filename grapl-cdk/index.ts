6b49e4b7710a3328dad34f99cfd349a86548f4e9
Insanitybit <insanitybit@gmail.com>
2019-10-27 14:47:14 -0700

    Remove edge_ux files

--- grapl-cdk/index.ts
+++ grapl-cdk/index.ts
@@ -1367,17 +1367,3 @@ class Grapl extends cdk.App {
 }
 
 new Grapl().synth();
-
-// cdk deploy graplvpcs-stack && \
-// cdk deploy graplhistorydb-stack && \
-// cdk deploy grapl-event-emitters-stack && \
-// cdk deploy graplmastergraph-stack && \
-// cdk deploy graplengagementgraph-stack && \
-// cdk deploy grapl-generic-subgraph-generator-stack && \
-// cdk deploy grapl-sysmon-subgraph-generator-stack && \
-// cdk deploy grapl-node-identity-mapper-stack && \
-// cdk deploy grapl-node-identifier-stack && \
-// cdk deploy grapl-graph-merger-stack && \
-// cdk deploy grapl-analyzer-dispatcher-stack && \
-// cdk deploy grapl-analyzer-executor-stack && \
-// cdk deploy grapl-engagement-creator-stack
