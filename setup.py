be401150357aadf2de11afd1509ef95eb9764d84
Insanitybit <insanitybit@gmail.com>
2019-11-04 11:09:04 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="grapl_analyzerlib",
-    version="0.1.170",
+    version="0.1.171",
     description="Library for Grapl Analyzers",
     url="https://github.com/insanitybit/grapl/",
     author="insanitybit",
