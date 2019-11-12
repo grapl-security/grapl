b2c90995b09d81b83581169bb4df0365cd877e62
Insanitybit <insanitybit@gmail.com>
2019-11-12 14:01:38 -0800

    Fix imports

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="grapl_analyzerlib",
-    version="0.1.215",
+    version="0.1.216",
     description="Library for Grapl Analyzers",
     url="https://github.com/insanitybit/grapl/",
     author="insanitybit",
