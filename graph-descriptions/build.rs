c8de3b5a7c362238b5a994809827d536079f423b
Insanitybit <insanitybit@gmail.com>
2019-12-22 12:06:39 -0800

    More default fields for networking

--- graph-descriptions/build.rs
+++ graph-descriptions/build.rs
@@ -104,17 +104,17 @@ fn main() {
                            "#[builder(default = \"::uuid::Uuid::new_v4().to_string()\")]");
 
     config.field_attribute(".graph_description.Asset.asset_id", "#[builder(default)]");
-    config.field_attribute(".graph_description.Asset.host_name", "#[builder(default)]");
-    config.field_attribute(".graph_description.Asset.host_domain", "#[builder(default)]");
-    config.field_attribute(".graph_description.Asset.host_fqdn", "#[builder(default)]");
+    config.field_attribute(".graph_description.Asset.hostname", "#[builder(default)]");
     config.field_attribute(".graph_description.Asset.mac_address", "#[builder(default)]");
-    config.field_attribute(".graph_description.Asset.host_ip", "#[builder(default)]");
-    config.field_attribute(".graph_description.Asset.operating_system", "#[builder(default)]");
+    config.field_attribute(".graph_description.Asset.first_seen_timestamp", "#[builder(default)]");
+    config.field_attribute(".graph_description.Asset.last_seen_timestamp", "#[builder(default)]");
 
 
     config.field_attribute(".graph_description.IpAddress.node_key", "#[builder(field(private))]");
     config.field_attribute(".graph_description.IpAddress.node_key",
                            "#[builder(default = \"::uuid::Uuid::new_v4().to_string()\")]");
+    config.field_attribute(".graph_description.IpAddress.first_seen_timestamp", "#[builder(default)]");
+    config.field_attribute(".graph_description.IpAddress.last_seen_timestamp", "#[builder(default)]");
 
 
     config.field_attribute(".graph_description.IpPort.node_key", "#[builder(field(private))]");
