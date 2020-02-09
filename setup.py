8cf811179b46882c3632a54d32178fcbaeb2ca90
Insanitybit <insanitybit@gmail.com>
2020-02-09 12:50:48 -0800

    Catchup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="grapl_analyzerlib",
-    version="0.2.16",
+    version="0.2.17",
     description="Library for Grapl Analyzers",
     url="https://github.com/insanitybit/grapl_analyzerlib/",
     author="insanitybit",
