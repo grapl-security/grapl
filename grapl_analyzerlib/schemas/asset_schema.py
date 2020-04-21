9508efa985205fb245c6597893cf7c06da143464
inickles <inickles@users.noreply.github.com>
2020-04-21 10:54:53 -0700

    Adding forward edge from AssetNode to IpAddressNode to connect the two. (#1)
    "An asset has these IP addresses".
    
    This edge is expected to exist by the cyrrent version of sysmon subgraph
    generator:
    https://github.com/insanitybit/grapl/blob/master/sysmon-subgraph-generator/src/main.rs#L281

--- grapl_analyzerlib/schemas/asset_schema.py
+++ grapl_analyzerlib/schemas/asset_schema.py
@@ -7,6 +7,12 @@ class AssetSchema(NodeSchema):
         (
             self.with_str_prop("hostname")
         )
+        .with_forward_edge(
+            "asset_ip",
+            # An asset can have multiple IP address
+            OneToMany(IpAddressNodeSchema),
+            "ip_assigned_to",
+        )
 
     @staticmethod
     def self_type() -> str:
