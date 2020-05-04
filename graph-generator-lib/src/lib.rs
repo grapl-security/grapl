e15b239a613534237c9a724fdfa3da8f155eb917
colin <colin@graplsecurity.com>
2020-05-03 19:02:13 -0700

    Start implementing a local wrapper

--- graph-generator-lib/src/lib.rs
+++ graph-generator-lib/src/lib.rs
@@ -1,116 +1,259 @@
-extern crate base16;
-extern crate failure;
-extern crate futures;
-extern crate graph_descriptions;
-#[macro_use] extern crate log;
-extern crate prost;
-extern crate rusoto_core;
-extern crate rusoto_s3;
-extern crate rusoto_sqs;
-extern crate serde_json;
-extern crate sha2;
-extern crate zstd;
-
 use std::io::Cursor;
 use std::str::FromStr;
-use std::time::{SystemTime, Duration};
+use std::sync::Arc;
+use std::time::{Duration, SystemTime};
 use std::time::UNIX_EPOCH;
 
+use aws_lambda_events::event::s3::{S3Bucket, S3Entity, S3Event, S3EventRecord, S3Object, S3RequestParameters, S3UserIdentity};
+use chrono::Utc;
 use failure::Error;
 use futures::future::Future;
 use graph_descriptions::graph_description::*;
+use lambda_runtime::Context;
+use log::*;
 use prost::Message;
-use rusoto_core::Region;
+use rusoto_core::{HttpClient, Region};
 use rusoto_s3::{PutObjectRequest, S3, S3Client};
+use rusoto_sqs::{SendMessageRequest, Sqs, SqsClient};
 use serde_json::Value;
 use sha2::{Digest, Sha256};
+use sqs_lambda::cache::NopCache;
+use sqs_lambda::completion_event_serializer::CompletionEventSerializer;
+use sqs_lambda::event_decoder::PayloadDecoder;
+use sqs_lambda::event_handler::EventHandler;
+use sqs_lambda::local_sqs_service::local_sqs_service;
+use tokio::runtime::Runtime;
 
-pub fn send_logs_to_generators(
-    sourcetype: impl AsRef<str>,
-    logs: Vec<u8>,
-) -> Result<(), Error> {
+#[derive(Debug, Clone, Default)]
+pub struct ZstdDecoder;
 
-
-    let key = {
-        let mut hasher = Sha256::default();
-        hasher.input(&logs);
-
-        let key = hasher.result();
-        let key = base16::encode_lower(&key);
-        let epoch = SystemTime::now()
-            .duration_since(UNIX_EPOCH).unwrap().as_secs();
-
-        let day = epoch - (epoch % (24 * 60 * 60));
-
-        format!("{}/{}/{}", sourcetype.as_ref(), day, key)
-    };
-
-    info!("Sending {} logs to {}", logs.len(), key);
-    let region = {
-        let region_str = std::env::var("AWS_REGION").expect("AWS_REGION");
-        Region::from_str(&region_str).expect("Invalid Region")
-    };
-    let s3_client = S3Client::new(region);
-
-    let bucket_prefix = std::env::var("BUCKET_PREFIX").expect("BUCKET_PREFIX");
-
-    s3_client.put_object(PutObjectRequest {
-        bucket: bucket_prefix + "-grapl-raw-log-bucket",
-        key,
-        body: Some(logs.into()),
-        ..Default::default()
-    }).sync()?;
-    info!("Uploaded raw-logs");
-    Ok(())
-}
-
-pub fn upload_subgraphs<S>(s3_client: &S, subgraphs: GeneratedSubgraphs) -> Result<(), Error>
-    where S: S3
+impl PayloadDecoder<Vec<u8>> for ZstdDecoder
 {
-    info!("upload_subgraphs");
-    let mut proto = Vec::with_capacity(5000);
-    subgraphs.encode(&mut proto)?;
+    fn decode(&mut self, body: Vec<u8>) -> Result<Vec<u8>, Box<dyn std::error::Error>>
+    {
+        let mut decompressed = Vec::new();
 
-    let mut hasher = Sha256::default();
-    hasher.input(&proto);
+        let mut body = Cursor::new(&body);
 
-    let key = base16::encode_lower(hasher.result().as_ref());
+        zstd::stream::copy_decode(&mut body, &mut decompressed)?;
 
-    let bucket_prefix = std::env::var("BUCKET_PREFIX").expect("BUCKET_PREFIX");
+        Ok(decompressed)
+    }
+}
 
-    let bucket = bucket_prefix + "-unid-subgraphs-generated-bucket";
-    let epoch = SystemTime::now()
-        .duration_since(UNIX_EPOCH).unwrap().as_secs();
 
-    // Bucket by day
-    let day = epoch - (epoch % (24 * 60 * 60));
+#[derive(Clone, Debug, Default)]
+pub struct SubgraphSerializer {
+    proto: Vec<u8>,
+}
 
-    // Key is day/time-hash
-    let key = format!("{}/{}-{}",
-                      day,
-                      epoch,
-                      base16::encode_lower(&key)
-    );
+impl CompletionEventSerializer for SubgraphSerializer {
+    type CompletedEvent = Graph;
+    type Output = Vec<u8>;
+    type Error = sqs_lambda::error::Error<Arc<failure::Error>>;
 
-    let mut compressed = Vec::with_capacity(proto.len());
-    let mut proto = Cursor::new(&proto);
-    zstd::stream::copy_encode(&mut proto, &mut compressed, 4)
-        .expect("compress zstd capnp");
+    fn serialize_completed_events(
+        &mut self,
+        completed_events: &[Self::CompletedEvent],
+    ) -> Result<Vec<Self::Output>, Self::Error> {
+        let mut subgraph = Graph::new(
+            0
+        );
 
-    info!("uploading unidentifed_subgraphs to {}", key);
+        let mut pre_nodes = 0;
+        let mut pre_edges = 0;
 
-    s3_client.put_object(PutObjectRequest {
-        bucket,
-        key,
-        body: Some(compressed.into()),
-        ..Default::default()
-    }).with_timeout(Duration::from_secs(5)).sync()?;
+        for sg in completed_events.iter() {
+            pre_nodes += sg.nodes.len();
+            pre_edges += sg.edges.len();
+            subgraph.merge(sg);
+        }
 
-    info!("uploaded unidentified subgraphs");
+        if subgraph.is_empty() {
+            warn!(
+                concat!(
+                "Output subgraph is empty. Serializing to empty vector.",
+                "pre_nodes: {} pre_edges: {}"
+                ),
+                pre_nodes,
+                pre_edges,
+            );
+            return Ok(vec![]);
+        }
 
+        info!(
+            "Serializing {} nodes {} edges. Down from {} nodes {} edges.",
+            subgraph.nodes.len(),
+            subgraph.edges.len(),
+            pre_nodes,
+            pre_edges,
+        );
+
+        let subgraphs = GeneratedSubgraphs { subgraphs: vec![subgraph] };
+
+        self.proto.clear();
+
+        prost::Message::encode(&subgraphs, &mut self.proto)
+            .map_err(Arc::new)
+            .map_err(|e| {
+                sqs_lambda::error::Error::EncodeError(e.to_string())
+            })?;
+
+        let mut compressed = Vec::with_capacity(self.proto.len());
+        let mut proto = Cursor::new(&self.proto);
+        zstd::stream::copy_encode(&mut proto, &mut compressed, 4)
+            .map_err(Arc::new)
+            .map_err(|e| {
+                sqs_lambda::error::Error::EncodeError(e.to_string())
+            })?;
+
+        Ok(vec![compressed])
+    }
+}
+
+
+fn init_sqs_client() -> SqsClient
+{
+    info!("Connecting to local us-east-1 http://sqs.us-east-1.amazonaws.com:9324");
+
+    SqsClient::new_with(
+        HttpClient::new().expect("failed to create request dispatcher"),
+        rusoto_credential::StaticProvider::new_minimal(
+            "dummy_sqs".to_owned(),
+            "dummy_sqs".to_owned(),
+        ),
+        Region::Custom {
+            name: "us-east-1".to_string(),
+            endpoint: "http://sqs.us-east-1.amazonaws.com:9324".to_string(),
+        },
+    )
+}
+
+fn init_s3_client() -> S3Client
+{
+    info!("Connecting to local http://s3:9000");
+    S3Client::new_with(
+        HttpClient::new().expect("failed to create request dispatcher"),
+        rusoto_credential::StaticProvider::new_minimal(
+            "minioadmin".to_owned(),
+            "minioadmin".to_owned(),
+        ),
+        Region::Custom {
+            name: "locals3".to_string(),
+            endpoint: "http://s3:9000".to_string(),
+        },
+    )
+}
+
+pub async fn local_service<
+    EH: EventHandler<
+        InputEvent=Vec<u8>,
+        OutputEvent=Graph,
+        Error=sqs_lambda::error::Error<Arc<failure::Error>>
+    > + Send + Sync + Clone + 'static
+>(
+    bucket_name: String,
+    queue_url: String,
+    generator: EH,
+) -> Result<(), Box<dyn std::error::Error>> {
+    local_sqs_service(
+        queue_url,
+        bucket_name,
+        Context {
+            deadline: Utc::now().timestamp_millis() + 10_000,
+            ..Default::default()
+        },
+        init_s3_client(),
+        init_sqs_client(),
+        ZstdDecoder::default(),
+        SubgraphSerializer { proto: Vec::with_capacity(1024) },
+        generator,
+        NopCache {},
+        |_, event_result| { dbg!(event_result); },
+        move |bucket, key| async move {
+            let output_event = S3Event {
+                records: vec![
+                    S3EventRecord {
+                        event_version: None,
+                        event_source: None,
+                        aws_region: None,
+                        event_time: chrono::Utc::now(),
+                        event_name: None,
+                        principal_id: S3UserIdentity { principal_id: None },
+                        request_parameters: S3RequestParameters { source_ip_address: None },
+                        response_elements: Default::default(),
+                        s3: S3Entity {
+                            schema_version: None,
+                            configuration_id: None,
+                            bucket: S3Bucket {
+                                name: Some(bucket),
+                                owner_identity: S3UserIdentity { principal_id: None },
+                                arn: None,
+                            },
+                            object: S3Object {
+                                key: Some(key),
+                                size: Some(0),
+                                url_decoded_key: None,
+                                version_id: None,
+                                e_tag: None,
+                                sequencer: None,
+                            },
+                        },
+                    }
+                ]
+            };
+
+            let sqs_client = init_sqs_client();
+
+            // publish to SQS
+            sqs_client.send_message(
+                SendMessageRequest {
+                    message_body: serde_json::to_string(&output_event)
+                        .expect("failed to encode s3 event"),
+                    queue_url: "http://sqs.us-east-1.amazonaws.com:9324/queue/node-identifier-queue".to_string(),
+                    ..Default::default()
+                }
+            ).await?;
+
+            Ok(())
+        },
+    ).await?;
     Ok(())
 }
 
+
+pub fn run_graph_generator_local<
+    EH: EventHandler<
+        InputEvent=Vec<u8>,
+        OutputEvent=Graph,
+        Error=sqs_lambda::error::Error<Arc<failure::Error>>
+    > + Send + Sync + Clone + 'static
+>
+(
+    bucket_name: String,
+    queue_url: String,
+    generator: EH,
+) {
+    std::thread::sleep_ms(10_000);
+    let mut runtime = Runtime::new().unwrap();
+
+    loop {
+        let bucket_name = bucket_name.clone();
+        let queue_url = queue_url.clone();
+        let generator = generator.clone();
+
+        if let Err(e) = runtime.block_on(async move {
+            local_service(
+                bucket_name.clone(),
+                queue_url.clone(),
+                generator.clone(),
+            ).await
+        }) {
+            error!("{}", e);
+            std::thread::sleep_ms(2_000);
+        }
+    }
+}
 
 
 #[cfg(test)]
