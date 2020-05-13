0eef2150c0adf80e50217e99c529f889318c7ff0
Max Wittek <maxwittek@gmail.com>
2020-05-12 19:21:58 -0700

    Added a Github Actions workflow that does tests and lints (#6)

--- grapl_provision.py
+++ grapl_provision.py
@@ -180,6 +180,5 @@ if __name__ == "__main__":
         else:
             break
 
-
     time.sleep(1)
     print("grapl_provision complete!\n")
---
+++ local_dgraph/run_on_github_actions.sh
@@ -0,0 +1,2 @@
+# Run it in the background
+docker run --detach --rm -p 8000:8000 -p 8080:8080 -p 9080:9080 dgraph/standalone:latest
