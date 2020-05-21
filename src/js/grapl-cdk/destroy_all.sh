bfb3e93e0b3e3ea18b972d6574837c2c980d7468
inickles-grapl <inickles@graplsecurity.com>
2020-05-20 18:58:33 -0700

    New CDK.

--- src/js/grapl-cdk/destroy_all.sh
+++ src/js/grapl-cdk/destroy_all.sh
@@ -1,25 +1,6 @@
 #!/usr/bin/env bash
 npm run build &&
-cdk destroy -f --require-approval=never grapl-event-emitters-stack && \
-cdk destroy -f --require-approval=never graplvpcs-stack && \
-cdk destroy -f --require-approval=never graplhistorydb-stack && \
-cdk destroy -f --require-approval=never mastergraphcluster-stack && \
-cdk destroy -f --require-approval=never engagementgraphcluster-stack && \
-cdk destroy -f --require-approval=never grapl-generic-subgraph-generator-stack && \
-cdk destroy -f --require-approval=never grapl-sysmon-subgraph-generator-stack && \
-cdk destroy -f --require-approval=never grapl-node-identity-mapper-stack && \
-cdk destroy -f --require-approval=never grapl-node-identifier-stack && \
-cdk destroy -f --require-approval=never grapl-graph-merger-stack && \
-cdk destroy -f --require-approval=never grapl-analyzer-dispatcher-stack && \
-cdk destroy -f --require-approval=never grapl-analyzer-executor-stack && \
-cdk destroy -f --require-approval=never grapl-engagement-creator-stack && \
-cdk destroy -f --require-approval=never grapl-user-auth-table-stack && \
-cdk destroy -f --require-approval=never engagementedge-stack && \
-cdk destroy -f --require-approval=never engagement-ux-stack && \
-cdk destroy -f --require-approval=never engagements-notebook-stack && \
-
-
-# cdk deploy engagement-creation-service-stack
+cdk destroy -f --require-approval=never "*"
 
 date
 
