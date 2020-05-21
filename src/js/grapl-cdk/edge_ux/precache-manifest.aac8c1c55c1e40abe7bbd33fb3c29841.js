dd95648c72dff37428c4b9892edf608ea53651ce
inickles-grapl <inickles@graplsecurity.com>
2020-05-21 15:46:56 -0700

    Restoring edge_ux - oops.

---
+++ src/js/grapl-cdk/edge_ux/precache-manifest.aac8c1c55c1e40abe7bbd33fb3c29841.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1559545d7b59b6208d6de62ec9460833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa5d4a42dc6ff8f6434",
+    "url": "/static/css/main.68c2e791.chunk.css"
+  },
+  {
+    "revision": "73d3de952035869dbe40",
+    "url": "/static/js/2.81b333a1.chunk.js"
+  },
+  {
+    "revision": "09ebe9339401330dc9228b4c8aaa1a4c",
+    "url": "/static/js/2.81b333a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa5d4a42dc6ff8f6434",
+    "url": "/static/js/main.53a7fb3a.chunk.js"
+  },
+  {
+    "revision": "57f3014d064f35faf08e",
+    "url": "/static/js/runtime-main.21ddd4f8.js"
+  },
+  {
+    "revision": "803a6c667c909d2f62aac6f8277ef1cd",
+    "url": "/static/media/nodes.803a6c66.jpg"
+  }
+]);
---
+++ src/js/grapl-cdk/edge_ux/precache-manifest.f16058b150e67663838fdb9096fe6b28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab271dc891ddd155bf7d98c29eb8afac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "966be6cc109c63c4f7e9",
+    "url": "/static/css/main.68c2e791.chunk.css"
+  },
+  {
+    "revision": "9db14a3b224d92a2f4bc",
+    "url": "/static/js/2.b5cf535e.chunk.js"
+  },
+  {
+    "revision": "09ebe9339401330dc9228b4c8aaa1a4c",
+    "url": "/static/js/2.b5cf535e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "966be6cc109c63c4f7e9",
+    "url": "/static/js/main.a6d4fabf.chunk.js"
+  },
+  {
+    "revision": "57f3014d064f35faf08e",
+    "url": "/static/js/runtime-main.21ddd4f8.js"
+  },
+  {
+    "revision": "803a6c667c909d2f62aac6f8277ef1cd",
+    "url": "/static/media/nodes.803a6c66.jpg"
+  }
+]);
