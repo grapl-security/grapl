2a328c974c8dc4da63dc26748295fa469accb9c5
inickles-grapl <inickles@graplsecurity.com>
2020-05-22 10:52:08 -0700

    Update deploy_all.sh to reflect new stack name for UX.

--- src/js/grapl-cdk/deploy_all.sh
+++ src/js/grapl-cdk/deploy_all.sh
@@ -3,6 +3,6 @@ npm run build &&
 cdk deploy --require-approval=never GraplCdkStack EngagementEdgeStack GraphqlEndpointStack && \
 rm -rf ./edge_ux_package && \
 cdk synth && \
-cdk deploy --require-approval=never EngagementUX && \
+cdk deploy --require-approval=never EngagementUXStack && \
 
 date
