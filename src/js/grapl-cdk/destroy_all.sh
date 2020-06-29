d9e6a3521eecf54a10c2114d08307f8c90c57cb8
jgrillo <jgrillo@graplsecurity.com>
2020-06-29 14:59:29 -0400

    remove dangerous S3 stuff

--- src/js/grapl-cdk/destroy_all.sh
+++ src/js/grapl-cdk/destroy_all.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-for BUCKET in $(aws s3 ls s3:// | cut -d' ' -f 3)
-do
-    aws s3 rm --recursive s3://$BUCKET
-done
 
 npm run build &&
 cdk destroy -f --require-approval=never "*"
