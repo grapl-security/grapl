355ef4f4a1c5f978f2f6fc2ab4bbc50f591fc416
andrea-grapl <64504029+andrea-grapl@users.noreply.github.com>
2020-08-05 15:41:42 -0700

    Internal 184 log out redirect (#198)
    * converted useInterval to timeMod, extracted dashboard styles, fixed useEffect bug of excessive calling
    
    * broke styles into modules, formatting and addition of card info when not logged in
    
    * removed logic to check if local for login, added else statement for rerender to display login message in uploadplugin and sidebar components
    
    * fixed duplicate use state bug
    
    * fixed grapl header styling for engagementview
    
    * refactoring, fixed key errors for tables
    
    * changed color of button
    
    * black formatting

--- src/js/graphql_endpoint/server.js
+++ src/js/graphql_endpoint/server.js
@@ -51,13 +51,7 @@ const corsDelegate = (req, callback) => {
 }
 
 
-const middleware = [cors(corsDelegate)];
-
-if (!IS_LOCAL) {
-    middleware.push(validateJwt)
-} else {
-    console.info("Running locally - disabling auth");
-}
+const middleware = [cors(corsDelegate), validateJwt];
 
 app.options('*', cors(corsDelegate));
 app.use('/graphql', middleware, graphqlHTTP({
