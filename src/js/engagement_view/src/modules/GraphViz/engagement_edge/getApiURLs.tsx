0320f70c4079b5a93e5efaafdba16a992eae1378
inickles-grapl <64668260+inickles-grapl@users.noreply.github.com>
2020-08-18 08:23:30 -0700

    Fixing a couple issues in the Engagement UX: (#217)
    - Regex for our CDK hack was broken causing wrong URL for login.
    - Login URL redirect needed updating for new routing.

--- src/js/engagement_view/src/modules/GraphViz/engagement_edge/getApiURLs.tsx
+++ src/js/engagement_view/src/modules/GraphViz/engagement_edge/getApiURLs.tsx
@@ -1,11 +1,11 @@
 export const getAuthEdge = () => {
-    return "http://" + window.location.hostname + ":8900/"
+    return "http://"+window.location.hostname+":8900/"
 }
 
 export const getGraphQlEdge = () => {
-    return "http://" + window.location.hostname + ":5000/"
+    return "http://"+window.location.hostname+":5000/"
 }
 
 export const getModelPluginEdge = () => {
-    return "http://" + window.location.hostname + ":8123/"
+    return "http://"+window.location.hostname+":8123/"
 }
