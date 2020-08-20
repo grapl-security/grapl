0aad3861fdeaa2f1241bf13a121619b81885e021
jgrillo <jgrillo@graplsecurity.com>
2020-08-20 15:33:01 -0400

    WIP

--- src/python/analyzer-deployer/app.py
+++ src/python/analyzer-deployer/app.py
@@ -1,6 +1,9 @@
+import dataclasses
+from typing import Dict, List, Union
+
 import boto3
 
-from mypy_boto3 import sqs
+from mypy_boto3 import sqs, dynamodb
 
 from chalice import Chalice
 
@@ -13,9 +16,91 @@ def _create_queue(queue_name: str):
     pass
 
 
-@app.route("/")
-def index():
-    return {"hello": "world"}
+@dataclasses.dataclass
+class Analyzer:
+    analyzer_id: str
+    analyzer_versions: List[int]
+    analyzer_active: bool
+    created_time: int
+    last_update_time: int
+
+    def as_dict(self) -> Dict[str, Union[str, int, bool, List[int]]]:
+        dataclasses.asdict(self)
+
+
+@dataclasses.dataclass
+class PortConfig:
+    protocol: str
+    port: int
+
+    def as_dict(self) -> Dict[str, Union[str, int]]:
+        dataclasses.asdict(self)
+
+
+@dataclasses.dataclass
+class TableConfig:
+    table: str
+    write: bool
+
+    def as_dict(self) -> Dict[str, Union[str, bool]]:
+        dataclasses.asdict(self)
+
+
+@dataclasses.dataclass
+class SecretConfig:
+    SecretId: str
+    VersionId: str = None
+    VersionStage: str = None
+
+    def as_dict(self) -> Dict[str, str]:
+        dataclasses.asdict(self)
+
+
+@dataclasses.dataclass
+class AnalyzerConfig:
+    requires_external_internet: List[PortConfig] = None
+    requires_dynamodb: List[TableConfig] = None
+    requires_graph: bool = False
+    requires_secrets: List[SecretConfig] = None
+
+    def as_dict(self) -> Dict[str, Union[str, bool, int, List[Dict[str, Union[str, int]]]]]:
+        dataclasses.asdict(self)
+
+
+@dataclasses.dataclass
+class AnalyzerDeployment:
+    analyzer_id: str
+    analyzer_version: int
+    s3_key: str
+    currently_deployed: bool
+    last_deployed_time: int = None
+    analyzer_configuration: AnalyzerConfig = None
+
+    def as_dict(self) -> Dict[str, Union[str, int, bool]]:
+        dataclasses.asdict(self)
+
+
+@dataclasses.dataclass
+class CreateAnalyzerResponse:
+    analyzer_id: str
+    analyzer_version: int
+    s3_key: str
+
+    def as_dict(self) -> Dict[str, Union[str, int]]:
+        dataclasses.asdict(self)
+
+
+def _create_analyzer(
+    dynamodb_client: dynamodb.DynamoDBServiceResource,
+) -> CreateAnalyzerResponse:
+    analyzer = Analyzer()
+    analyzers_table = dynamodb_client.Table("Analyzers")
+    analyzers_table.put_item(Item=)
+
+
+@app.route("/1/analyzers", methods=["POST"])
+def create_analyzer():
+    return _create_analyzer().as_dict()
 
 
 # The view function above will return {"hello": "world"}
