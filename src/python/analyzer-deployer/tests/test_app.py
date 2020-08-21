aa27ec428e95c0dfad0a22b3ee1b6ff8626d66fa
jgrillo <jgrillo@graplsecurity.com>
2020-08-21 11:12:02 -0400

    serde tests

--- src/python/analyzer-deployer/tests/test_app.py
+++ src/python/analyzer-deployer/tests/test_app.py
@@ -4,39 +4,110 @@ import unittest
 
 import hypothesis.strategies as st
 
+from typing import List
+
 from hypothesis import given
 from chalice.test import Client
 
 from analyzer_deployer.app import (
-    app, Analyzer, PortConfig, SecretConfig, TableConfig, AnalyzerConfig,
-    AnalyzerDeployment, CreateAnalyzerResponse
+    app,
+    Analyzer,
+    PortConfig,
+    SecretConfig,
+    TableConfig,
+    AnalyzerConfig,
+    AnalyzerDeployment,
+    CreateAnalyzerResponse,
 )
 
 
-class TestAnalyzer(unittest.TestCase):
-    @given(
-        st.text(), st.lists(st.integers()), st.booleans(), st.integers(), st.integers()
+def _analyzers() -> st.SearchStrategy[Analyzer]:
+    return st.builds(
+        Analyzer,
+        **{
+            "analyzer_id": st.text(),
+            "analyzer_versions": st.lists(st.integers()),
+            "analyzer_active": st.booleans(),
+            "created_time": st.integers(),
+            "last_update_time": st.integers(),
+        }
     )
-    def test_analyzer_encode_decode_invariant(
-        self,
-        analyzer_id,
-        analyzer_versions,
-        analyzer_active,
-        created_time,
-        last_update_time,
-    ):
-        analyzer = Analyzer(
-            analyzer_id,
-            analyzer_versions,
-            analyzer_active,
-            created_time,
-            last_update_time,
-        )
+
+
+def _port_configs() -> st.SearchStrategy[PortConfig]:
+    return st.builds(PortConfig, **{"protocol": st.text(), "port": st.integers()})
+
+
+def _table_configs():
+    return st.builds(TableConfig, **{"table": st.text(), "write": st.booleans()})
+
+
+def _secret_configs():
+    return st.builds(
+        SecretConfig,
+        **{"SecretId": st.text(), "VersionId": st.text(), "VersionStage": st.text()}
+    )
+
+
+def _analyzer_configs():
+    return st.builds(
+        AnalyzerConfig,
+        **{
+            "requires_external_internet": st.lists(_port_configs()),
+            "requires_dynamodb": st.lists(_table_configs()),
+            "requires_graph": st.booleans(),
+            "requires_secrets": st.lists(_secret_configs()),
+        }
+    )
+
+
+def _analyzer_deployments():
+    return st.builds(
+        AnalyzerDeployment,
+        **{
+            "analyzer_id": st.text(),
+            "analyzer_version": st.integers(),
+            "s3_key": st.text(),
+            "currently_deployed": st.booleans(),
+            "last_deployed_time": st.integers(),
+            "analyzer_configuration": _analyzer_configs(),
+        }
+    )
+
+
+def _create_analyzer_responses():
+    return st.builds(
+        CreateAnalyzerResponse,
+        **{
+            "analyzer_id": st.text(),
+            "analyzer_version": st.integers(),
+            "s3_key": st.text(),
+        }
+    )
+
+
+class TestSerde(unittest.TestCase):
+    @given(_analyzers())
+    def test_analyzer_encode_decode_invariant(self, analyzer: Analyzer):
         serialized = json.dumps(dataclasses.asdict(analyzer))
         deserialized = json.loads(serialized)
         self.assertDictEqual(deserialized, dataclasses.asdict(analyzer))
 
-    def test_
+    @given(_analyzer_deployments())
+    def test_analyzer_deployment_encode_decode_invariant(self, analyzer_deployment):
+        serialized = json.dumps(dataclasses.asdict(analyzer_deployment))
+        deserialized = json.loads(serialized)
+        self.assertDictEqual(deserialized, dataclasses.asdict(analyzer_deployment))
+
+    @given(_create_analyzer_responses())
+    def test_create_analyzer_response_encode_decode_invariant(
+        self, create_analyzer_response
+    ):
+        serialized = json.dumps(dataclasses.asdict(create_analyzer_response))
+        deserialized = json.loads(serialized)
+        self.assertDictEqual(
+            deserialized, dataclasses.asdict(create_analyzer_response)
+        )
 
 
 class TestApp(unittest.TestCase):
