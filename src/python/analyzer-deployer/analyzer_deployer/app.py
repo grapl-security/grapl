6fd50637567c35f047607f0ce3f2ee26a28c8895
jgrillo <jgrillo@graplsecurity.com>
2020-08-24 16:04:18 -0400

    remove chalice boilerplate

--- src/python/analyzer-deployer/analyzer_deployer/app.py
+++ src/python/analyzer-deployer/analyzer_deployer/app.py
@@ -81,24 +81,3 @@ def _create_analyzer(
 def create_analyzer():
     dynamodb_client = boto3.resource("dynamodb")
     return dataclasses.asdict(_create_analyzer(dynamodb_client))
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
