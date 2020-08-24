ed565e4e82deb6c6b9f3619a5eaa9b02f15bb794
jgrillo <jgrillo@graplsecurity.com>
2020-08-24 15:24:56 -0400

    remove cdk + ts

--- src/js/analyzer-cdk/bin/analyzer-cdk.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { AnalyzerCdkStack } from '../lib/analyzer-cdk-stack';
-
-const app = new cdk.App();
-new AnalyzerCdkStack(app, 'AnalyzerCdkStack');
