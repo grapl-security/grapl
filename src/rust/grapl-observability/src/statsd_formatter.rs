91fa905d843d25fda2a879745554bd9e39edf477
Max Wittek <wimax@graplsecurity.com>
2020-08-28 21:05:41 -0700

    it works! it needs more tests

--- src/rust/grapl-observability/src/statsd_formatter.rs
+++ src/rust/grapl-observability/src/statsd_formatter.rs
@@ -1,25 +1,95 @@
 use crate::metric_error::MetricError;
 use failure::Error;
+use itertools::join;
 use lazy_static::lazy_static;
 use regex::Regex;
 
 lazy_static! {
     static ref INVALID_CHARS: Regex = Regex::new("[|#,=:]").unwrap();
 }
-// const DEFAULT_SAMPLE_RATE: f64 = 1.0;
+
+// TODO look at lifetimes, should this own or not, etc
+// seems odd to .to_string() everytihng going in here
+pub struct TagPair {
+    tag_key: String,
+    tag_value: String,
+}
+
+impl TagPair {
+    fn statsd_serialized(&self) -> String {
+        return format!("{}={}", self.tag_key, self.tag_value);
+    }
+}
 
 fn reject_invalid_chars(s: &str) -> Result<(), Error> {
     let matched = INVALID_CHARS.is_match(s);
-    if !matched {
-        Ok(())
-    } else {
+    if matched {
         Err(Error::from(MetricError {}))
+    } else {
+        Ok(())
     }
+}
+
+pub enum MetricType {
+    Gauge,
+    Counter,
+    Millis,
+    Histogram,
+}
+
+impl MetricType {
+    fn statsd_type(&self) -> String {
+        match self {
+            MetricType::Gauge => "g",
+            MetricType::Counter => "c",
+            MetricType::Millis => "ms",
+            MetricType::Histogram => "h",
+        }
+        .to_string()
+    }
+}
+
+pub fn statsd_format(
+    metric_name: &str,
+    value: f64, // should i make a union type?
+    typ: MetricType,
+    sample_rate: Option<f64>,
+    tags: &[TagPair],
+) -> Result<String, Error> {
+    // unlikely we'd go over this
+    reject_invalid_chars(metric_name)?;
+
+    let mut sections: Vec<String> = Vec::with_capacity(6);
+
+    sections.push(format!(
+        "{metric_name}:{value}",
+        metric_name = metric_name,
+        value = value
+    ));
+    sections.push(typ.statsd_type());
+
+    match (typ, sample_rate) {
+        (MetricType::Counter, Some(rate)) => {
+            // a rate of 1.0 we'll just ignore
+            if rate >= 0.0 && rate < 1.0 {
+                sections.push(format!("@{sample_rate}", sample_rate = rate));
+            } else {
+                return Err(Error::from(MetricError {}));
+            }
+        }
+        _ => {}
+    }
+
+    let tag_section = join(tags.into_iter().map(TagPair::statsd_serialized), ",");
+    if !tag_section.is_empty() {
+        sections.push(format!("#{}", tag_section));
+    }
+    return Ok(sections.join("|"));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::statsd_formatter::reject_invalid_chars;
+    use crate::statsd_formatter::{reject_invalid_chars, statsd_format, MetricType, TagPair};
 
     const INVALID_STRS: [&str; 5] = [
         "some|metric",
@@ -30,6 +100,25 @@ mod tests {
     ];
 
     const VALID_STR: &str = "some_str";
+    const VALID_VALUE: f64 = 12345.6;
+
+    fn make_tags() -> Vec<TagPair> {
+        vec![
+            TagPair {
+                tag_key: "some_key".to_string(),
+                tag_value: "some_value".to_string(),
+            },
+            TagPair {
+                tag_key: "some_key_2".to_string(),
+                tag_value: "some_value_2".to_string(),
+            },
+        ]
+    }
+
+    fn make_empty_tags() -> [TagPair; 0] {
+        let empty_slice: [TagPair; 0] = [];
+        return empty_slice;
+    }
 
     #[test]
     fn test_reject_invalid_chars() {
@@ -39,5 +128,34 @@ mod tests {
         });
 
         assert!(reject_invalid_chars(VALID_STR).is_ok())
+    }
+
+    #[test]
+    fn test_statsd_format_basic_counter() {
+        let result = statsd_format(
+            VALID_STR,
+            VALID_VALUE,
+            MetricType::Counter,
+            None,
+            &make_empty_tags(),
+        )
+        .unwrap();
+        assert_eq!(result, "some_str:12345.6|c")
+    }
+
+    #[test]
+    fn test_statsd_format_tags() {
+        let result = statsd_format(
+            VALID_STR,
+            VALID_VALUE,
+            MetricType::Counter,
+            None,
+            &make_tags(),
+        )
+        .unwrap();
+        assert_eq!(
+            result,
+            "some_str:12345.6|c|#some_key=some_value,some_key_2=some_value_2"
+        )
     }
 }
