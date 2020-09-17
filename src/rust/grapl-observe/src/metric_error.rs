edae8185bcd31531df6e92e2925c6f305a783728
Max Wittek <wimax@graplsecurity.com>
2020-09-17 13:33:11 -0700

    better testing around MetricReporter - dependency inject utcnow and the Write
    fmt
    
    Updating to also test utc
    
    Getting closer; may need to arc it
    
    it compiles woo
    
    we did it fam

--- src/rust/grapl-observe/src/metric_error.rs
+++ src/rust/grapl-observe/src/metric_error.rs
@@ -6,6 +6,14 @@ pub enum MetricError {
     MetricInvalidCharacterError(),
     #[error("MetricInvalidSampleRateError")]
     MetricInvalidSampleRateError(),
-    #[error("MetricBufWriteError")]
+    #[error("MetricBufWriteError: {0}")]
     MetricBufWriteError(#[from] std::fmt::Error),
+    #[error("MetricIoWriteError: {0}")]
+    MetricIoWriteError(String),
+}
+
+impl From<std::io::Error> for MetricError {
+    fn from(i: std::io::Error) -> MetricError {
+        MetricError::MetricIoWriteError(i.to_string())
+    }
 }
