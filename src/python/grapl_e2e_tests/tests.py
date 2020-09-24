5566afa0613c595fa9de18e7acaba818bce440a7
Max Wittek <wimax@graplsecurity.com>
2020-09-24 11:29:13 -0700

    Flesh out the "does dgraph get populated" e2e test

--- src/python/grapl_e2e_tests/tests.py
+++ src/python/grapl_e2e_tests/tests.py
@@ -1,4 +1,10 @@
 from unittest import TestCase
+from grapl_analyzerlib.grapl_client import MasterGraphClient
+from grapl_analyzerlib.nodes.lens import LensQuery, LensView
+import resources
+from typing import Any, Optional
+
+LENS_NAME = "DESKTOP-FVSHABR"
 
 
 class TestEndToEnd(TestCase):
@@ -9,4 +15,28 @@ class TestEndToEnd(TestCase):
         assert True
 
     def test_expected_data_in_dgraph(self) -> None:
-        assert True
+        lens_resource = wait_for_lens()
+        wait_result = resources.wait_on_resources([lens_resource], timeout_secs=120)
+        lens: LensView = wait_result[lens_resource]
+        assert lens.get_lens_name() == LENS_NAME
+        assert len(lens.get_scope()) == 3
+
+
+def wait_for_lens():
+    local_client = MasterGraphClient()
+    query = LensQuery().with_lens_name(LENS_NAME)
+    return WaitForLens(local_client, query)
+
+
+class WaitForLens(resources.WaitForResource):
+    def __init__(self, dgraph_client: Any, query: LensQuery):
+        self.dgraph_client = dgraph_client
+        self.query = query
+
+    def acquire(self) -> Optional[Any]:
+        # result = self.query.get_count(self.dgraph_client)
+        result = self.query.query_first(self.dgraph_client)
+        return result
+
+    def __str__(self) -> str:
+        return f"WaitForLens({self.query})"
