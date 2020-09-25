aa869a525763a80a329dd5f47f02bc5117524a6c
wimax-grapl <69007229+wimax-grapl@users.noreply.github.com>
2020-09-25 14:28:55 -0700

    Make e2e test more robust
    * add a sleep before checking edges of a lensnode
    
    * okay
    
    * Okay now its actually robust

--- src/python/grapl_e2e_tests/tests.py
+++ src/python/grapl_e2e_tests/tests.py
@@ -2,7 +2,8 @@ from unittest import TestCase
 from grapl_analyzerlib.grapl_client import MasterGraphClient
 from grapl_analyzerlib.nodes.lens import LensQuery, LensView
 import resources
-from typing import Any, Optional
+from typing import Any, Optional, Callable
+import inspect
 
 LENS_NAME = "DESKTOP-FVSHABR"
 
@@ -17,9 +18,14 @@ class TestEndToEnd(TestCase):
     def test_expected_data_in_dgraph(self) -> None:
         lens_resource = wait_for_lens()
         wait_result = resources.wait_on_resources([lens_resource], timeout_secs=120)
+
         lens: LensView = wait_result[lens_resource]
         assert lens.get_lens_name() == LENS_NAME
-        assert len(lens.get_scope()) == 3
+
+        # lens scope is not atomic
+        resources.wait_on_resources(
+            [WaitForCondition(lambda: (len(lens.get_scope()) == 4))]
+        )
 
 
 def wait_for_lens():
@@ -28,13 +34,29 @@ def wait_for_lens():
     return WaitForLens(local_client, query)
 
 
+class WaitForCondition(resources.WaitForResource):
+    """ just something nice n generic """
+
+    def __init__(self, fn: Callable[[], Optional[bool]]) -> None:
+        self.fn = fn
+
+    def acquire(self) -> Optional[Any]:
+        result = self.fn()
+        if result:
+            return self  # just anything non-None
+        else:
+            return None
+
+    def __str__(self) -> str:
+        return f"WaitForCondition({inspect.getsource(self.fn)})"
+
+
 class WaitForLens(resources.WaitForResource):
-    def __init__(self, dgraph_client: Any, query: LensQuery):
+    def __init__(self, dgraph_client: Any, query: LensQuery) -> None:
         self.dgraph_client = dgraph_client
         self.query = query
 
     def acquire(self) -> Optional[Any]:
-        # result = self.query.get_count(self.dgraph_client)
         result = self.query.query_first(self.dgraph_client)
         return result
 
