d96bbe999de0eeab7c3390ada9346372b40df5e2
Max Wittek <wimax@graplsecurity.com>
2020-10-07 15:31:35 -0700

    add grapl-tests-common

---
+++ src/python/grapl-tests-common/grapl_tests_common/resources.py
@@ -0,0 +1,79 @@
+from time import sleep
+from datetime import datetime, timezone, timedelta
+import logging
+import boto3  # type: ignore
+import botocore  # type: ignore
+from typing import Any, Set, Sequence, Optional, Dict, Mapping
+from typing_extensions import Protocol
+from itertools import cycle
+
+
+class WaitForResource(Protocol):
+    def acquire(self) -> Optional[Any]:
+        pass
+
+
+class WaitForS3Bucket(WaitForResource):
+    def __init__(self, s3_client: Any, bucket_name: str):
+        self.s3_client = s3_client
+        self.bucket_name = bucket_name
+
+    def acquire(self) -> Optional[Any]:
+        try:
+            return self.s3_client.head_bucket(Bucket=self.bucket_name)
+        except self.s3_client.exceptions.NoSuchBucket:
+            return None
+
+    def __str__(self) -> str:
+        return f"WaitForS3Bucket({self.bucket_name})"
+
+
+class WaitForSqsQueue(WaitForResource):
+    def __init__(self, sqs_client: Any, queue_name: str):
+        self.sqs_client = sqs_client
+        self.queue_name = queue_name
+
+    def acquire(self) -> Optional[Any]:
+        try:
+            return self.sqs_client.get_queue_url(QueueName=self.queue_name)
+        except (
+            self.sqs_client.exceptions.QueueDoesNotExist,
+            botocore.parsers.ResponseParserError,
+        ):
+            return None
+
+    def __str__(self) -> str:
+        return f"WaitForSqsQueue({self.queue_name})"
+
+
+def wait_for(
+    resources: Sequence[WaitForResource], 
+    timeout_secs: int=30
+) -> Mapping[WaitForResource, Any]:
+    completed: Dict[WaitForResource, Any] = {}
+
+    get_now = lambda: datetime.now(tz=timezone.utc)
+
+    timeout_after = get_now() + timedelta(seconds=timeout_secs)
+
+    # Cycle through `resources` forever, until all resources are attained
+    # hacky? potentially O(infinity)? yes
+    for resource in cycle(resources):
+        now = get_now()
+        if now >= timeout_after:
+            raise TimeoutError(f"Timed out waiting for {resource}")
+        if len(completed) == len(resources):
+            break
+        if resource in completed:
+            continue
+
+        secs_remaining = int((timeout_after - now).total_seconds())
+        logging.info(f"Waiting for resource ({secs_remaining} secs remain): {resource}")
+
+        result = resource.acquire()
+        if result is not None:
+            completed[resource] = result
+        else:
+            sleep(1)
+
+    return completed
---
+++ src/python/grapl-tests-common/grapl_tests_common/setup.py
@@ -0,0 +1,122 @@
+import time
+from os import environ
+from grapl_tests_common import resources
+from sys import stdout, exit
+from typing import Any, NamedTuple, Sequence
+import boto3  # type: ignore
+import logging
+import pytest
+import subprocess
+import sys
+
+# mypy later maybe
+S3ServiceResource = Any
+SqsServiceResource = Any
+
+BUCKET_PREFIX = environ["BUCKET_PREFIX"]
+assert BUCKET_PREFIX == "local-grapl"
+
+AnalyzerUpload = NamedTuple(
+    "AnalyzerUpload",
+    (
+        ("local_path", str),
+        ("s3_key", str),
+    ),
+)
+
+
+def _upload_analyzers(
+    s3_client: S3ServiceResource, analyzers: Sequence[AnalyzerUpload]
+) -> None:
+    """
+    Basically reimplementing upload_local_analyzers.sh
+    Janky, since Jesse will have an analyzer-uploader service pretty soon.
+    """
+
+    bucket = f"{BUCKET_PREFIX}-analyzers-bucket"
+    for (local_path, s3_key) in analyzers:
+        logging.info(f"S3 uploading {local_path}")
+        with open(local_path, "r") as f:
+            s3_client.put_object(Body=f.read(), Bucket=bucket, Key=s3_key)
+
+
+def _upload_test_data(
+    s3_client: S3ServiceResource, test_data_paths: Sequence[str]
+) -> None:
+    logging.info(f"Running upload-sysmon-logs")
+
+    # i hate this lol
+    # but it's probably better than mucking with path and importing that module...
+    for path in test_data_paths:
+        subprocess.run(
+            [
+                "python3",
+                "/home/grapl/etc/local_grapl/bin/upload-sysmon-logs.py",
+                "--bucket_prefix",
+                BUCKET_PREFIX,
+                "--logfile",
+                path,
+                "--use-links",
+                "True",
+            ]
+        )
+
+
+def _create_s3_client() -> S3ServiceResource:
+    return boto3.client(
+        "s3",
+        endpoint_url="http://s3:9000",
+        aws_access_key_id="minioadmin",
+        aws_secret_access_key="minioadmin",
+    )
+
+
+def _create_sqs_client() -> SqsServiceResource:
+    # mostly cribbed from upload-sysmon-logs
+    return boto3.client(
+        "sqs",
+        endpoint_url="http://sqs:9324",
+        region_name="us-east-1",
+        aws_access_key_id="minioadmin",
+        aws_secret_access_key="minioadmin",
+    )
+
+
+def _verbose_sleep(secs: int, reason: str) -> None:
+    logging.info(f"Sleeping for {secs} secs: {reason}")
+    time.sleep(secs)
+
+
+def setup(
+    analyzers: Sequence[AnalyzerUpload],
+    test_data_paths: Sequence[str],
+) -> None:
+    logging.basicConfig(stream=stdout, level=logging.INFO)
+    _verbose_sleep(10, "awaiting local aws")
+
+    s3_client = _create_s3_client()
+    sqs_client = _create_sqs_client()
+
+    resources.wait_for(
+        [
+            # for uploading analyzers
+            resources.WaitForS3Bucket(s3_client, f"{BUCKET_PREFIX}-analyzers-bucket"),
+            # for upload-sysmon-logs.py
+            resources.WaitForS3Bucket(s3_client, f"{BUCKET_PREFIX}-sysmon-log-bucket"),
+            resources.WaitForSqsQueue(sqs_client, "grapl-sysmon-graph-generator-queue"),
+        ]
+    )
+
+    _upload_analyzers(s3_client, analyzers)
+    _upload_test_data(s3_client, test_data_paths)
+
+    _verbose_sleep(30, "let the pipeline do its thing")
+
+
+def exec_pytest() -> None:
+    result = pytest.main(
+        [
+            "-s",  # disable stdout capture
+        ]
+    )
+    sys.exit(result)
