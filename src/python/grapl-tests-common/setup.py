e09e4fe86409064f42c0459b351edb5a71aa1129
Max Wittek <wimax@graplsecurity.com>
2020-10-09 18:51:59 -0700

    move upload_sysmon_logs out of etc

--- src/python/grapl-tests-common/setup.py
+++ src/python/grapl-tests-common/setup.py
@@ -41,6 +41,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "typing_extensions",
+        "zstd",
     ],
     # We'll probably have some dataclasses in here in the future
     python_requires=">=3.6",
