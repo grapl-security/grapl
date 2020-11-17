9910221f0bc3dd23c5b09fcbf3921f9ae486c708
inickles-grapl <inickles@graplsecurity.com>
2020-11-16 16:36:11 -0800

    Try to get required CDK deployment args from environment. Falls back to variables defined in bin/grapl-cdk.ts.

--- src/js/grapl-cdk/bin/grapl-cdk.ts
+++ src/js/grapl-cdk/bin/grapl-cdk.ts
@@ -5,17 +5,23 @@ import * as cdk from '@aws-cdk/core';
 import { GraplCdkStack } from '../lib/grapl-cdk-stack';
 import { EngagementUx } from '../lib/engagement';
 
-const deployName = 'Grapl-MYDEPLOYMENT';
-const graplVersion = 'latest';
+// Deployment parameters
+const deployName = undefined; // ex: 'Grapl-my-deployment'
+const graplVersion = undefined;
 const watchfulEmail = undefined;
+
+const stackName = process.env.GRAPL_CDK_DEPLOYMENT_NAME || deployName;
+if (!stackName) {
+    throw new Error("Error: Missing Grapl deployment name. Set via bin/grapl-cdk.ts, or environment variable GRAPL_CDK_DEPLOYMENT_NAME.");
+}
 
 const app = new cdk.App();
 
 const grapl = new GraplCdkStack(app, 'Grapl', {
-    version: graplVersion,
-    stackName: deployName,
-    tags: { 'grapl deployment': deployName },
-    watchfulEmail,
+    version:process.env.GRAPL_CDK_VERSION ||  graplVersion || 'latest',
+    stackName: stackName,
+    watchfulEmail: process.env.GRAPL_CDK_WATCHFUL_EMAIL || watchfulEmail,
+    tags: { 'grapl deployment': stackName},
     description: 'Grapl base deployment',
 });
 
