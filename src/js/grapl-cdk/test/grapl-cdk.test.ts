1ae4085a2032d53b518ef59ae1a17a63aa1b5c90
jgrillo-grapl <64786504+jgrillo-grapl@users.noreply.github.com>
2020-11-25 23:51:19 -0500

    Internal 79 dgraph disk monitoring (#372)
    * internal-79 WIP
    
    * oops
    
    * a few bugfixes, release automation
    
    * some updates from testing
    
    * swarm cluster automation
    
    * fixes
    
    * more fixes
    
    * more fixes
    
    * isort
    
    * actually create s3 buckets
    
    * some updates
    
    * some bug fixes
    
    * note that deploying takes time
    
    * readme updates
    
    * fixes
    
    * more fixes
    
    * almost there
    
    * fixed!
    
    * docs updates and bugfixes
    
    * oops
    
    * isort
    
    * fmt
    
    * clean up instance class handling
    
    * oops
    
    * readme updates
    
    * setup script updates
    
    * fix dgraph issue, ship logs to cloudwatch
    
    * lints
    
    * fix lgtm issues
    
    * updates for review comments
    
    Co-authored-by: wimax-grapl <69007229+wimax-grapl@users.noreply.github.com>

--- src/js/grapl-cdk/test/grapl-cdk.test.ts
+++ src/js/grapl-cdk/test/grapl-cdk.test.ts
@@ -4,6 +4,7 @@ import {
     MatchStyle,
 } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 import * as GraplCdk from '../lib/grapl-cdk-stack';
 
 test('Empty Stack', () => {
@@ -12,6 +13,7 @@ test('Empty Stack', () => {
     const stack = new GraplCdk.GraplCdkStack(app, 'MyTestStack', {
         stackName: 'Grapl-Test',
         version: 'latest',
+        dgraphInstanceType: new ec2.InstanceType("t3a.medium"),
     });
     // THEN
     expectCDK(stack).to(
