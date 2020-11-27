13b76860acfa83fa62d3b85d3df76708c69c8c83
inickles-grapl <64668260+inickles-grapl@users.noreply.github.com>
2020-11-27 09:54:26 -0800

    Make the CloudWatch Pipeline Dashboard name specific to deployment name.

--- src/js/grapl-cdk/lib/pipeline_dashboard.ts
+++ src/js/grapl-cdk/lib/pipeline_dashboard.ts
@@ -2,33 +2,41 @@ import * as cdk from '@aws-cdk/core';
 import * as cloudwatch from '@aws-cdk/aws-cloudwatch';
 import { Service } from './service';
 
-function invocationsWidget(service: Service, isRetry?: boolean): cloudwatch.GraphWidget {
-    const titleSuffix = isRetry ? " (retry)" : "";
-    const handler = isRetry ? service.event_retry_handler : service.event_handler;
+function invocationsWidget(
+    service: Service,
+    isRetry?: boolean
+): cloudwatch.GraphWidget {
+    const titleSuffix = isRetry ? ' (retry)' : '';
+    const handler = isRetry
+        ? service.event_retry_handler
+        : service.event_handler;
     return new cloudwatch.GraphWidget({
         title: `Invoke ${service.serviceName}${titleSuffix}`,
         left: [
-            handler.metricInvocations({color: cloudwatch.Color.BLUE}),
-            handler.metricErrors({color: cloudwatch.Color.RED}),
+            handler.metricInvocations({ color: cloudwatch.Color.BLUE }),
+            handler.metricErrors({ color: cloudwatch.Color.RED }),
         ],
-        width: 12,  // max of 24; we have 2 next to each other
+        width: 12, // max of 24; we have 2 next to each other
         liveData: true,
-    })
-};
+    });
+}
+
+export class PipelineDashboardProps {
+    readonly services: Service[];
+    readonly namePrefix: string;
+}
 
 export class PipelineDashboard extends cdk.Construct {
     constructor(
         scope: cdk.Construct,
         id: string,
-        services: Service[],
+        props: PipelineDashboardProps
     ) {
         super(scope, id);
-        const dashboard = new cloudwatch.Dashboard(
-            scope, 
-            'GraplPipelineDashboard', 
-            {dashboardName: 'GraplPipelineDashboard'},
-        );
-        for (const service of services) {
+        const dashboard = new cloudwatch.Dashboard(this, 'Dashboard', {
+            dashboardName: props.namePrefix + '-PipelineDashboard',
+        });
+        for (const service of props.services) {
             const invocations = invocationsWidget(service, false);
             const retryInvocations = invocationsWidget(service, true);
             dashboard.addWidgets(invocations, retryInvocations);
