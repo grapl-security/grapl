37e41e13e398b3df82379ba9d1adbb85cf0031ff
inickles-grapl <inickles@graplsecurity.com>
2020-11-29 20:04:55 -0800

    Update the Edege UX to the latest build for the CDK.

---
+++ src/js/grapl-cdk/edge_ux/precache-manifest.e8b34e2229e1adcb099eeb404faf3315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb5f55d76cb1507b40c4ce483c62ad0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "020042cf28b572a7cfd1",
+    "url": "/static/css/main.c328d195.chunk.css"
+  },
+  {
+    "revision": "97572c6ad42169cfc30c",
+    "url": "/static/js/2.6a944662.chunk.js"
+  },
+  {
+    "revision": "81b4a70d52a7735842ce4aa830f2a2c0",
+    "url": "/static/js/2.6a944662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "020042cf28b572a7cfd1",
+    "url": "/static/js/main.844ca6ed.chunk.js"
+  },
+  {
+    "revision": "59379d73f145341cdd6f",
+    "url": "/static/js/runtime-main.f4c0327b.js"
+  },
+  {
+    "revision": "803a6c667c909d2f62aac6f8277ef1cd",
+    "url": "/static/media/nodes.803a6c66.jpg"
+  }
+]);
