65c07220b464a08f6c38b2ab798fb70f3bd85d1c
hxnyk <hkwokcle@gmail.com>
2020-12-21 15:07:41 -0800

    Clean up sagemaker notebook fetch and update ui component to match

--- src/js/engagement_view/src/components/Dashboard.tsx
+++ src/js/engagement_view/src/components/Dashboard.tsx
@@ -1,69 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import { checkLogin } from '../Login';
-import GraplHeader from "./reusableComponents/GraplHeader";
-import { Link } from 'react-router-dom';
-import { dasboardStyles } from './makeStyles/DashboardStyles';
-import LoginNotification from "./reusableComponents/Notifications";
-import {getNotebookUrl} from "./SagemakerNotebook";
+import React, { useEffect, useState } from "react";
 import Button from "@material-ui/core/Button";
+import { useAsync } from "react-async-hook";
+import { checkLogin } from "../Login";
+import GraplHeader from "./reusableComponents/GraplHeader";
+import { Link } from "react-router-dom";
+import { dasboardStyles } from "./makeStyles/DashboardStyles";
+import LoginNotification from "./reusableComponents/Notifications";
+import { getNotebookUrl } from "../services/notebookService";
 
-
-const useStyles = dasboardStyles; 
+const useStyles = dasboardStyles;
 
 export default function Dashboard() {
-    const classes = useStyles();
-    
-    const [state, setState] = useState({
-        loggedIn: true,
-        renderedOnce: false,
-    });
+  const asyncSagemakerUrl = useAsync(getNotebookUrl, []);
+  const classes = useStyles();
 
-    useEffect(
-        () => {
-            if(state.renderedOnce){
-                return;
-            }
+  const [state, setState] = useState({
+    loggedIn: true,
+    renderedOnce: false,
+  });
 
-            const interval = setInterval(async () => {
-                await checkLogin()
-                .then((loggedIn) => {
-                    if(!loggedIn){
-                        console.warn("Logged out")
-                    }
-                    setState({
-                        loggedIn: loggedIn || false, 
-                        renderedOnce: true,
-                    });
-                });
-            }, 2000);
+  useEffect(() => {
+    if (state.renderedOnce) {
+      return;
+    }
 
-            return () => { clearInterval(interval) }
-        }, 
-    [state, setState])
+    const interval = setInterval(async () => {
+      await checkLogin().then((loggedIn) => {
+        if (!loggedIn) {
+          console.warn("Logged out");
+        }
+        setState({
+          loggedIn: loggedIn || false,
+          renderedOnce: true,
+        });
+      });
+    }, 2000);
 
-    console.log("state - loggedin", state.loggedIn); 
+    return () => {
+      clearInterval(interval);
+    };
+  }, [state, setState]);
 
-    const loggedIn = state.loggedIn;
+  console.log("state - loggedin", state.loggedIn);
 
-    return (
-        <> 
-            <GraplHeader displayBtn={false} />
+  const loggedIn = state.loggedIn;
 
-            <div className = { classes.dashboard}>
-                <section className = { classes.nav }>
-                        <Link to = "/engagements" className = {classes.link}> Engagements </Link>
-                        <Link to = "/plugins" className = {classes.link}> Upload Plugin </Link>
-                        <Button onClick={getNotebookUrl} className = {classes.link}> Open Engagement Notebook </Button>
-                </section>
-                
-                <section className = { classes.welcome }>
-                    <div className = {classes.loggedIn}>
-                        {!loggedIn ? <LoginNotification /> : ""}
-                    </div>
+  if (asyncSagemakerUrl.loading || !asyncSagemakerUrl.result) {
+    return <div>Loading...</div>;
+  }
 
-                    <h1> Welcome! </h1>
-                </section>
-            </div>
-        </>
-    )
-}       
+  const openSagemakerUrl = () => window.open(asyncSagemakerUrl.result);
+
+  return (
+    <>
+      <GraplHeader displayBtn={false} />
+
+      <div className={classes.dashboard}>
+        <section className={classes.nav}>
+          <Link to="/engagements" className={classes.link}>
+            Engagements
+          </Link>
+          <Link to="/plugins" className={classes.link}>
+            Upload Plugin
+          </Link>
+          <Button onClick={openSagemakerUrl} className={classes.link}>
+            Open Engagement Notebook
+          </Button>
+        </section>
+
+        <section className={classes.welcome}>
+          <div className={classes.loggedIn}>
+            {!loggedIn ? <LoginNotification /> : ""}
+          </div>
+
+          <h1> Welcome! </h1>
+        </section>
+      </div>
+    </>
+  );
+}
---
+++ src/js/engagement_view/src/services/notebookService.ts
@@ -0,0 +1,8 @@
+import apiFetch from "./fetch";
+import DEV_API_EDGES from "./constants";
+
+export const getNotebookUrl = async (): Promise<string> => {
+  return apiFetch(`${DEV_API_EDGES.auth}/getNotebook`, "post").then(
+    (result) => result.success.notebook_url
+  );
+};
