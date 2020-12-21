f3900744e2a4711de4923a7fcb72ec341b7f352d
hxnyk <hkwokcle@gmail.com>
2020-12-21 14:05:22 -0800

    Move routes out of App

---
+++ src/js/engagement_view/src/routes/routes.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../LogIn.css";
+import { LogIn } from "../Login";
+import { EngagementUx } from "../components/EngagementView";
+import Dashboard from "../components/Dashboard";
+import UploadPlugin from "../components/UploadPlugin";
+import { HashRouter, Route, Switch } from "react-router-dom";
+
+// Updates our react state, as well as localStorage state, to reflect the page
+// we should render
+
+export default function Routes() {
+  console.log("Grapl loaded");
+
+  return (
+    <HashRouter>
+      <Switch>
+        <Route exact path="/login" component={LogIn} />
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/plugins" component={UploadPlugin} />
+        <Route exact path="/engagements" component={EngagementUx} />
+      </Switch>
+    </HashRouter>
+  );
+}
