5797af6d103c187fea1d22f97ee68942330f85c3
andrea-grapl <andrea@graplsecurity.com>
2020-12-30 16:06:54 -0700

    removed commented out code

--- src/js/engagement_view/src/services/uploadPlugin/getPluginList.tsx
+++ src/js/engagement_view/src/services/uploadPlugin/getPluginList.tsx
@@ -1,35 +1,11 @@
-import { PluginPayload } from "../../components/uploadPlugin/utils/uploadPluginTypes";
-import {getModelPluginEdge} from "../getApiURLs"
-
 import DEV_API_EDGES from '../constants';
 import {apiFetch} from '../fetch';
 
-
 export const getPluginList = async () => {
-    // console.log("getModelPluginEdge", getModelPluginEdge())
-
- 
-
-    // const res = await fetch(`${getModelPluginEdge()}listModelPlugins`, 
-    //     {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //         credentials: 'include',
-    //     }
-    // );
-    
-    // console.log("res", res);
-
-    // const body = await res.json() as any;
-    
-    // console.log("body", body);
-
     try { 
-        const getPluginList = await apiFetch(`${DEV_API_EDGES.modelPluginEdge}/listModelPlugins`, "POST");
+        const getPluginListReq = await apiFetch(`${DEV_API_EDGES.modelPluginEdge}/listModelPlugins`, "POST");
 
-        let pluginList: string[] = getPluginList.success.plugin_list;
+        let pluginList: string[] = getPluginListReq.success.plugin_list;
 
         return pluginList;
     } catch (e) {
