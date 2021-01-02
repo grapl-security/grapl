4dfe7800b0b21bb832c69a128fd110982dd1fba3
inickles-grapl <inickles@graplsecurity.com>
2021-01-02 09:04:04 -0800

    Change github actions to test make + buildx.

--- etc/ci_scripts/install_requirements.sh
+++ etc/ci_scripts/install_requirements.sh
@@ -1,3 +1,8 @@
 
-wget https://github.com/dnephin/dobi/releases/download/v0.13.0/dobi-linux
-chmod +x dobi-linux
+# wget https://github.com/dnephin/dobi/releases/download/v0.13.0/dobi-linux
+# chmod +x dobi-linux
+
+# update docker
+docker --version
+sudo apt install docker-ce docker-ce-cli containerd.io
+docker --version
