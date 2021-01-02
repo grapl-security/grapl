81a0b844a714cf605cee834486e4581d7f936b8e
inickles-grapl <inickles@graplsecurity.com>
2021-01-02 09:17:49 -0800

    add docker gpg key to github actions.

--- etc/ci_scripts/install_requirements.sh
+++ etc/ci_scripts/install_requirements.sh
@@ -4,6 +4,7 @@
 
 # update docker
 docker --version
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
 sudo apt-get update
 sudo apt install docker-ce docker-ce-cli containerd.io
