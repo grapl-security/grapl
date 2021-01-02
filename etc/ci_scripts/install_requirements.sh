0274d8269fbcce9d7079d3efe0e88a37dd1b48d4
inickles-grapl <inickles@graplsecurity.com>
2021-01-02 09:14:56 -0800

    attempt to update docker in github actions.

--- etc/ci_scripts/install_requirements.sh
+++ etc/ci_scripts/install_requirements.sh
@@ -4,5 +4,7 @@
 
 # update docker
 docker --version
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+sudo apt-get update
 sudo apt install docker-ce docker-ce-cli containerd.io
 docker --version
