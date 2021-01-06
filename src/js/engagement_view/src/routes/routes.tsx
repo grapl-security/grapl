474eeb6ee926d9888784951f3f262292f086ad1e
andrea-grapl <andrea@graplsecurity.com>
2021-01-05 21:45:51 -0700

    remove comment

--- src/js/engagement_view/src/routes/routes.tsx
+++ src/js/engagement_view/src/routes/routes.tsx
@@ -6,9 +6,6 @@ import Dashboard from "../components/dashboard/Dashboard";
 import UploadPlugin from "../components/uploadPlugin/UploadPluginView";
 import { HashRouter, Route, Switch } from "react-router-dom";
 
-// Updates our react state, as well as localStorage state, to reflect the page
-// we should render
-
 export default function Routes() {
   console.log("Grapl loaded");
 
