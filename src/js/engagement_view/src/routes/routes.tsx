e6d118fc456062177a0df1abb9e4eff8f70e6fa0
andrea-grapl <64504029+andrea-grapl@users.noreply.github.com>
2021-01-07 15:38:52 -0700

    Internal 133 frontend refactor (#517)
    * Move routes out of App
    
    * Add services folder with fetch wrapper, constants, and sample service
    
    * Add react-async-hook to package.json
    
    * Clean up sagemaker notebook fetch and update ui component to match
    
    * Clean up imports for reusable components
    
    * Move loading check into onclick method
    
    * Remove extra spacing
    
    * Move login check into services
    
    * Update checkLogin  to match previous functionality
    
    * utilize absolute imports by specifying baseUrl in tsconfig.json & modified file paths accordingly
    
    * dashboard reorg
    
    * refactored components folder
    
    * component + apiReq refactoring
    
    * more reorg
    
    * refactor services
    
    * refactored uploadPlugin API requests to use new fetch functions
    
    * renaming for clarity
    
    * removed commented out code
    
    * refactored type folder
    
    * fix query in graphQLEndpointRetrieveGraphReq, add service map, additional reorganizing
    
    * remove .json in uploadFilesToDgraph because apiFetchPostRequest already converts data to json
    
    * moved types to type folder
    
    * .cdk files from ./build.sh
    
    * fix baseUrl in .tsconfig for absolute imports and use where possible - can not use absolute imports in deeply nested folders - a problem for another day. Also rerun build.sh
    
    * actually commit build.sh files.....
    
    * remove comment
    
    * build.sh files
    
    * removed unused import to fix lgtm warning
    
    * remove localfix for uploading plugins
    
    * removed localfix for model-plugin-deployer, black formatting
    
    * black formatting
    
    * black formatting
    
    * redo py lint
    
    * fix formatting for model plugin & reformat using black
    
    * formatting
    
    * PR review changes + build.sh
    
    Co-authored-by: hxnyk <hkwokcle@gmail.com>
    Co-authored-by: wimax-grapl <69007229+wimax-grapl@users.noreply.github.com>
    Co-authored-by: Max Wittek <wimax@graplsecurity.com>

--- src/js/engagement_view/src/routes/routes.tsx
+++ src/js/engagement_view/src/routes/routes.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import "../LogIn.css";
-import { LogIn } from "../Login";
-import { EngagementUx } from "../components/EngagementView";
-import Dashboard from "../components/Dashboard";
-import UploadPlugin from "../components/UploadPlugin";
+import "../components/login/LogIn.css";
+import { LogIn } from "../components/login/Login";
+import { EngagementUx } from "../components/engagementView/EngagementView";
+import Dashboard from "../components/dashboard/Dashboard";
+import UploadPlugin from "../components/uploadPlugin/UploadPluginView";
 import { HashRouter, Route, Switch } from "react-router-dom";
-
-// Updates our react state, as well as localStorage state, to reflect the page
-// we should render
 
 export default function Routes() {
   console.log("Grapl loaded");
