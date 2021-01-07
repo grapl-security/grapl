11549675dae194b222b63374f33e2739d6522243
andrea-grapl <andrea@graplsecurity.com>
2021-01-06 20:21:57 -0700

    remove axios dependency and run build.sh

---
+++ src/js/grapl-cdk/edge_ux_pre_replace/precache-manifest.40dcaaf594eb97d22bfc0aadd4c4144e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2edbd950b8c6f4419e7e0e7c372c3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e9eab1b5c341e9e8475",
+    "url": "/static/css/main.c328d195.chunk.css"
+  },
+  {
+    "revision": "584512b05894a3224813",
+    "url": "/static/js/2.599b7689.chunk.js"
+  },
+  {
+    "revision": "5ae0f76582f211776655eda6978b7713",
+    "url": "/static/js/2.599b7689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9eab1b5c341e9e8475",
+    "url": "/static/js/main.e383285f.chunk.js"
+  },
+  {
+    "revision": "59379d73f145341cdd6f",
+    "url": "/static/js/runtime-main.f4c0327b.js"
+  },
+  {
+    "revision": "803a6c667c909d2f62aac6f8277ef1cd",
+    "url": "/static/media/nodes.803a6c66.jpg"
+  }
+]);
