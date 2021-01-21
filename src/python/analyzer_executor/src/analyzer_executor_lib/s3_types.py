09c189f5dc6d078f996fe274f4758ee54f88c224
wimax-grapl <69007229+wimax-grapl@users.noreply.github.com>
2021-01-21 11:33:12 -0800

    Analyzer Executor: extract the run-local "pull messages" stuff to its own class (will help with fargate) (#551)
    * Move analyzer_executor run_local to use an s3_event_retrieve()
    
    * EventRetriever is a class now
    
    * re-only-local-ize a code block
    
    * extract emit stuff
    
    * remove some unused imports
    
    * oops import pls
    
    * cm comments
    
    * Refactor polling a tad

---
+++ src/python/analyzer_executor/src/analyzer_executor_lib/s3_types.py
@@ -0,0 +1,39 @@
+from typing import Dict, List
+
+from typing_extensions import TypedDict
+
+
+class S3BucketDict(TypedDict):
+    name: str
+    ownerIdentity: Dict
+    arn: str
+
+
+class S3ObjectDict(TypedDict):
+    key: str
+    size: str
+    versionId: str
+
+
+class S3DescriptorDict(TypedDict):
+    """
+    The s3 key provides information about the bucket and
+    object involved in the event. The object key name value
+    is URL encoded.
+    """
+
+    bucket: Dict
+    object: Dict
+
+
+class S3PutRecordDict(TypedDict):
+    # basically, s3:ObjectCreated:Put
+    awsRegion: str
+    eventName: str
+    eventSource: str
+    eventTime: str
+    s3: S3DescriptorDict
+
+
+class SQSMessageBody(TypedDict):
+    Records: List[S3PutRecordDict]
