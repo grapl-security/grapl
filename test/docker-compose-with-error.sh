5e6c40e43addf41af94192923e3a7a090982561d
inickles-grapl <inickles@graplsecurity.com>
2021-01-22 07:17:24 -0800

    Expand on test/docker-compose-with-error.sh usage info and add missing check for required param.

--- test/docker-compose-with-error.sh
+++ test/docker-compose-with-error.sh
@@ -2,7 +2,18 @@
 
 set -e
 
-usage() { echo "Usage: $0 -f docker-compose.yml" 1>&2; exit 1; }
+usage() { 
+    echo 'Usage: $0 -p <project-name> -f docker-compose1.yml [SERVICES]' 1>&2
+    echo
+    echo 'This script calls into `docker-compose up` with the supplied arguments' 1>&2
+    echo 'and checks the exit code of each container upon exit. If any container' 1>&2
+    echo 'exit code is non-zero, this script will exit non-zero.' 1>&2
+    echo
+    echo 'Options:' 1>&2
+    echo '    -f	Path to compose file. Can be passed multiple times.' 1>&2
+    echo '    -p	Project name.' 1>&2
+    exit 1
+}
 
 while getopts "hf:p:" arg; do
     case $arg in
@@ -18,7 +29,7 @@ while getopts "hf:p:" arg; do
     esac
 done
 
-if [ -z "${FILE_ARGS}" ] ; then
+if [ -z "${FILE_ARGS}" ] || [ -z "${p}" ]; then
     usage
 fi
 
