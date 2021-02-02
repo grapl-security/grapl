32c525c50a38b2f87d909d58f28737a045b04bff
Max Wittek <wimax@graplsecurity.com>
2021-02-02 13:26:59 -0800

    Resurrect the nested record stuff

--- src/python/analyzer_executor/src/analyzer_executor_lib/analyzer_executor.py
+++ src/python/analyzer_executor/src/analyzer_executor_lib/analyzer_executor.py
@@ -209,6 +209,9 @@ class AnalyzerExecutor:
         )
 
         for event in events["Records"]:
+            if not self.is_local:
+                event = json.loads(event["body"])["Records"][0]  # type: ignore
+
             data = parse_s3_event(s3, event)
 
             message = json.loads(data)
