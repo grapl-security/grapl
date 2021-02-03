71fca392717aa1d614f2a43ace3dd142ca80d6b8
colin <colin@graplsecurity.com>
2021-02-02 19:52:26 -0800

    Clean up macros

--- src/rust/graph-descriptions/src/lib.rs
+++ src/rust/graph-descriptions/src/lib.rs
@@ -1,6 +1,8 @@
-use crate::graph_description::*;
-use crate::node_property::Property;
+pub use crate::graph_description::*;
+pub use crate::node_property::Property;
 
+// A helper macro to generate `as_*` methods for the NodeProperty wrapper type, and its internall
+// enumeration
 macro_rules! impl_as {
     ($base_t:ty, $as_ident:ident, $r:pat, $p:ident, $to_t:ty, $variant:path) => {
         impl $base_t {
@@ -20,6 +22,7 @@ macro_rules! impl_as {
     };
 }
 
+// A helper macro to generate `From` impl boilerplate.
 macro_rules ! impl_from_for {
     ($into_t:ty, $field:tt, $from_t:ty) => {
         impl From<$from_t> for $into_t
@@ -30,10 +33,16 @@ macro_rules ! impl_from_for {
             }
         }
     };
-
     ($into_t:ty, $field:tt, $head:ty, $($tail:ty),*) => {
         impl_from_for!($into_t, $field, $head);
         impl_from_for!($into_t, $field, $($tail),*);
+    };
+    ($from_t:tt, $to_t:ty) => {
+        impl From<$from_t> for $to_t {
+            fn from($from_t (s): $from_t) -> Self {
+                Self:: $from_t (s)
+            }
+        }
     };
 }
 
@@ -437,14 +446,6 @@ impl std::string::ToString for NodeProperty {
     }
 }
 
-impl_as! {NodeProperty, as_increment_only_uint, u64, node_property::Property::IncrementOnlyUintProp}
-impl_as! {NodeProperty, as_immutable_uint, u64, node_property::Property::ImmutableUintProp}
-impl_as! {NodeProperty, as_decrement_only_uint, u64, node_property::Property::DecrementOnlyUintProp}
-impl_as! {NodeProperty, as_decrement_only_int, i64, node_property::Property::DecrementOnlyIntProp}
-impl_as! {NodeProperty, as_increment_only_int, i64, node_property::Property::IncrementOnlyIntProp}
-impl_as! {NodeProperty, as_immutable_int, i64, node_property::Property::ImmutableIntProp}
-impl_as! {NodeProperty, as_immutable_str, &str, node_property::Property::ImmutableStrProp }
-
 impl Property {
     pub fn into_json(self) -> serde_json::Value {
         match self {
@@ -473,6 +474,80 @@ impl NodeDescription {
     }
 }
 
+impl<T> From<T> for NodeProperty
+where
+    T: Into<Property>,
+{
+    fn from(t: T) -> Self {
+        NodeProperty {
+            property: Some(t.into()),
+        }
+    }
+}
+
+impl From<NodeDescription> for IdentifiedNode {
+    fn from(n: NodeDescription) -> Self {
+        // todo: Remove any properties that were used *only* for identification purposes
+        IdentifiedNode {
+            properties: n.properties,
+            node_key: n.node_key,
+            node_type: n.node_type,
+        }
+    }
+}
+
+impl MergedNode {
+    pub fn from(n: IdentifiedNode, uid: u64) -> Self {
+        Self {
+            uid,
+            properties: n.properties,
+            node_key: n.node_key,
+            node_type: n.node_type,
+        }
+    }
+
+    pub fn merge(&mut self, other: &Self) {
+        debug_assert_eq!(self.node_type, other.node_type);
+        debug_assert_eq!(self.node_key, other.node_key);
+        for (prop_name, prop_value) in other.properties.iter() {
+            match self.properties.get_mut(prop_name) {
+                Some(self_prop) => self_prop.merge(prop_value),
+                None => {
+                    self.properties
+                        .insert(prop_name.clone(), prop_value.clone());
+                }
+            }
+        }
+    }
+
+    pub fn get_node_key(&self) -> &str {
+        self.node_key.as_str()
+    }
+
+    pub fn clone_node_key(&self) -> String {
+        self.node_key.clone()
+    }
+}
+
+impl IdentifiedNode {
+    pub fn into(self, uid: u64) -> MergedNode {
+        MergedNode {
+            uid,
+            properties: self.properties,
+            node_key: self.node_key,
+            node_type: self.node_type,
+        }
+    }
+    pub fn get_node_key(&self) -> &str {
+        self.node_key.as_str()
+    }
+
+    pub fn clone_node_key(&self) -> String {
+        self.node_key.clone()
+    }
+}
+
+// We use separate types here because it makes a lot of the codegen easier
 pub struct IncrementOnlyIntProp(pub i64);
 
 pub struct DecrementOnlyIntProp(pub i64);
@@ -560,118 +635,18 @@ impl_from_for!(
     &i8
 );
 impl_from_for!(ImmutableStrProp, 0, String, &String, &str);
+impl_from_for!(ImmutableStrProp, Property);
+impl_from_for!(IncrementOnlyIntProp, Property);
+impl_from_for!(DecrementOnlyIntProp, Property);
+impl_from_for!(ImmutableIntProp, Property);
+impl_from_for!(IncrementOnlyUintProp, Property);
+impl_from_for!(DecrementOnlyUintProp, Property);
+impl_from_for!(ImmutableUintProp, Property);
 
-impl From<ImmutableStrProp> for Property {
-    fn from(ImmutableStrProp(s): ImmutableStrProp) -> Self {
-        Property::ImmutableStrProp(s)
-    }
-}
-
-impl From<IncrementOnlyIntProp> for Property {
-    fn from(IncrementOnlyIntProp(p): IncrementOnlyIntProp) -> Self {
-        Self::IncrementOnlyIntProp(p)
-    }
-}
-
-impl From<DecrementOnlyIntProp> for Property {
-    fn from(DecrementOnlyIntProp(p): DecrementOnlyIntProp) -> Self {
-        Self::DecrementOnlyIntProp(p)
-    }
-}
-
-impl From<ImmutableIntProp> for Property {
-    fn from(ImmutableIntProp(p): ImmutableIntProp) -> Self {
-        Self::ImmutableIntProp(p)
-    }
-}
-
-impl From<IncrementOnlyUintProp> for Property {
-    fn from(IncrementOnlyUintProp(p): IncrementOnlyUintProp) -> Self {
-        Self::IncrementOnlyUintProp(p)
-    }
-}
-
-impl From<DecrementOnlyUintProp> for Property {
-    fn from(DecrementOnlyUintProp(p): DecrementOnlyUintProp) -> Self {
-        Self::DecrementOnlyUintProp(p)
-    }
-}
-
-impl From<ImmutableUintProp> for Property {
-    fn from(ImmutableUintProp(p): ImmutableUintProp) -> Self {
-        Self::ImmutableUintProp(p)
-    }
-}
-
-impl<T> From<T> for NodeProperty
-where
-    T: Into<Property>,
-{
-    fn from(t: T) -> Self {
-        NodeProperty {
-            property: Some(t.into()),
-        }
-    }
-}
-
-impl From<NodeDescription> for IdentifiedNode {
-    fn from(n: NodeDescription) -> Self {
-        // todo: Remove any properties that were used *only* for identification purposes
-        IdentifiedNode {
-            properties: n.properties,
-            node_key: n.node_key,
-            node_type: n.node_type,
-        }
-    }
-}
-
-impl MergedNode {
-    pub fn from(n: IdentifiedNode, uid: u64) -> Self {
-        Self {
-            uid,
-            properties: n.properties,
-            node_key: n.node_key,
-            node_type: n.node_type,
-        }
-    }
-
-    pub fn merge(&mut self, other: &Self) {
-        debug_assert_eq!(self.node_type, other.node_type);
-        debug_assert_eq!(self.node_key, other.node_key);
-        for (prop_name, prop_value) in other.properties.iter() {
-            match self.properties.get_mut(prop_name) {
-                Some(self_prop) => self_prop.merge(prop_value),
-                None => {
-                    self.properties
-                        .insert(prop_name.clone(), prop_value.clone());
-                }
-            }
-        }
-    }
-
-    pub fn get_node_key(&self) -> &str {
-        self.node_key.as_str()
-    }
-
-    pub fn clone_node_key(&self) -> String {
-        self.node_key.clone()
-    }
-}
-
-impl IdentifiedNode {
-    pub fn into(self, uid: u64) -> MergedNode {
-        MergedNode {
-            uid,
-            properties: self.properties,
-            node_key: self.node_key,
-            node_type: self.node_type,
-        }
-    }
-    pub fn get_node_key(&self) -> &str {
-        self.node_key.as_str()
-    }
-
-    pub fn clone_node_key(&self) -> String {
-        self.node_key.clone()
-    }
-}
+impl_as! {NodeProperty, as_increment_only_uint, u64, node_property::Property::IncrementOnlyUintProp}
+impl_as! {NodeProperty, as_immutable_uint, u64, node_property::Property::ImmutableUintProp}
+impl_as! {NodeProperty, as_decrement_only_uint, u64, node_property::Property::DecrementOnlyUintProp}
+impl_as! {NodeProperty, as_decrement_only_int, i64, node_property::Property::DecrementOnlyIntProp}
+impl_as! {NodeProperty, as_increment_only_int, i64, node_property::Property::IncrementOnlyIntProp}
+impl_as! {NodeProperty, as_immutable_int, i64, node_property::Property::ImmutableIntProp}
+impl_as! {NodeProperty, as_immutable_str, &str, node_property::Property::ImmutableStrProp }
