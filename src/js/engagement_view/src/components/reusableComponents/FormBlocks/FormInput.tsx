55dd44712ad59b2cf7e205999431481173e385ad
hxnyk <hkwokcle@gmail.com>
2021-02-11 19:02:17 -0800

    Add form input component that combines label/field/error

---
+++ src/js/engagement_view/src/components/reusableComponents/FormBlocks/FormInput.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Field, useField } from "formik";
+import "./FormStyles.css";
+
+interface FormInputProps {
+  name: string;
+  label: string;
+  placeholder?: string;
+  inputType?: "text" | "password";
+  error?: React.ReactNode;
+  marginBottom?: boolean;
+}
+
+const FormInput: React.FC<FormInputProps> = ({
+  name,
+  label,
+  placeholder,
+  inputType = "text",
+  marginBottom,
+}) => {
+  const [field, meta, helpers] = useField(name);
+
+  return (
+    <div
+      className="inputContainer"
+      style={marginBottom ? { marginBottom: "1em" } : {}}
+    >
+      <label htmlFor={name}>{label}</label>
+      <Field
+        className="formField"
+        name={name}
+        type={inputType}
+        placeholder={placeholder}
+      />
+
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+export default FormInput;
