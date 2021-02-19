9ea26a2184e5f0bf2e9509729a455e3a63498232
jgrillo <jgrillo@graplsecurity.com>
2021-02-19 13:47:31 -0500

    more lints

--- src/python/grapl_provision/grapl_provision.py
+++ src/python/grapl_provision/grapl_provision.py
@@ -9,9 +9,7 @@ from uuid import uuid4
 import boto3
 import botocore
 import pydgraph
-
 from confluent_kafka.admin import AdminClient, NewTopic
-
 from grapl_analyzerlib.grapl_client import GraphClient, MasterGraphClient
 from grapl_analyzerlib.node_types import (
     EdgeRelationship,
