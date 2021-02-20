9f75cf739e167d5b0581d00320e5c53753fb8d2a
colin-grapl <62314572+colin-grapl@users.noreply.github.com>
2021-02-19 18:47:18 -0800

    Internal 255 model plugin error (#621)
    * Temporary stash
    
    * Add prod proxy path
    
    * Use error code
    
    * Reformat
    
    * Set loglevel back to ERROR
    
    * Remove debug being hardcoded

--- src/js/grapl-cdk/edge_ux_pre_replace/service-worker.js
+++ src/js/grapl-cdk/edge_ux_pre_replace/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3df44f3dbb8682bfc7d1163b22f65661.js"
+  "/precache-manifest.c5b66cfdf9c2503a133ed5d0c194e92c.js"
 );
 
 self.addEventListener('message', (event) => {
