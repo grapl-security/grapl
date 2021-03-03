63a83b5e925488592885f7b6108c8885acd3ae2c
Christopher Maier <chris@graplsecurity.com>
2021-03-03 14:50:01 -0500

    [PR Feedback] Add envvars back to some local scripts
    This is admittedly gross, but absent larger changes, this seems like
    the best way forward from a user-experience standpoint. In the near
    term, we need to consolidate these local grapl scripts into something
    more structured, like `graplctl`.
    
    Signed-off-by: Christopher Maier <chris@graplsecurity.com>

--- etc/local_grapl/bin/upload-osquery-logs.py
+++ etc/local_grapl/bin/upload-osquery-logs.py
@@ -4,6 +4,7 @@ Despite the path, this is *not* tied just to Local Grapl, and can also be used o
 """
 
 import argparse
+import os
 import sys
 from pathlib import Path
 from typing import Callable
@@ -34,6 +35,32 @@ def hack_PATH_to_include_grapl_tests_common() -> Callable:
 
     return upload_osquery_logs
 
+def setup_env(bucket_prefix: str):
+    """Ensures the environment is set up appropriately for interacting
+    with Local Grapl (running inside a Docker Compose network locally)
+    from *outside* that network (i.e., from your workstation).
+
+    """
+    # NOTE: These values are copied from local-grapl.env. It's
+    # unfortunate, yes, but in the interests of a decent
+    # user-experience, we'll eat that pain for now. In the near term,
+    # we should pull this functionality into something like graplctl
+    # with a more formalized way of pointing to a specific Grapl
+    # instance.
+    if bucket_prefix == "local-grapl":
+        kvs = [
+            ("AWS_REGION", "us-east-1"),
+            ("S3_ENDPOINT", "http://localhost:9000"),
+            ("S3_ACCESS_KEY_ID", "THIS_IS_A_FAKE_AWS_ACCESS_KEY_ID"),
+            ("S3_ACCESS_KEY_SECRET", "THIS_IS_A_FAKE_AWS_SECRET_ACCESS_KEY"),
+            ("SQS_ENDPOINT", "http://localhost:4566"),
+            ("SQS_ACCESS_KEY_ID", "THIS_IS_A_FAKE_AWS_ACCESS_KEY_ID"),
+            ("SQS_ACCESS_KEY_SECRET", "THIS_IS_A_FAKE_AWS_SECRET_ACCESS_KEY"),
+        ]
+        for (k, v) in kvs:
+            # fun fact: os.putenv is bad and this is preferred
+            os.environ[k] = v
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Send osquery logs to Grapl")
@@ -54,6 +81,7 @@ if __name__ == "__main__":
     if args.bucket_prefix is None:
         raise Exception("Provide bucket prefix as first argument")
 
+    setup_env(args.bucket_prefix)
     upload_fn = hack_PATH_to_include_grapl_tests_common()
     upload_fn(
         args.bucket_prefix,
--- etc/local_grapl/bin/upload_analyzer_local.sh
+++ etc/local_grapl/bin/upload_analyzer_local.sh
@@ -1,18 +1,32 @@
 # no unset variables please
 set -eu
 
-ANALYZER_UPLOAD_SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
-LOCAL_GRAPL_DIR=$ANALYZER_UPLOAD_SCRIPT_PATH/../
-export AWS_ACCESS_KEY_ID="${S3_ACCESS_KEY_ID}"
-export AWS_SECRET_ACCESS_KEY="${S3_ACCESS_KEY_SECRET}"
-export BUCKET_PREFIX="local-grapl"
+analyzer_upload_script_path=$(dirname "$(readlink -f "$0")")
+local_grapl_dir="${analyzer_upload_script_path}/../"
+
+# Ensures the environment is set up appropriately for interacting with
+# Local Grapl (running inside a Docker Compose network locally) from
+# *outside* that network (i.e., from your workstation).
+#
+# NOTE: These values are copied from local-grapl.env. It's
+# unfortunate, yes, but in the interests of a decent
+# user-experience, we'll eat that pain for now. In the near term,
+# we should pull this functionality into something like graplctl
+# with a more formalized way of pointing to a specific Grapl
+# instance.
+export AWS_ACCESS_KEY_ID="THIS_IS_A_FAKE_AWS_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="THIS_IS_A_FAKE_AWS_SECRET_ACCESS_KEY"
+
+bucket_prefix="local-grapl"
+local_s3_endpoint="http://localhost:9000"
+region="us-east-1"
 
 aws s3 cp \
-    $LOCAL_GRAPL_DIR/suspicious_svchost/main.py \
-    s3://${BUCKET_PREFIX}-analyzers-bucket/analyzers/suspicious_svchost/main.py \
-    --endpoint-url="${S3_ENDPOINT}" --region="${AWS_REGION}"
+    "${local_grapl_dir}/suspicious_svchost/main.py" \
+    "s3://${bucket_prefix}-analyzers-bucket/analyzers/suspicious_svchost/main.py" \
+    --endpoint-url="${local_s3_endpoint}" --region="${region}"
 
 aws s3 cp \
-    $LOCAL_GRAPL_DIR/unique_cmd_parent/main.py \
-    s3://${BUCKET_PREFIX}-analyzers-bucket/analyzers/unique_cmd_parent/main.py \
-    --endpoint-url="${S3_ENDPOINT}" --region="${AWS_REGION}"
+    "${local_grapl_dir}/unique_cmd_parent/main.py" \
+    "s3://${bucket_prefix}-analyzers-bucket/analyzers/unique_cmd_parent/main.py" \
+    --endpoint-url="${local_s3_endpoint}" --region="${region}"
