91702a00ebfeb43c6f832c973f84028eb411436a
andrea-grapl <andrea@graplsecurity.com>
2021-03-07 15:33:32 -0700

    pre-replace files

--- src/js/engagement_view/upload_localhost.sh
+++ src/js/engagement_view/upload_localhost.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ./build/ || exit
 
-export AWS_ACCESS_KEY_ID=minioadmin
-export AWS_SECRET_ACCESS_KEY=minioadmin
+export AWS_ACCESS_KEY_ID="THIS_IS_A_FAKE_AWS_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="THIS_IS_A_FAKE_AWS_SECRET_ACCESS_KEY"
 
 aws s3 sync . s3://local-grapl-engagement-ux-bucket/ \
     --endpoint-url=http://localhost:9000 \
