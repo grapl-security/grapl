69eb908a26a432b7bdbfc636384d1e883b597f39
andrea-grapl <andrea@graplsecurity.com>
2021-03-15 16:24:52 -0600

    WIP - need to figure out how to access dynamodb from graphql endpoint, might be an auth error?

--- src/js/graphql_endpoint/modules/schema.js
+++ src/js/graphql_endpoint/modules/schema.js
@@ -3,7 +3,33 @@ const grpc = require("@grpc/grpc-js");
 const { GraphQLJSONObject } = require("graphql-type-json");
 const { json } = require("express");
 
-const {getNodeType} = require("./getNodeType");
+const AWS = require("aws-sdk");
+
+// ## TODO? DECIDE HOW TO get an instance of dynamodb in graphql??
+const getNodeType = (dynamodb, nodeUid) => {
+	// make a call to dynamo db with the node and get back it's node type
+	AWS.config.update({ region: "us-east-1" });
+
+	// Create the DynamoDB service object
+	const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+	const params = {
+		TableName: dynamodb.Table(os.environ["DEPLOYMENT_NAME"] + "-grapl_schema_table"),
+		Key: {
+			KEY_NAME: { node_type: nodeUid },// is this supposed to be a primary key? 
+		},
+		ProjectionExpression: "uid", //ing that identifies	the attributes that you want
+	};
+
+	// Call DynamoDB to read the item from the table
+	ddb.getItem(params, function (err, data) {
+		if (err) {
+			console.log("Error", err);
+		} else {
+			console.log("Success", data.Item);
+		}
+	});
+};
 
 const {
 	GraphQLObjectType,
