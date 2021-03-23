d2ff4d6c8e4c59b84735a19f8b8dfe941c29bb66
andrea-grapl <andrea@graplsecurity.com>
2021-03-22 20:58:56 -0400

    merge conflicts

--- src/js/graphql_endpoint/modules/root_query.ts
+++ src/js/graphql_endpoint/modules/root_query.ts
@@ -120,6 +120,47 @@ function enrichNode(node: RawNode) {
 	);
 }
 
+const AWS = require("aws-sdk");
+const IS_LOCAL = process.env.IS_LOCAL == "True" || null;
+
+const getDisplayProperty = async (nodeType: string) => {
+	try {
+		const region = process.env.AWS_REGION;
+		AWS.config.update({ region: region });
+
+		const ddb = new AWS.DynamoDB({
+			// new client
+			apiVersion: "2012-08-10",
+			region: IS_LOCAL ? process.env.AWS_REGION : undefined,
+			accessKeyId: IS_LOCAL ? process.env.DYNAMODB_ACCESS_KEY_ID : undefined,
+			secretAccessKey: IS_LOCAL
+				? process.env.DYNAMODB_ACCESS_KEY_SECRET
+				: undefined,
+			endpoint: IS_LOCAL ? process.env.DYNAMODB_ENDPOINT : undefined,
+		});
+
+		const params = {
+			TableName: process.env.GRAPL_DISPLAY_TABLE,
+			Key: {
+				node_type: { S: nodeType }, // get display prop for a given node based on the type
+			},
+			ProjectionExpression: "display_property", // identifies	the attributes that you want to query for
+		};
+
+		const response = await ddb.getItem(params).promise();
+
+		if (response.Item === undefined) {
+			return "dgraph_type";
+		}
+		return response.Item.display_property;
+	} catch (e) {
+		console.error(
+			"Error Querying DynamoDB for dispaly property in schema.js",
+			e
+		);
+	}
+};
+
 const handleLensScope = async (parent: MysteryParentType, args: LensArgs) => {
 	console.log("handleLensScope args: ", args);
 	const dg_client = getDgraphClient();
@@ -201,6 +242,17 @@ const handleLensScope = async (parent: MysteryParentType, args: LensArgs) => {
 					enrichNode(neighbor[predicate]);
 				}
 			}
+		}
+	}
+
+	for (const node of lens_subgraph["scope"]) {
+		const nodeType = node.dgraph_type.filter(filterDefaultDgraphNodeTypes)[0];
+		const displayProperty = await getDisplayProperty(nodeType);
+
+		if (node[displayProperty.S] !== undefined) {
+			node["display"] = nodeType;
+		} else {
+			node["display"] = node[displayProperty.S].toString();
 		}
 	}
 
