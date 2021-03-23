8a594952ffb8cb8e7e211739460e2f7a1af6b599
Christopher Maier <chris@graplsecurity.com>
2021-03-23 12:10:55 -0400

    Add a lot of Pulumi code for lambdas, etc. running locally
    Here, we get the first of our lambda-based services constructed in
    Pulumi.
    
    We have a `Lambda` abstraction, which contains the definition for an
    AWS lambda and an alias, as well as functionality to optionally push
    logs from that lambda to another lambda (our metric-forwarder).
    
    Our `Lambda` is parameterized with a `LambdaArgs` abstraction, which
    contains information about how to set up the function, including the
    code archive, handler endpoint, resource limits, etc. We can also
    subclass this to create target argument types, such as
    `PythonLambdaArgs`, to allow us to centralize information like which
    Python runtime we use.
    
    Building outward, we have a `QueueDrivenLambda`, which takes an
    instance of our `Lambda` and couples it with resources that allow it
    to be triggered by output from an SQS queue.
    
    This, in turn, is an important component of our `Service` abstraction,
    which mirrors the same abstraction from our CDK code. It consists of a
    series of SQS queues that trigger a "handler" lambda, along with a
    backup "retry handler" lambda.
    
    Finally, we come to concrete instances of our Grapl services:
    `EngagementCreator` and `MetricForwarder`. These are built on top of
    the previous abstractions, along with whatever associated AWS
    resources are needed for their specific operation.
    
    Currently these are set up for running in Local Grapl (notably, they
    need a reference to a VPC to achieve parity with their CDK
    counterparts). That will come in a follow-up PR that allows Pulumi to
    read CDK stack information to gradually move real deployments over to
    being fully managed by Pulumi a resource at a time.
    
    Signed-off-by: Christopher Maier <chris@graplsecurity.com>

---
+++ pulumi/infra/queue_driven_lambda.py
@@ -0,0 +1,66 @@
+import json
+from typing import Optional
+
+import pulumi_aws as aws
+from infra.lambda_ import Lambda, LambdaArgs
+from infra.metric_forwarder import MetricForwarder
+
+import pulumi
+
+
+class QueueDrivenLambda(pulumi.ComponentResource):
+    """ A lambda function that is triggered by an SQS queue. """
+
+    def __init__(
+        self,
+        name: str,
+        queue: aws.sqs.Queue,
+        args: LambdaArgs,
+        forwarder: Optional[MetricForwarder] = None,
+        opts: Optional[pulumi.ResourceOptions] = None,
+    ) -> None:
+
+        super().__init__("grapl:QueueDrivenLambda", name, None, opts)
+
+        self.function = Lambda(
+            name,
+            args=args,
+            forwarder=forwarder,
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
+        self.policy = aws.iam.RolePolicy(
+            f"{name}-consumes-from-queue",
+            role=args.execution_role.name,
+            policy=queue.arn.apply(
+                lambda arn: json.dumps(
+                    {
+                        "Version": "2012-10-17",
+                        "Statement": [
+                            {
+                                "Effect": "Allow",
+                                "Action": [
+                                    "sqs:ChangeMessageVisibility",
+                                    "sqs:DeleteMessage",
+                                    "sqs:GetQueueAttributes",
+                                    "sqs:GetQueueUrl",
+                                    "sqs:ReceiveMessage",
+                                ],
+                                "Resource": arn,
+                            }
+                        ],
+                    }
+                )
+            ),
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
+        self.event_source_mapping = aws.lambda_.EventSourceMapping(
+            f"queue-triggers-{name}",
+            event_source_arn=queue.arn,
+            function_name=self.function.function.arn,
+            batch_size=10,  # Default value for SQS queues
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
+        self.register_outputs({})
---
+++ pulumi/infra/service.py
@@ -0,0 +1,76 @@
+import dataclasses
+from typing import Mapping, Optional, Union
+
+from infra.lambda_ import LambdaExecutionRole, PythonLambdaArgs
+from infra.metric_forwarder import MetricForwarder
+from infra.queue_driven_lambda import QueueDrivenLambda
+from infra.service_queue import ServiceQueue
+
+import pulumi
+
+
+# TODO: Needs a VPC
+class Service(pulumi.ComponentResource):
+    def __init__(
+        self,
+        name: str,
+        forwarder: MetricForwarder,
+        lambda_description: str,
+        lambda_handler_fn: str,
+        lambda_code_path: str,
+        env: Mapping[str, Union[str, pulumi.Output[str]]],
+        opts: Optional[pulumi.ResourceOptions] = None,
+    ) -> None:
+        super().__init__("grapl:Service", name, None, opts)
+
+        self.name = name
+
+        self.queue = ServiceQueue(name, opts=pulumi.ResourceOptions(parent=self))
+
+        # Handlers *and* retry handlers both use this same role
+        self.role = LambdaExecutionRole(
+            name,
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
+        handler_memory = 128  # MB
+        handler_timeout = 45  # Seconds
+
+        args = PythonLambdaArgs(
+            execution_role=self.role,
+            description=lambda_description,
+            handler=lambda_handler_fn,
+            code_path=lambda_code_path,
+            memory_size=handler_memory,
+            timeout=handler_timeout,
+            env={**env, "SOURCE_QUEUE_URL": self.queue.queue.id, "IS_RETRY": "False"},
+        )
+
+        self.handler = QueueDrivenLambda(
+            f"{name}-Handler",
+            self.queue.queue,
+            args=args,
+            forwarder=forwarder,
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
+        # Note: The retry handler gets twice the memory and twice the
+        # timeout as the "normal" handler.
+        self.retry_handler = QueueDrivenLambda(
+            f"{name}-RetryHandler",
+            self.queue.retry_queue,
+            args=dataclasses.replace(
+                args,
+                memory_size=handler_memory * 2,
+                timeout=handler_timeout * 2,
+                env={
+                    **env,
+                    "SOURCE_QUEUE_URL": self.queue.retry_queue.id,
+                    "IS_RETRY": "True",
+                },
+            ),
+            forwarder=forwarder,
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
+        self.register_outputs({})
