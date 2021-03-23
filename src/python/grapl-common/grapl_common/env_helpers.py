116871266cac77da1abe837d28429443ca989abc
Christopher Maier <chris@graplsecurity.com>
2021-03-23 11:15:04 -0400

    Update Python env_helpers to work in Localstack
    Absent further tweaking of our Local Grapl setup (perhaps with some
    proxies and customized DNS handling?) we need a way for lambda
    functions running in Localstack to be able to connect to the AWS
    services of Localstack, rather than trying to reach out to the actual
    AWS.
    
    It's not _ideal_, but it's not the worst thing either.
    
    Signed-off-by: Christopher Maier <chris@graplsecurity.com>

--- src/python/grapl-common/grapl_common/env_helpers.py
+++ src/python/grapl-common/grapl_common/env_helpers.py
@@ -55,7 +55,9 @@ def _client_get(client_create_fn: Callable[..., Any], params: ClientGetParams) -
 
     # Unlike Rust FromEnv, we rely on boto3's built in region handling.
 
-    if all((endpoint_url, access_key_id, access_key_secret)):
+    if _running_in_localstack():
+        return _localstack_client(client_create_fn, params)
+    elif all((endpoint_url, access_key_id, access_key_secret)):
         # Local, all are passed in from docker-compose.yml
         logging.info(f"Creating a local client for {which_service}")
         assert (
@@ -89,6 +91,43 @@ def _client_get(client_create_fn: Callable[..., Any], params: ClientGetParams) -
         raise FromEnvException(
             f"You specified access key but not endpoint for {params.boto3_client_name}?"
         )
+
+
+def _running_in_localstack() -> bool:
+    """Detects whether or not code is running in Localstack.
+
+    When running lambda functions in Localstack, the
+    `LOCALSTACK_HOSTNAME` environment variable will be set, allowing
+    us to compose an appropriate endpoint at which the lambda can
+    interact with other Localstack-hosted AWS services.
+
+    Needless to say, this should not be present in the environment of
+    a lambda actually running in AWS.
+
+    """
+    return "LOCALSTACK_HOSTNAME" in os.environ
+
+
+def _localstack_client(
+    client_create_fn: Callable[..., Any], params: ClientGetParams
+) -> Any:
+    """Create a boto3 client for interacting with AWS services running in
+    Localstack from a lambda function also running in Localstack.
+
+
+    Localstack provides LOCALSTACK_HOSTNAME and EDGE_PORT for creating
+    a proper endpoint value. In addition, appropriate values for
+    AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_REGION are also
+    provided.
+
+    See the _running_in_localstack function, as well.
+    """
+    service = params.boto3_client_name
+    logging.info("Creating a {service} client for Localstack!")
+    return client_create_fn(
+        service,
+        endpoint_url=f"http://{os.environ['LOCALSTACK_HOSTNAME']}:{os.environ['EDGE_PORT']}",
+    )
 
 
 _SQSParams = ClientGetParams(
