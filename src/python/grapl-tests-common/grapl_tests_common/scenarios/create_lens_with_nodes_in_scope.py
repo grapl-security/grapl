75bb31be3c4fa0cd7063b460f64e6d4c5deda37f
Max Wittek <wimax@graplsecurity.com>
2021-03-23 18:03:01 -0700

    fix build errors

--- src/python/grapl-tests-common/grapl_tests_common/scenarios/create_lens_with_nodes_in_scope.py
+++ src/python/grapl-tests-common/grapl_tests_common/scenarios/create_lens_with_nodes_in_scope.py
@@ -1,13 +1,21 @@
-import hypothesis.strategies as st
-
 import unittest
+
 import hypothesis
+import hypothesis.strategies as st
 from grapl_analyzerlib.grapl_client import GraphClient
-from grapl_analyzerlib.prelude import LensView, AssetView
 from grapl_analyzerlib.node_types import PropType
-from grapl_analyzerlib.test_utils.dgraph_utils import create_edge
-from grapl_analyzerlib.test_utils.strategies.asset_view_strategy import get_or_create_asset, asset_props, AssetProps
-from grapl_analyzerlib.test_utils.dgraph_utils import node_key_for_test, upsert
+from grapl_analyzerlib.prelude import AssetView, LensView
+from grapl_analyzerlib.test_utils.dgraph_utils import (
+    create_edge,
+    node_key_for_test,
+    upsert,
+)
+from grapl_analyzerlib.test_utils.strategies.asset_view_strategy import (
+    AssetProps,
+    asset_props,
+    get_or_create_asset,
+)
+
 
 @hypothesis.given(asset_props())
 def create_lens_with_nodes_in_scope(
@@ -15,7 +23,7 @@ def create_lens_with_nodes_in_scope(
     graph_client: GraphClient,
     asset_prop: AssetProps,
 ) -> None:
-    lens_name="whatever"  # TODO
+    lens_name = "whatever"  # TODO
     lens = LensView.get_or_create(
         gclient=graph_client,
         lens_name=lens_name,
