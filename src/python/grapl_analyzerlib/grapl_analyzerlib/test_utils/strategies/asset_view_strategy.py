a2476f2a0a0a90dc8fea6196587aaa78fc3a97ca
Max Wittek <wimax@graplsecurity.com>
2021-03-28 21:00:02 -0700

    IT WORKS now lets make it better

--- src/python/grapl_analyzerlib/grapl_analyzerlib/test_utils/strategies/asset_view_strategy.py
+++ src/python/grapl_analyzerlib/grapl_analyzerlib/test_utils/strategies/asset_view_strategy.py
@@ -1,12 +1,11 @@
 import unittest
 from typing import cast, Dict, Any, NewType
-from uuid import UUID
 
+import uuid
 import hypothesis.strategies as st
 
 from grapl_analyzerlib.grapl_client import GraphClient
 from grapl_analyzerlib.nodes.asset import AssetView
-from grapl_analyzerlib.node_types import PropType
 from grapl_analyzerlib.test_utils.dgraph_utils import node_key_for_test, upsert
 from grapl_analyzerlib.test_utils.strategies.misc import text_dgraph_compat
 
@@ -18,7 +17,7 @@ def asset_props_strategy() -> st.SearchStrategy[AssetProps]:
         AssetProps,
         st.builds(
             dict,
-            node_key=st.uuids(),
+            node_key=st.just("replaced in get_or_create_asset"),
             hostname=text_dgraph_compat(),
         ),
     )
@@ -27,7 +26,9 @@ def asset_props_strategy() -> st.SearchStrategy[AssetProps]:
 def get_or_create_asset(
     test: unittest.TestCase, graph_client: GraphClient, node_props: AssetProps
 ) -> AssetView:
-    node_key = node_key_for_test(test, str(node_props["node_key"]))
+    # Introduce randomness here, because otherwise Hypothesis would generate
+    # key collisions at the @given stage
+    node_key = node_key_for_test(test, str(uuid.uuid4()))
     return cast(
         AssetView, upsert(graph_client, "Asset", AssetView, node_key, node_props)
     )
