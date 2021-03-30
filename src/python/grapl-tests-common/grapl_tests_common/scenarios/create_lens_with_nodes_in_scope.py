e2e17b5409d15672b4cca13e8f989857467bcdd0
Max Wittek <wimax@graplsecurity.com>
2021-03-30 12:57:36 -0700

    oh let me refactor

--- src/python/grapl-tests-common/grapl_tests_common/scenarios/create_lens_with_nodes_in_scope.py
+++ src/python/grapl-tests-common/grapl_tests_common/scenarios/create_lens_with_nodes_in_scope.py
@@ -4,7 +4,7 @@ import uuid
 from grapl_analyzerlib.grapl_client import GraphClient
 from grapl_analyzerlib.node_types import PropType
 from grapl_analyzerlib.prelude import AssetView, LensView
-from grapl_analyzerlib.test_utils.dgraph_utils import create_edge
+from grapl_analyzerlib.test_utils.dgraph_utils import create_edge, node_key_for_test
 from grapl_analyzerlib.test_utils.strategies.asset_view_strategy import (
     AssetProps,
     get_or_create_asset,
@@ -16,8 +16,10 @@ def create_lens_with_nodes_in_scope(
     graph_client: GraphClient,
     asset_props: AssetProps,
 ) -> LensView:
-    # Give each test run its own lens
-    lens_name = f"{test.id()}-{uuid.uuid4()}"
+    """
+    the Lens here might be better refactored like asset_props_strategy/get_or_create_asset
+    """
+    lens_name = node_key_for_test(test)  #  just something tied to this test + random
     lens = LensView.get_or_create(
         gclient=graph_client,
         lens_name=lens_name,
