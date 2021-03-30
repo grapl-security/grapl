026a8de2da5cff2e56b4ea1c1d94bd7edb378f1e
ajm <73959780+ajm-grapl@users.noreply.github.com>
2021-03-29 20:58:39 -0400

    remove duplicate code from plugins frontend test

--- test/cypress/integration/plugins.js
+++ test/cypress/integration/plugins.js
@@ -1,58 +1,26 @@
-describe("sanity check", () => {
-	it("passes", () => {
-		expect(true).to.equal(true);
-	});
-});
+describe("with authentication", () => {
+    before(() => {
+        cy.clearCookies();
+        Cypress.Cookies.preserveOnce('grapl_jwt');
+        cy.login_flow();
+    });
 
-describe("application loads", () => {
-	it("visits the front page", () => {
-		cy.visit("/");
-	});
-});
+    after(() => {
+        cy.clearCookies();
+    });
 
-describe("login form test", () => {
-	it("allows the user to log in with a valid username and password", () => {
-		cy.visit("/");
-		cy.contains(/login/i).click();
-		cy.location("href").should("include", "/login");
+    beforeEach(() => {
+        Cypress.Cookies.preserveOnce('grapl_jwt');
+    });
 
-		cy.get("[placeholder='Username']").type("grapluser"); // known good demo password
-		cy.get("[placeholder='Password']").type("graplpassword"); // known good demo password
-		cy.contains(/submit/i).click();
-	});
-});
-
-describe("auth validation", () => {
-	beforeEach(() => {
-		cy.request({
-			url: `http://localhost:1234/auth/login`,
-			method: "POST",
-			credentials: "include",
-			headers: new Headers({
-				"Content-Type": "application/json",
-			}),
-			body: JSON.stringify({
-				username: "grapluser",
-				password: "graplpassword",
-			}),
-		}).then((body) => {
-			const grapl_jwt = { user: { authenticationData: { token: body.token } } };
-			window.localStorage.setItem("grapl_jwt", JSON.stringify(grapl_jwt));
-		});
-
-		cy.contains("login").should("not.exist");
-		it("checks we're authenticated on model-plugin page", () => {
-			cy.get("grapl_jwt").should("exist");
-		});
-
-		it("uploads a model plugin and validates rendering in plugin table ", () => {
-			cy.url().should("include", "plugins");
-			cy.contains(/plugin/i).click();
-			const filePath = "../fixtures/sample_plugins.zip";
-			cy.get('input[type="file"]').attachFile(filePath);
-			cy.get(".submitBtn").click();
-			cy.contains("Successfully").should("exist");
-			cy.contains("grapl_plug_ins").should("exist");
-		});
-	});
+    it("uploads a model plugin and validates rendering in plugin table ", () => {
+        cy.contains(/plugin/i).click();
+        cy.url().should("include", "plugins");
+        cy.contains(/plugin/i).click();
+        const filePath = "../fixtures/sample_plugins.zip";
+        cy.get('input[type="file"]').attachFile(filePath);
+        cy.get(".submitBtn").click();
+        cy.contains("Successfully").should("exist");
+        cy.contains("grapl_plug_ins").should("exist");
+    });
 });
