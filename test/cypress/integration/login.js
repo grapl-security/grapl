d6ef26c009c1679f9af66722cd71a1f7e0872eea
ajm <73959780+ajm-grapl@users.noreply.github.com>
2021-03-31 15:45:03 -0400

    Add comments per review

--- test/cypress/integration/login.js
+++ test/cypress/integration/login.js
@@ -7,6 +7,12 @@ describe("without authentication", () => {
 });
 
 describe("with authentication", () => {
+    // You will notice:
+    //   Cypress.Cookies.preserveOnce('grapl_jwt');
+    // is duplicated across `before` and `beforeEach`.
+    // It's redundant, but it works.
+    // We can experiment with removing it later.
+
     before(() => {
         cy.clearCookies();
         Cypress.Cookies.preserveOnce('grapl_jwt');
@@ -20,6 +26,10 @@ describe("with authentication", () => {
     beforeEach(() => {
         Cypress.Cookies.preserveOnce('grapl_jwt');
     });
+
+    // the following three (seemingly no-op) tests demonstrate that an
+    // authentication cookie was set on the browser in the `before` hook,
+    // and that it is successfully preserved across separate tests:
 
     it("(1) checks to make sure grapl_jwt was set", () => {
         cy.getCookie("grapl_jwt").should("exist");
