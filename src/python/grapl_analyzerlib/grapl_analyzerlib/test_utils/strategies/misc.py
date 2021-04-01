c3fc9d334dd1a19c638bfab5703bc048af3b8127
wimax-grapl <69007229+wimax-grapl@users.noreply.github.com>
2021-04-01 13:39:56 -0700

    Minor change to hypothesis (#718)

--- src/python/grapl_analyzerlib/grapl_analyzerlib/test_utils/strategies/misc.py
+++ src/python/grapl_analyzerlib/grapl_analyzerlib/test_utils/strategies/misc.py
@@ -9,9 +9,9 @@ T = TypeVar("T", bound=str)
 
 @st.composite
 def text_dgraph_compat(
-    draw: Callable[[st.SearchStrategy[str]], str], max_size: int = 128
+    draw: Callable[[st.SearchStrategy[str]], str], max_size: int = 64
 ) -> str:
-    base_text = draw(st.text(min_size=3, max_size=max_size))
+    base_text = draw(st.text(min_size=4, max_size=max_size))
     # Don't fuck with newlines due to a dgraph bug
     # https://github.com/dgraph-io/dgraph/issues/4694
     assume(len(base_text) > 3)
