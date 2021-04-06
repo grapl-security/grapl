02ac708439b75f93a7cc19b640c5b4d83fadadfe
Max Wittek <wimax@graplsecurity.com>
2021-04-06 14:25:01 -0700

    make format

--- src/python/graphql_endpoint_tests/test_graphql_endpoint.py
+++ src/python/graphql_endpoint_tests/test_graphql_endpoint.py
@@ -53,7 +53,7 @@ class TestGraphqlEndpoint(TestCase):
         # Ensure we strip the Entity and Base types
         assert gql_lens["scope"][0]["dgraph_type"] == ["Asset"]
         assert gql_lens["scope"][0]["hostname"] == asset_props["hostname"]
-    
+
     def test_describe_asset_type(
         self,
     ) -> None:
@@ -61,14 +61,8 @@ class TestGraphqlEndpoint(TestCase):
 
         result = _query_graphql_about_type("Asset", graphql_client)
         assert result["name"] == "Asset"
-        
-        uid_looks_like = {
-            "name": "uid",
-            "type": {
-                "name": "Int",
-                "kind": "SCALAR"
-            }
-        }
+
+        uid_looks_like = {"name": "uid", "type": {"name": "Int", "kind": "SCALAR"}}
         assert any(x for x in result["fields"] if x == uid_looks_like)
 
 
@@ -90,9 +84,8 @@ def _query_graphql_endpoint_for_lenses(
     resp = gql_client.query(query)
     return resp["lenses"]
 
-def _query_graphql_about_type(
-    type_name: str, graphql_client: GraphqlEndpointClient
-):
+
+def _query_graphql_about_type(type_name: str, graphql_client: GraphqlEndpointClient):
     query = """
     query QueryGraphqlAboutType($type_name: String!) {
     __type(name: $type_name) {
