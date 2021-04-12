452831161e20c8a6ef84e75436fc733e9bf1e1bc
Max Wittek <wimax@graplsecurity.com>
2021-04-12 13:29:49 -0700

    fix a test

--- src/python/graphql_endpoint_tests/test_graphql_endpoint.py
+++ src/python/graphql_endpoint_tests/test_graphql_endpoint.py
@@ -57,7 +57,7 @@ class TestGraphqlEndpoint(TestCase):
         assert asset_node["dgraph_type"] == ["Asset"]
         assert asset_node["hostname"] == asset_props["hostname"]
         # Ensure we send along the Display
-        assert gql_lens["scope"][0]["display"] == ["hostname"]
+        assert asset_node["display"] == asset_props["hostname"] 
 
     def test_describe_asset_type(
         self,
