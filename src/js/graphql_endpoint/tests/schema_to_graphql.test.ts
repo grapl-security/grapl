2846debabc28589b2cd3813abd8f03bff9eb4727
Max Wittek <wimax@graplsecurity.com>
2021-04-12 14:36:17 -0700

    basic Jest setup

---
+++ src/js/graphql_endpoint/tests/schema_to_graphql.test.ts
@@ -0,0 +1,15 @@
+import {
+    GraphQLBoolean,
+    GraphQLFieldConfigMap,
+    GraphQLInt,
+    GraphQLList,
+    GraphQLObjectType,
+    GraphQLOutputType,
+    GraphQLString,
+    GraphQLUnionType,
+} from "graphql";
+import { Schema, SchemaProperty } from "../modules/schema_client";
+
+test("schema_to_graphql", () => {
+
+});
