99b66ce6f8b70f7904d23a8f2fc5cce9db47dc2a
Max Wittek <wimax@graplsecurity.com>
2021-04-23 14:25:36 -0700

    Refactor tests a bit to have a shared.py

--- src/python/graplctl/tests/test_graplctl.py
+++ src/python/graplctl/tests/test_graplctl.py
@@ -6,27 +6,17 @@ from unittest.mock import ANY, Mock, patch
 from click.testing import CliRunner
 from graplctl import __version__, cli
 from tests.fake_uploads.fake_analyzer import main as fake_analyzer_main_py
+from tests.shared import invoke_with_default_args, patch_boto3_session
 
 
 def test_version() -> None:
     assert __version__ == "0.1.0"
 
 
-DEFAULT_ARGS = [
-    "--grapl-region",
-    "us-west-2",
-    "--grapl-deployment-name",
-    "fake-deployment",
-    "--grapl-version",
-    "fake-version",
-]
-
-
 def test_queues_ls() -> None:
-    runner = CliRunner()
-    with _patch_boto3_session() as mock_session:
-        _return_fake_queues(mock_session)
-        result = runner.invoke(cli.main, [*DEFAULT_ARGS, "queues", "ls"])
+    with patch_boto3_session() as mock_session:
+        mock_session.return_fake_queues()
+        result = invoke_with_default_args(["queues", "ls"])
     assert result.exit_code == 0
     assert (
         result.output
@@ -34,14 +24,14 @@ def test_queues_ls() -> None:
     )
 
 
+# TODO: Add a `test_queues_redrive`
+
+
 def test_dev_upload_analyzer__path_is_wrong() -> None:
-    runner = CliRunner()
     main_py_path = "some_inexistent_dir/main.py"
-    with _patch_boto3_session() as mock_session:
-        result = runner.invoke(
-            cli.main,
+    with patch_boto3_session() as mock_session:
+        result = invoke_with_default_args(
             [
-                *DEFAULT_ARGS,
                 "upload",
                 "analyzer",
                 "--analyzer_main_py",
@@ -53,22 +43,18 @@ def test_dev_upload_analyzer__path_is_wrong() -> None:
 
 
 def test_dev_upload_analyzer__calls_s3() -> None:
-    runner = CliRunner()
     main_py_path = fake_analyzer_main_py.__file__
-
-    with _patch_boto3_session() as mock_session:
-        _mock_out_s3(mock_session)
-        result = runner.invoke(
-            cli.main,
+    with patch_boto3_session() as mock_session:
+        result = invoke_with_default_args(
             [
-                *DEFAULT_ARGS,
                 "upload",
                 "analyzer",
                 "--analyzer_main_py",
                 str(main_py_path),
             ],
         )
-    mock_s3_client = mock_session.client.return_value
+
+    mock_s3_client = mock_session.session.client.return_value
     mock_s3_client.put_object.assert_called_with(
         Body=ANY,  # technically, you could read the contents of `fake_analyzer_main` but whatever
         Bucket="fake-deployment-analyzers-bucket",
@@ -76,35 +62,3 @@ def test_dev_upload_analyzer__calls_s3() -> None:
     )
     assert result.exit_code == 0
     assert result.output == "Uploaded analyzer 'fake_analyzer'\n"
-
-
-# TODO: Add a `test_queues_redrive`
-
-
-def _return_fake_queues(mock_session: Mock) -> None:
-    sqs_client = mock_session.client.return_value
-    sqs_client.list_queues.return_value = {
-        "QueueUrls": [
-            "http://queue1",
-            "http://queue1-retry-queue",
-            "http://queue1-dead-letter-queue",
-            "http://queue2",
-            "http://queue2-retry-queue",
-            "http://queue2-dead-letter-queue",
-        ]
-    }
-
-
-def _mock_out_s3(mock_session: Mock) -> None:
-    s3_client = mock_session.client.return_value
-    s3_client.put_object.return_value = None
-
-
-@contextmanager
-def _patch_boto3_session() -> ContextManager[Mock]:
-    with patch.object(
-        cli.boto3, cli.boto3.Session.__name__, spec_set=cli.boto3.Session
-    ) as p_session_cls:
-        session = Mock(name="Session instance")
-        p_session_cls.return_value = session
-        yield session
