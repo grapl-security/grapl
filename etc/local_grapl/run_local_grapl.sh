f026889885d19076da83ecbb20efa842770e3b29
jgrillo <jgrillo@graplsecurity.com>
2021-04-26 18:24:05 +0100

    remove unused scripts

--- etc/local_grapl/run_local_grapl.sh
+++
@@ -1,2 +0,0 @@
-docker-compose up --build  -d
-python3 ./grapl_provision.py
--- etc/local_grapl/run_local_services.sh
+++
@@ -1,5 +0,0 @@
-docker run -d -p 9324:9324 vsouza/sqs-local;
-docker run -p 9000:9000 minio/minio server /data &
-
-docker run --rm -p 8000:8000 -p 8080:8080 -p 9080:9080 dgraph/standalone:latest &
-docker run --rm -p 8001:8000 -p 8081:8080 -p 9081:9080 dgraph/standalone:latest &
