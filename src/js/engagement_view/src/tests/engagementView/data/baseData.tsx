433c2b7394018409b563b3c66c97e3ad276dbaf3
andrea-grapl <andrea@graplsecurity.com>
2021-04-28 19:53:27 -0600

    mapNodeProps for node display table

--- src/js/engagement_view/src/tests/engagementView/data/baseData.tsx
+++ src/js/engagement_view/src/tests/engagementView/data/baseData.tsx
@@ -1,19 +1,75 @@
 export const baseNodeData = {
-    uid: 50133,
-    dgraph_type: ["Risk"],
-    node_key: "Suspicious svchost",
-    analyzer_name: "Suspicious svchost",
-    risk_score: 75,
+	uid: 50133,
+	dgraph_type: ["Risk"],
+	node_key: "Suspicious svchost",
+	analyzer_name: "Suspicious svchost",
+	risk_score: 75,
 };
 
-export const linkData = 
-    { source: 40, name: "asset_processes", target: 171 }
+export const linkData = { source: 40, name: "asset_processes", target: 171 };
 
 export const processNode = {
-    uid: 207,
-    node_key: '739c609a-05ac-4bed-949c-387efb06114a',
-    dgraph_type: [ 'Process' ],
-    display: null,
-    process_name: 'svchost.exe',
-    process_id: 6132
+	uid: 207,
+	node_key: "739c609a-05ac-4bed-949c-387efb06114a",
+	dgraph_type: ["Process"],
+	display: null,
+	process_name: "svchost.exe",
+	process_id: 6132,
+};
+
+export const rawNode = {
+	analyzerNames: "Rare Parent of cmd.exe, Suspicious svchost",
+	asset_ip: null,
+	asset_processes: undefined,
+	dgraph_type: ["Asset"],
+	display: "DESKTOP-FVSHABR",
+	files_on_asset: null,
+	fx: 7.039093729879062,
+	fy: -0.4117743026991536,
+	hostname: "DESKTOP-FVSHABR",
+	id: 10057,
+	index: 0,
+	last_index_time: null,
+	name: 10057,
+	nodeLabel: "DESKTOP-FVSHABR",
+	nodeType: "Asset",
+	node_key: "e52e45e0-7ebf-4539-a66c-8b580441d101",
+	risk_score: 85,
+	risks: undefined,
+	uid: 10057,
+	vx: 0,
+	vy: 0,
+	x: 7.039093729879062,
+	y: -0.4117743026991536,
+	__indexColor: "#ec0001",
+};
+
+export const displayNodeData = {
+    analyzerNames: "Rare Parent of cmd.exe, Suspicious svchost",
+    hostname: "DESKTOP-FVSHABR",
+    node_key: "e52e45e0-7ebf-4539-a66c-8b580441d101",
+    risk_score: 85,    
 }
+
+export const hidden = new Set([
+    "id",
+    "dgraph.type",
+    "dgraph_type",
+    "__indexColor",
+    "risks",
+    "uid",
+    "scope",
+    "name",
+    "nodeType",
+    "nodeLabel",
+    "x",
+    "y",
+    "index",
+    "vy",
+    "vx",
+    "fx",
+    "fy",
+    "links",
+    "neighbors",
+    "display",
+]);
--- src/js/engagement_view/src/tests/graphDisplay.test.tsx
+++ src/js/engagement_view/src/tests/graphDisplay.test.tsx
@@ -3,7 +3,7 @@ import {
     vizGraphFromLensScope,
 } from "components/graphDisplay/graphLayout/vizGraphFromLensScope";
 
-import { baseNodeData, processNode } from "./engagementView/data/baseData";
+import { baseNodeData, processNode, rawNode, displayNodeData, hidden } from "./engagementView/data/baseData";
 
 import {
     vizGraphData,
@@ -22,10 +22,48 @@ import {
     getNodeLabel,
 } from "components/graphDisplay/graphLayout/labels";
 
+import {
+    nodeFillColor,
+    riskOutline
+} from "components/graphDisplay/graphVizualization/nodeColoring";
+
+import {mapNodeProps} from "components/graphDisplay/graphLayout/mapNodeProps"
+
+test("map display node properties for node table", () => {
+    const displayNode = {} as any;
+
+    mapNodeProps(rawNode as any, (propName: string) => {
+        const prop = rawNode[propName];
+
+        if (!hidden.has(propName)) {
+            if (prop) {
+                if (propName.includes("_time")) {
+                    try {
+                        displayNode[propName] = new Date(prop).toLocaleString();
+                    } catch (e) {
+                        displayNode[propName] = prop;
+                    }
+                } else {
+                    displayNode[propName] = prop;
+                }
+            }
+        }
+    })
+
+    expect(displayNode).toMatchObject(displayNodeData)
+})
+
 // graphQLAdjacencyMatrix
 test("get node type from dGraph type", () => {
     expect(getNodeType(baseNodeData)).toBe("Risk");
 });
+
+test("create graph from lens scope for graph display vizualization for react-force-graph", () => {
+    expect(vizGraphFromLensScope(vizGraphData as any)).toMatchObject(
+        vizGraphReturnData
+    );
+});
+
 
 test("merge graph data HAS changed and graph WILL be updated", () => {
     expect(
@@ -43,6 +81,8 @@ test("link label for children", () => {
     expect(getLinkLabel("children")).toBe("executed")
 } )
 
+// Test graph styling for high risk process node
+// Assuming if process works, formatting for other node types also works
 test("link label for processes", () => {
     expect(getLinkLabel("asset_processes")).toBe("asset_processes")
 } )
@@ -50,3 +90,11 @@ test("link label for processes", () => {
 test("get node label", () => {
     expect(getNodeLabel("Process", processNode as any)).toBe("Process")
 })
+
+test("get nodeFillColor for process", () => {
+    expect(nodeFillColor("Process")).toBe("rgba(31, 120, 180, .8)")
+} )
+
+test("get high riskOutline", () => {
+    expect(riskOutline(80)).toBe("#E50F14")
+} )
