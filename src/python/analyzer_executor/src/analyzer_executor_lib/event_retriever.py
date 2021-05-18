3fbf392e1a89163958e8e4aa025f73b7d14be2ce
jgrillo <jgrillo@graplsecurity.com>
2021-05-18 17:50:46 +0100

    attempt to fix typechecking

--- src/python/analyzer_executor/src/analyzer_executor_lib/event_retriever.py
+++ src/python/analyzer_executor/src/analyzer_executor_lib/event_retriever.py
@@ -4,8 +4,8 @@ import time
 import traceback
 from typing import TYPE_CHECKING, Iterator
 
-import boto3
-import botocore.exceptions
+import boto3  # type: ignore
+import botocore.exceptions  # type: ignore
 from analyzer_executor_lib.sqs_types import SQSMessage
 from grapl_common.env_helpers import SQSClientFactory
 from grapl_common.grapl_logger import get_module_grapl_logger
