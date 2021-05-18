84d06e4ccd0fc94f1bfcfef709db7eb15b63e075
andrea <andrea@penguin>
2021-05-17 22:35:55 -0600

    WIP don't touch this

--- src/rust/grapl-router-api/src/lib.rs
+++ src/rust/grapl-router-api/src/lib.rs
@@ -1,5 +1,24 @@
 pub mod model_plugin_deployer_router;
 
+// use actix_web::{Request, Response};
+
+
+// let mut request = Request::builder();
+// request.uri("https://www.rust-lang.org/")
+   //    .header("User-Agent", "my-awesome-agent/1.0");
+
+// if needs_awesome_header() {
+   // request.header("Awesome", "yes");
+// }
+
+// let response = send(request.body(()).unwrap());
+
+// fn send(req: Request<()>) -> Response<()> {
+    // ...
+// }
+
+
+
 
 #[cfg(test)]
 mod tests {
--- src/rust/grapl-router-api/src/model_plugin_deployer_router/deploy.rs
+++ src/rust/grapl-router-api/src/model_plugin_deployer_router/deploy.rs
@@ -1,7 +1,24 @@
 use actix_web::{post, Responder};
+// use actix_web::error::InternalError;
+// use actix_web::http::StatusCode;
 
+use serde;
+
+#[derive(serde::Serialize)]
+pub struct Response {
+    pub res: String
+}
+
+// actix procedural macros that route incoming http requests
 #[post("/modelPluginDeployer/deploy")]
 pub async fn grapl_model_plugin_deployer() -> impl Responder {
+    //curl("http:colinapi.com")
+    //.await
+
+    // CALL MODEL-PLUGIN-DEPOYER GRPC CLIENT
+
+    // we come in on a path. Based on that path, we route the request to the appropriate service.
+
     // route to the graplModelPluginDeployer
     // use grcp client for model-plugin-deployer
     // setup & write tests with an http client
