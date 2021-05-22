8c42d19dfeef08078186a5d496e1f668dcaab09a
Christopher Maier <chris@graplsecurity.com>
2021-05-21 20:36:58 -0400

    Format and lint files in src/js
    Signed-off-by: Christopher Maier <chris@graplsecurity.com>

--- src/js/graphql_endpoint/start_potentially_with_debugger.sh
+++ src/js/graphql_endpoint/start_potentially_with_debugger.sh
@@ -2,9 +2,9 @@
 
 SUBSTRING='graphql_endpoint'
 if [[ "$DEBUG_SERVICES" == *"$SUBSTRING"* ]]; then
-  echo "Starting with debugger"
-  node --inspect=0.0.0.0:$VSC_DEBUGGER_PORT_FOR_GRAPHQL_ENDPOINT server.js
+    echo "Starting with debugger"
+    node --inspect="0.0.0.0:${VSC_DEBUGGER_PORT_FOR_GRAPHQL_ENDPOINT}" server.js
 else
-  echo "Starting sans debugger"
-  yarn start
+    echo "Starting sans debugger"
+    yarn start
 fi
