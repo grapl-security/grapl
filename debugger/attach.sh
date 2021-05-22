57980c32a576ff232b802f782aeca550dda88ed9
Christopher Maier <chris@graplsecurity.com>
2021-05-21 21:16:55 -0400

    Format and lint files in the debugger directory
    Signed-off-by: Christopher Maier <chris@graplsecurity.com>

--- debugger/attach.sh
+++ debugger/attach.sh
@@ -5,14 +5,14 @@ set -euo pipefail
 target_container_name="${1}"
 
 docker run \
-       --interactive \
-       --tty \
-       --rm \
-       --pid="container:${target_container_name}" \
-       --net="container:${target_container_name}" \
-       --privileged \
-       --cap-add sys_admin \
-       --cap-add sys_ptrace \
-       --volume="$(pwd):/from-host" \
-       --workdir="/from-host" \
-       grapl/debugger
+    --interactive \
+    --tty \
+    --rm \
+    --pid="container:${target_container_name}" \
+    --net="container:${target_container_name}" \
+    --privileged \
+    --cap-add sys_admin \
+    --cap-add sys_ptrace \
+    --volume="$(pwd):/from-host" \
+    --workdir="/from-host" \
+    grapl/debugger
