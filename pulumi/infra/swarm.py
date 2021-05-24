08452885b4f40a0f12523511615860e60c1ac4ed
Christopher Maier <chris@graplsecurity.com>
2021-05-24 09:33:16 -0400

    attach_policy_to_ship_logs_to_cloudwatch isn't "private"
    Prefixing it with a "_" causes editor linters to erroneously flag it
    as unused, when it actually is.
    
    Signed-off-by: Christopher Maier <chris@graplsecurity.com>

--- pulumi/infra/swarm.py
+++ pulumi/infra/swarm.py
@@ -143,7 +143,7 @@ class Swarm(pulumi.ComponentResource):
             role=self.role, policy=policies.CLOUDWATCH_AGENT_SERVER_POLICY
         )
         policies.attach_policy(role=self.role, policy=policies.SSM_POLICY)
-        policies._attach_policy_to_ship_logs_to_cloudwatch(
+        policies.attach_policy_to_ship_logs_to_cloudwatch(
             role=self.role, log_group=self.log_group, opts=child_opts
         )
 
