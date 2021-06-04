243623ade94ca95d6189f9ccdce18a0bcbc889fe
jgrillo <jgrillo@graplsecurity.com>
2021-06-04 20:47:07 +0100

    remove defunct graplctl commands and add a "dgraph destroy" command

--- src/python/graplctl/graplctl/aws/lib.py
+++ src/python/graplctl/graplctl/aws/lib.py
@@ -1,11 +1,7 @@
 import base64
 import logging
 import os
-import pathlib
-import shutil
-import subprocess
 import sys
-from typing import IO, AnyStr
 
 from mypy_boto3_lambda import LambdaClient
 
@@ -16,91 +12,6 @@ LOGGER.addHandler(logging.StreamHandler(stream=sys.stdout))
 GRAPL_CDK_RELATIVE_PATH = "src/js/grapl-cdk"
 EDGE_UX_DIRECTORY = "/edge_ux_post_replace"
 CDK_OUT_FILENAME = "cdk-output.json"
-
-
-def _run_shell_cmd(
-    cmd: str,
-    cwd: pathlib.Path,
-    stdout: IO[AnyStr],
-    stderr: IO[AnyStr],
-) -> subprocess.CompletedProcess:
-    return subprocess.run(
-        cmd,
-        stdout=stdout,
-        stderr=stderr,
-        check=True,
-        shell=True,
-        cwd=cwd.as_posix(),
-        executable="/bin/bash",
-    )
-
-
-def deploy_grapl(
-    grapl_root: pathlib.Path, aws_profile: str, stdout: IO[AnyStr], stderr: IO[AnyStr]
-) -> None:
-    grapl_cdk_dir = pathlib.Path(grapl_root.absolute(), GRAPL_CDK_RELATIVE_PATH)
-    edge_ux_artifact_dir = pathlib.Path(grapl_cdk_dir, EDGE_UX_DIRECTORY)
-    outputs_file = pathlib.Path(grapl_cdk_dir, CDK_OUT_FILENAME)
-
-    if edge_ux_artifact_dir.exists():
-        shutil.rmtree(edge_ux_artifact_dir)
-
-    os.mkdir(edge_ux_artifact_dir)
-
-    LOGGER.info("building cdk")
-    _run_shell_cmd("npm run build", cwd=grapl_cdk_dir, stdout=stdout, stderr=stderr)
-    LOGGER.info("built cdk")
-
-    LOGGER.info("deploying Grapl stack")
-    _run_shell_cmd(
-        f"cdk deploy --require-approval=never --profile={aws_profile} --outputs-file={outputs_file.as_posix()} Grapl",
-        cwd=grapl_cdk_dir,
-        stdout=stdout,
-        stderr=stderr,
-    )
-    LOGGER.info("deployed Grapl stack")
-
-    shutil.rmtree(edge_ux_artifact_dir)
-    os.mkdir(edge_ux_artifact_dir)
-
-    LOGGER.info("creating edge UX package")
-    _run_shell_cmd(
-        "npm run create_edge_ux_package",
-        cwd=grapl_cdk_dir,
-        stdout=stdout,
-        stderr=stderr,
-    )
-    LOGGER.info("created edge UX package")
-
-    LOGGER.info("deploying EngagementUX stack")
-    _run_shell_cmd(
-        f"cdk deploy --require-approval=never --profile={aws_profile} --outputs-file={outputs_file.as_posix()} EngagementUX",
-        cwd=grapl_cdk_dir,
-        stdout=stdout,
-        stderr=stderr,
-    )
-    LOGGER.info("deployed EngagementUX stack")
-
-    shutil.rmtree(edge_ux_artifact_dir)
-
-
-def destroy_grapl(
-    grapl_root: pathlib.Path, aws_profile: str, stdout: IO[AnyStr], stderr: IO[AnyStr]
-) -> None:
-    grapl_cdk_dir = pathlib.Path(grapl_root.absolute(), GRAPL_CDK_RELATIVE_PATH)
-
-    LOGGER.info("building cdk")
-    _run_shell_cmd("npm run build", cwd=grapl_cdk_dir, stdout=stdout, stderr=stderr)
-    LOGGER.info("built cdk")
-
-    LOGGER.info("destroying all stacks")
-    _run_shell_cmd(
-        f'cdk destroy --profile={aws_profile} --force --require-approval=never "*"',
-        cwd=grapl_cdk_dir,
-        stdout=stdout,
-        stderr=stderr,
-    )
-    LOGGER.info("destroyed all stacks")
 
 
 def _invoke_lambda(lambda_: LambdaClient, function_name: str) -> None:
@@ -119,7 +30,7 @@ def _invoke_lambda(lambda_: LambdaClient, function_name: str) -> None:
         LOGGER.info(f"lambda invocation succeeded for {function_name}")
     else:
         LOGGER.error(
-            f"{''.join(l.decode('utf-8') for l in result['Payload'].readlines())}"
+            f"{''.join(l.decode('utf-8') for l in result['Payload'].iter_lines())}"
         )
         raise Exception(
             f"lambda invocation for {function_name} failed with status {status}: {result['FunctionError']}"
