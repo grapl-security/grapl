0cd2d224684f94e6fc4424c2707da7a5b1394f68
jgrillo <jgrillo@graplsecurity.com>
2021-06-05 02:33:50 +0100

    add debug logs to engagement edge client and adjust some resource names

--- src/python/grapl-tests-common/grapl_tests_common/clients/engagement_edge_client.py
+++ src/python/grapl-tests-common/grapl_tests_common/clients/engagement_edge_client.py
@@ -6,6 +6,7 @@ from typing import Optional
 import boto3
 import requests
 from grapl_common.env_helpers import SecretsManagerClientFactory
+from grapl_common.grapl_logger import get_module_grapl_logger
 
 _JSON_CONTENT_TYPE_HEADERS = {"Content-type": "application/json"}
 _ORIGIN = {
@@ -14,6 +15,8 @@ _ORIGIN = {
 
 IS_LOCAL = bool(os.getenv("IS_LOCAL", default=False))
 
+LOGGER = get_module_grapl_logger(default_log_level="DEBUG")
+
 
 class EngagementEdgeException(Exception):
     pass
@@ -21,6 +24,7 @@ class EngagementEdgeException(Exception):
 
 def _get_test_user_password(deployment_name: str) -> str:
     secretsmanager = SecretsManagerClientFactory(boto3).from_env()
+    LOGGER.debug(f"retrieving {deployment_name}-TestUserPassword")
     return secretsmanager.get_secret_value(
         SecretId=f"{deployment_name}-TestUserPassword"
     )["SecretString"]
@@ -40,8 +44,10 @@ class EngagementEdgeClient:
         port = int(os.environ["GRAPL_HTTP_FRONTEND_PORT"]) if IS_LOCAL else 443
         protocol = "http" if IS_LOCAL else "https"
         self.endpoint = f'{protocol}://{os.environ["GRAPL_API_HOST"]}:{port}/auth'
+        LOGGER.debug(f"created EngagementEdgeClient for endpoint {self.endpoint}")
 
     def get_jwt(self) -> str:
+        LOGGER.debug("retrieving jwt cookie")
         username = os.environ["GRAPL_TEST_USER_NAME"]
         password = _sha_and_pepper(
             username=username,
@@ -49,6 +55,7 @@ class EngagementEdgeClient:
                 deployment_name=os.environ["DEPLOYMENT_NAME"]
             ),
         )
+        LOGGER.debug(f"logging in with user {username} at {self.endpoint}/login")
         resp = requests.post(
             f"{self.endpoint}/login",
             json={
@@ -67,6 +74,7 @@ class EngagementEdgeClient:
             raise EngagementEdgeException(
                 f"Couldn't find grapl_jwt cookie in {resp.cookies}"
             )
+        LOGGER.debug("retrieved jwt cookie")
         return cookie
 
     def invalid_creds(self) -> requests.Response:
