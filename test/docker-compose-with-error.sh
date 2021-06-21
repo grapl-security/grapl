7ba7e8832c6d20f69152f7eff78cada28fbc8020
Max Wittek <wimax@graplsecurity.com>
2021-06-20 18:48:55 -0700

    format

--- test/docker-compose-with-error.sh
+++ test/docker-compose-with-error.sh
@@ -27,7 +27,7 @@ EXIT_CODE=0
 ALL_TESTS=$(docker-compose ps --quiet ${TARGETS})
 for test in $ALL_TESTS; do
     test_exit_code_str=$(docker inspect -f "{{ .State.ExitCode }}" "${test}")
-    test_exit_code=$(($test_exit_code_str+0))  # cast to int
+    test_exit_code=$(($test_exit_code_str + 0)) # cast to int
     if [[ ${test_exit_code} -ne 0 ]]; then
         EXIT_CODE=$test_exit_code
         break
