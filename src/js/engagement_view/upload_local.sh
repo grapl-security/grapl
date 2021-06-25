04dbb20c49843dc0debf88c8f3805ee75c41250a
wimax-grapl <69007229+wimax-grapl@users.noreply.github.com>
2021-06-25 15:57:37 -0400

    Fix engagement-ux-uploader (#953)

--- src/js/engagement_view/upload_local.sh
+++ src/js/engagement_view/upload_local.sh
@@ -3,5 +3,9 @@ cd ./build/ || exit
 
 set -xeuo pipefail
 
+export AWS_ACCESS_KEY_ID="${GRAPL_AWS_ACCESS_KEY_ID}"
+export AWS_SECRET_ACCESS_KEY="${GRAPL_AWS_ACCESS_KEY_SECRET}"
+
 aws s3 sync . s3://local-grapl-engagement-ux-bucket/ \
-    --endpoint-url="${GRAPL_AWS_ENDPOINT}" --region="${AWS_REGION}"
+    --endpoint-url="${GRAPL_AWS_ENDPOINT}" \
+    --region="${AWS_REGION}"
