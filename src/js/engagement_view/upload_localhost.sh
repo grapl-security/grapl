80e0715b1d29e18b4a4993d277573a6f55458365
Max Wittek <wimax@graplsecurity.com>
2021-06-25 12:29:05 -0700

    Fix engagement-ux-uploader

--- src/js/engagement_view/upload_localhost.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ./build/ || exit
-
-export AWS_ACCESS_KEY_ID="test"
-export AWS_SECRET_ACCESS_KEY="test"
-
-aws s3 sync . s3://local-grapl-engagement-ux-bucket/ \
-    --endpoint-url=http://localhost:4566 \
-    --region=us-east-1
