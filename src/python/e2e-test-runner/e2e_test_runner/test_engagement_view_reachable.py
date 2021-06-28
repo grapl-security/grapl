98be3917f63437d1bc17aee72c2d0221e11dc960
Max Wittek <wimax@graplsecurity.com>
2021-06-28 14:26:56 -0700

    added a smoke test

---
+++ src/python/e2e-test-runner/e2e_test_runner/test_engagement_view_reachable.py
@@ -0,0 +1,37 @@
+import os
+from http import HTTPStatus
+from unittest import TestCase
+
+import pytest
+import requests
+from grapl_tests_common.clients.common import endpoint_url
+
+_IS_LOCAL = os.environ["IS_LOCAL"]
+
+
+class EngagementViewScraper:
+    def __init__(self) -> None:
+        self.endpoint = endpoint_url("/index.html")
+
+    def get_index(self) -> requests.Response:
+        resp = requests.get(url=self.endpoint)
+        if resp.status_code != HTTPStatus.OK:
+            raise Exception(f"{resp.status_code}: {resp.text}")
+        return resp
+
+
+@pytest.mark.integration_test
+class TestEngagementViewReachable(TestCase):
+    """
+    A very minor smoke test to prevent regressions where Engagement UX is no longer accessible.
+    This should be replaced by Selenium or something like that.
+    But - a small smoke test is beneficial here.
+    """
+
+    @pytest.mark.skipif(condition=not _IS_LOCAL, reason="currently broken in aws")
+    def test_engagement_view_reachable_when_is_local(self) -> None:
+        scraper = EngagementViewScraper()
+        resp = scraper.get_index()
+        # I could file-read it, but - this is a cheap smoke test, whatever
+        contents_substr = "Grapl: A Graph Analytics Platform for Detection & Response"  # from index.html
+        assert contents_substr in resp.content.decode("utf8")
