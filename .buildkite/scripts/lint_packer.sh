3adba5ad69939edd1ed3b8d369c0680fedf05e33
Max Wittek <wimax@graplsecurity.com>
2021-07-08 14:49:53 -0700

    Revert "What if I manually specify the pkr file"
    This reverts commit 3d6f656f665c0024a620716e9c4d5d42fb3f52c3.

--- .buildkite/scripts/lint_packer.sh
+++ .buildkite/scripts/lint_packer.sh
@@ -2,6 +2,4 @@
 set -euo pipefail
 
 echo "--- Linting Packer"
-# Ideally we could do the following.
-# packer fmt -check -diff -recursive packer/
-packer fmt -check -diff packer/nomad-server-client/image.pkr.hcl
+packer fmt -check -diff -recursive packer
