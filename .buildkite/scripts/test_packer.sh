10d60e15ef51d553a98c9504d46006a975608bde
Max Wittek <wimax@graplsecurity.com>
2021-07-09 10:44:25 -0700

    use globs

--- .buildkite/scripts/test_packer.sh
+++ .buildkite/scripts/test_packer.sh
@@ -8,4 +8,8 @@ echo -e "--- :packer: Performing test build of AMI"
 
 source .buildkite/scripts/lib/packer.sh
 
+# We don't actually use anything in `constants` here, but make sure we can
+# source it without errors.
+source .buildkite/scripts/lib/packer_constants.sh
+
 PACKER_VARS="-var build_ami=false" build_ami
