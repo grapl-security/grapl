1c5a2d5fd93aff9245b53cb11f917aa07d2daa74
colin <colin@graplsecurity.com>
2021-07-13 10:01:32 -0700

    Fix a minor issue in test_envelope.py

--- src/python/grapl-common/tests/test_envelope.py
+++ src/python/grapl-common/tests/test_envelope.py
@@ -7,9 +7,9 @@ from grapl_common.envelope import proto_uuid_to_pyuuid, pyuuid_to_proto_uuid
 class TestProtoUuidv4(unittest.TestCase):
     # A basic test to ensure that conversions between uuid representations preserve the uuid
     def test_uuid_roundtrip(self) -> None:
-        original_uuid = uuid.uuid4()
+        original_uuid: uuid.UUID = uuid.uuid4()
         proto_uuid = pyuuid_to_proto_uuid(original_uuid)
         converted_original = proto_uuid_to_pyuuid(proto_uuid)
-        converted_proto = pyuuid_to_proto_uuid(proto_uuid)
+        converted_proto = pyuuid_to_proto_uuid(converted_original)
         self.assertEqual(original_uuid, converted_original)
         self.assertEqual(proto_uuid, converted_proto)
