9fafc4f458b161028f5aaa00c5ff719bce22e81e
jgrillo <jgrillo@graplsecurity.com>
2021-07-15 20:50:50 +0100

    no acls in local grapl :(

--- pulumi/infra/kafka.py
+++ pulumi/infra/kafka.py
@@ -1,8 +1,11 @@
 from typing import Mapping, Optional, Union
+from pulumi.resource import ResourceOptions
 
 import pulumi_kafka as kafka
 
 import pulumi
+
+from infra.config import LOCAL_GRAPL
 
 
 class Kafka(pulumi.ComponentResource):
@@ -16,15 +19,23 @@ class Kafka(pulumi.ComponentResource):
     ):
         super().__init__("grapl:Kafka", name=name, props=None, opts=opts)
 
-        provider = kafka.Provider(
-            "kafka-provider",
-            bootstrap_servers=confluent["bootstrap_servers"],
-            opts=opts,
-            sasl_mechanism="plain",
-            sasl_password=confluent["grapl_pulumi"]["api_key"],
-            sasl_username=confluent["grapl_pulumi"]["api_secret"],
-            tls_enabled=True,
-        )
+        if LOCAL_GRAPL:
+            provider = kafka.Provider(
+                "kafka-provider",
+                bootstrap_servers=[confluent["bootstrap_servers"]],
+                opts=opts,
+                tls_enabled=False,
+            )
+        else:
+            provider = kafka.Provider(
+                "kafka-provider",
+                bootstrap_servers=[confluent["bootstrap_servers"]],
+                opts=opts,
+                sasl_mechanism="plain",
+                sasl_password=confluent["grapl_pulumi"]["api_key"],
+                sasl_username=confluent["grapl_pulumi"]["api_secret"],
+                tls_enabled=True,
+            )
 
         #
         # metrics topic
@@ -32,24 +43,25 @@ class Kafka(pulumi.ComponentResource):
 
         kafka.Topic(
             "metrics-topic",
-            provider=provider,
+            opts=pulumi.ResourceOptions(provider=provider),
             name="metrics",
             replication_factor=1,
             partitions=1,
         )
 
-        services = set(confluent.keys()).difference(
-            {"bootstrap_servers", "grapl_pulumi"}
-        )
-        for service in services:
-            # give every service write access to the metrics topic
-            kafka.Acl(
-                f"{service}-metrics-topic-acl",
-                provider=provider,
-                acl_resource_name="metrics",
-                acl_resource_type="Topic",
-                acl_principal=f"User:{confluent[service]['service_account_id']}",
-                acl_host="*",
-                acl_operation="Write",
-                acl_permission_type="Allow",
+        if not LOCAL_GRAPL:
+            services = set(confluent.keys()).difference(
+                {"bootstrap_servers", "grapl_pulumi"}
             )
+            for service in services:
+                # give every service write access to the metrics topic
+                kafka.Acl(
+                    f"{service}-metrics-topic-acl",
+                    opts=pulumi.ResourceOptions(provider=provider),
+                    acl_resource_name="metrics",
+                    acl_resource_type="Topic",
+                    acl_principal=f"User:{confluent[service]['service_account_id']}",
+                    acl_host="*",
+                    acl_operation="Write",
+                    acl_permission_type="Allow",
+                )
