17a3201bdf8ded766b496a916f036f12e78a9b76
colin <colin@graplsecurity.com>
2021-07-15 07:48:38 -0700

    Add Python wrappers around the protobuf

--- src/python/grapl-common/grapl_common/envelope.py
+++ src/python/grapl-common/grapl_common/envelope.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
+import json
 import uuid
-from uuid import uuid4
 
-from graplinc.grapl.api.services.v1beta1.types_pb2 import Envelope as _Envelope
-from graplinc.grapl.api.services.v1beta1.types_pb2 import Metadata, ProtoUuidV4
+from graplinc.grapl.api.services.v1beta1.types_pb2 import Envelope as ProtoEnvelope
+from graplinc.grapl.api.services.v1beta1.types_pb2 import Metadata as ProtoMetadata, ProtoUuidV4
 
 
 def proto_uuid_to_pyuuid(proto_uuid: ProtoUuidV4) -> uuid.UUID:
@@ -28,43 +28,97 @@ def pyuuid_to_proto_uuid(pyuuid: uuid.UUID) -> ProtoUuidV4:
     return proto_uuid
 
 
-def init_metadata() -> Metadata:
-    metadata = Metadata()
-    metadata.trace_id.CopyFrom(pyuuid_to_proto_uuid(uuid4()))
-    metadata.tenant_id.CopyFrom(pyuuid_to_proto_uuid(uuid4()))
-    return metadata
+class Metadata(object):
+    def __init__(self, trace_id: uuid.UUID, tenant_id: uuid.UUID) -> None:
+        self.trace_id = trace_id
+        self.tenant_id = tenant_id
+
+    @staticmethod
+    def new() -> Metadata:
+        return Metadata(trace_id=uuid.uuid4(), tenant_id=uuid.uuid4())
+
+    @staticmethod
+    def from_proto_bytes(b: bytes) -> Metadata:
+        metadata = ProtoEnvelope()
+        metadata.ParseFromString(b)
+        return Metadata.from_proto(metadata)
+
+    @staticmethod
+    def from_proto_parts(trace_id: ProtoUuidV4, tenant_id: ProtoUuidV4) -> Metadata:
+        return Metadata(proto_uuid_to_pyuuid(trace_id), proto_uuid_to_pyuuid(tenant_id))
+
+    @staticmethod
+    def from_proto(metadata: ProtoMetadata) -> Metadata:
+        return Metadata.from_proto_parts(metadata.trace_id, metadata.tenant_id)
+
+    def to_proto(self) -> ProtoMetadata:
+        metadata = ProtoMetadata()
+        metadata.trace_id.CopyFrom(pyuuid_to_proto_uuid(self.trace_id))
+        metadata.tenant_id.CopyFrom(pyuuid_to_proto_uuid(self.tenant_id))
+        return metadata
+
+    def to_proto_bytes(self) -> bytes:
+        return self.to_proto().SerializeToString()
+
+    def __str__(self) -> str:
+        return "Metadata " + json.dumps(vars(self))
 
 
 class Envelope(object):
-    def __init__(self, envelope: _Envelope) -> None:
-        self.envelope = envelope
+    def __init__(
+            self,
+            metadata: Metadata,
+            inner_message: bytes,
+            inner_type: str,
+    ) -> None:
+        self.metadata = metadata
+        self.inner_message = inner_message
+        self.inner_type = inner_type
 
     @staticmethod
-    def from_parts(metadata: Metadata, inner_message: bytes, inner_type: str) -> Envelope:
-        envelope = _Envelope()
-        envelope.metadata.CopyFrom(metadata)
-        envelope.inner_message = inner_message
-        envelope.inner_type = inner_type
-        return Envelope(envelope)
+    def from_proto_bytes(b: bytes) -> Envelope:
+        envelope = ProtoEnvelope()
+        envelope.ParseFromString(b)
+        return Envelope.from_proto(envelope)
 
     @staticmethod
-    def from_proto(s: bytes) -> Envelope:
-        envelope = _Envelope()
-        envelope.ParseFromString(s)
+    def from_proto(envelope: ProtoEnvelope) -> Envelope:
+        return Envelope.from_proto_parts(
+            envelope.metadata, envelope.inner_message, envelope.inner_type,
+        )
 
-        return Envelope(envelope)
+    @staticmethod
+    def from_proto_parts(
+            metadata: ProtoMetadata,
+            inner_message: bytes,
+            inner_type: str,
+    ) -> Envelope:
+        return Envelope(
+            Metadata.from_proto(metadata), inner_message, inner_type
+        )
 
-    def serialize_to_string(self) -> str:
-        return self.envelope.SerializeToString()
+    def to_proto(self) -> ProtoMetadata:
+        metadata = ProtoMetadata()
+        metadata.trace_id.CopyFrom(pyuuid_to_proto_uuid(self.trace_id))
+        metadata.tenant_id.CopyFrom(pyuuid_to_proto_uuid(self.tenant_id))
+        return metadata
 
-    @property
-    def metadata(self) -> Metadata:
-        return self.envelope.metadata
+    def to_proto_bytes(self) -> bytes:
+        return self.to_proto().SerializeToString()
 
-    @property
-    def inner_type(self) -> str:
-        return self.envelope.inner_type
+    def get_metadata(self) -> Metadata:
+        return self.metadata
 
-    @property
-    def inner_message(self) -> bytes:
-        return self.envelope.inner_message
+    def get_inner_type(self) -> str:
+        return self.inner_type
+
+    def get_inner_message(self) -> bytes:
+        return self.inner_message
+
+    def __str__(self) -> str:
+        return "Envelope " + json.dumps({
+            'metadata': vars(self.metadata),
+            'inner_type': self.inner_type,
+            # The message isn't valid utf8, and it could be huge, so just print out its length
+            'inner_message_len': len(self.inner_message)
+        })
