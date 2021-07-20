5d4092aeeacb0e5cf9e040905b0d0d1868524f05
Max Wittek <wimax@graplsecurity.com>
2021-07-20 11:30:32 -0700

    Clean up something that might have had a weird exit code, maybe

--- packer/nomad-server-client/setup_vector.sh
+++ packer/nomad-server-client/setup_vector.sh
@@ -8,8 +8,6 @@ curl -1sLf \
   'https://repositories.timber.io/public/vector/cfg/setup/bash.rpm.sh' \
 | sudo -E bash
 
-
-sudo yum --showduplicates list vector | expand
 sudo yum install "vector-${VECTOR_VERSION}"
 
 # Ensure it's all working correctly
