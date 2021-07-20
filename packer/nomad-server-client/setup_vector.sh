a274385f5db69d1cf588f997182d67ba37b164a7
Max Wittek <wimax@graplsecurity.com>
2021-07-20 11:55:48 -0700

    woops, change docs to point at yum

--- packer/nomad-server-client/setup_vector.sh
+++ packer/nomad-server-client/setup_vector.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # Adapted from:
 # - https://vector.dev/docs/setup/quickstart/
-# - https://vector.dev/docs/setup/installation/package-managers/apt/
+# - https://vector.dev/docs/setup/installation/package-managers/yum/
 set -euo pipefail
 
+# Add repo to YUM
 curl -1sLf \
   'https://repositories.timber.io/public/vector/cfg/setup/bash.rpm.sh' \
 | sudo -E bash
