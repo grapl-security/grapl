c9f161a4c4753b30b7eeddaf03bcd0993e01db28
Max Wittek <wimax@graplsecurity.com>
2021-07-20 13:40:07 -0700

    ugh why is it failing and moreover why is it so hard to debug

--- packer/nomad-server-client/setup_osquery.sh
+++ packer/nomad-server-client/setup_osquery.sh
@@ -5,13 +5,14 @@
 
 set -euo pipefail
 
-# Add repo
+echo "--- Add repo"
 curl -L https://pkg.osquery.io/rpm/GPG | sudo tee /etc/pki/rpm-gpg/RPM-GPG-KEY-osquery
 sudo yum-config-manager --add-repo https://pkg.osquery.io/rpm/osquery-s3-rpm.repo
 sudo yum-config-manager --enable osquery-s3-rpm-repo
 
+echo "--- Install osquery"
 sudo yum install --assumeyes osquery
 
-# Ensure it's all working correctly.
+echo "--- Ensure it's all working correctly"
 osqueryi --version
 # Yes, the `i` at the end is not a typo
