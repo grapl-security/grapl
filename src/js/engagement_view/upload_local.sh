2907e4dbbe97c4178075494f597f24c0ef03575e
Christopher Maier <chris@graplsecurity.com>
2021-07-23 13:21:09 -0400

    Pass ux-bucket directly to upload_local.sh script
    Signed-off-by: Christopher Maier <chris@graplsecurity.com>

--- src/js/engagement_view/upload_local.sh
+++ src/js/engagement_view/upload_local.sh
@@ -6,6 +6,6 @@ set -xeuo pipefail
 export AWS_ACCESS_KEY_ID="${GRAPL_AWS_ACCESS_KEY_ID}"
 export AWS_SECRET_ACCESS_KEY="${GRAPL_AWS_ACCESS_KEY_SECRET}"
 
-aws s3 sync . s3://local-grapl-engagement-ux-bucket/ \
+aws s3 sync . "s3://${GRAPL_UX_BUCKET}/" \
     --endpoint-url="${GRAPL_AWS_ENDPOINT}" \
     --region="${AWS_REGION}"
