3989046732230f1beb0b173f7c225e4d8e3e53bb
wimax-grapl <69007229+wimax-grapl@users.noreply.github.com>
2021-07-29 15:50:20 -0400

    Unified env_helpers.ts for our typescript stuff (#1041)
    * Unified env_helpers.ts for our typescript stuff
    
    * remove unused line from jwt

--- src/js/graphql_endpoint/modules/jwt.ts
+++ src/js/graphql_endpoint/modules/jwt.ts
@@ -1,25 +1,15 @@
 import * as express from "express";
 import * as jwt from "jsonwebtoken";
 import * as AWS from "aws-sdk";
+import { getAwsClient } from "./env_helpers";
 
-const IS_LOCAL = process.env.IS_LOCAL == "True" || null; // get this from environment
 const JWT_SECRET_ID = process.env.JWT_SECRET_ID; // get this from environment
 
 // Acts as a local cache of the secret so we don't have to refetch it every time
 let JWT_SECRET: string = "";
 
 function getSecretsManagerClient(): AWS.SecretsManager {
-    // TODO: This code should be unified with the dynamodb stuff in schema_client;
-    // or perhaps even create a typescript equivalent of env_helpers
-    return new AWS.SecretsManager({
-        apiVersion: "2017-10-17",
-        region: IS_LOCAL ? process.env.AWS_REGION : undefined,
-        accessKeyId: IS_LOCAL ? process.env.GRAPL_AWS_ACCESS_KEY_ID : undefined,
-        secretAccessKey: IS_LOCAL
-            ? process.env.GRAPL_AWS_ACCESS_KEY_SECRET
-            : undefined,
-        endpoint: IS_LOCAL ? process.env.GRAPL_AWS_ENDPOINT : undefined,
-    });
+    return getAwsClient(AWS.SecretsManager);
 }
 
 async function fetchJwtSecret(): Promise<string> {
