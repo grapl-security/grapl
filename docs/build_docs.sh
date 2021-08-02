0257f75a5dd2a8618eab3030b563b4b8b79d0232
Max Wittek <wimax@graplsecurity.com>
2021-08-02 13:05:52 -0700

    Fix up build_docs to deal with inexistent TARGET_DIR

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -6,12 +6,13 @@ THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "${THIS_DIR}"
 
 python3 -m venv venv
+# shellcheck disable=SC1091
 source venv/bin/activate
 pip install wheel
 pip install -r requirements.txt
 
 export TARGET_DIR="/tmp/grapl_docs"
-rm -r $TARGET_DIR
+rm -rf $TARGET_DIR || true
 # -W means warnings become errors
 sphinx-build ./ $TARGET_DIR -W
 echo "$TARGET_DIR/index.html"
