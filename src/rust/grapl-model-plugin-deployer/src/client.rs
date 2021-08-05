01406c62ef3908ff7f589cadf130bc1be15f047b
Max Wittek <wimax@graplsecurity.com>
2021-08-05 10:58:17 -0700

    Oh no, why is there a grapl-model-plugin-deployer in here

--- src/rust/grapl-model-plugin-deployer/src/client.rs
+++
@@ -1,5 +0,0 @@
-pub use crate::grapl_model_plugin_deployer::grapl_model_plugin_deployer_rpc_client::GraplModelPluginDeployerRpcClient;
-pub use crate::grapl_model_plugin_deployer::GraplModelPluginDeployerRequest;
-
-pub use tower::timeout::Timeout;
-pub use tonic::transport::Channel;
