cd2d0e91a086d08f65200e1f89d481365c33cc2c
Max Wittek <wimax@graplsecurity.com>
2021-08-05 11:34:36 -0700

    Compile the right protos

--- src/python/grapl-template-generator/grapl_template_generator/grapl-templates/rust-grpc-service/{{cookiecutter.project_slug}}/build.rs
+++ src/python/grapl-template-generator/grapl_template_generator/grapl-templates/rust-grpc-service/{{cookiecutter.project_slug}}/build.rs
@@ -4,5 +4,13 @@ fn main() {
     config
         .build_server(true)
         .build_client(true)
+        .compile_protos(
+            // These protos are moved to this directory by `def move_protos_to_global_proto_dir`
+            &[
+                "../../proto/graplinc/grapl/api/{{cookiecutter.snake_project_name}}/{{cookiecutter.snake_project_name}}.proto",
+                "../../proto/graplinc/grapl/api/{{cookiecutter.snake_project_name}}/{{cookiecutter.snake_project_name}}_health.proto",
+            ],
+            &["../../proto/"],
+        )
         .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 }
