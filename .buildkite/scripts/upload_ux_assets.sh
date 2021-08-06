c3be813dff4202446e75cb5afef034b832eb9856
Christopher Maier <chris@graplsecurity.com>
2021-08-06 15:04:53 -0400

    Put UX artifacts.json file in manifests directory
    Signed-off-by: Christopher Maier <chris@graplsecurity.com>

--- .buildkite/scripts/upload_ux_assets.sh
+++ .buildkite/scripts/upload_ux_assets.sh
@@ -21,10 +21,11 @@ main() {
     echo "--- :cloudsmith: Uploading ${UX_FILENAME} version ${version}"
     upload_raw_file_to_cloudsmith "dist/${UX_FILENAME}" "${version}" raw
 
-    artifact_json "${version}" "grapl-ux" > "${UX_ARTIFACTS_FILE}"
+    mkdir "${ARTIFACT_FILE_DIRECTORY}"
+    artifact_json "${version}" "grapl-ux" > "${ARTIFACT_FILE_DIRECTORY}/${UX_ARTIFACTS_FILE}"
 
-    echo "--- :buildkite: Uploading ${UX_ARTIFACTS_FILE} file"
-    buildkite-agent artifact upload "${UX_ARTIFACTS_FILE}"
+    echo "--- :buildkite: Uploading ${ARTIFACT_FILE_DIRECTORY}/${UX_ARTIFACTS_FILE} file"
+    buildkite-agent artifact upload "${ARTIFACT_FILE_DIRECTORY}/${UX_ARTIFACTS_FILE}"
 }
 
 main
