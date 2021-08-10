87a4c6a3f1b79eaad92d396730a380b30755a49f
Max Wittek <wimax@graplsecurity.com>
2021-08-10 11:04:34 -0700

    quiet stdout/err

--- src/python/grapl-template-generator/grapl_template_generator/test_generator.sh
+++ src/python/grapl-template-generator/grapl_template_generator/test_generator.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-# This script is meant to be run outside of Docker in a real, honest-to-goodness shell.
+
+# This script is meant to be run in a real, honest-to-goodness shell.
+# No Docker, no Pants, just a real Buildkite host or dev machine.
+
 set -euo pipefail
 
 GRAPL_ROOT="$(git rev-parse --show-toplevel)"
 cd "${GRAPL_ROOT}"
 
 # Make sure our tree is clean
-git diff --exit-code || (echo "Your git tree is dirty; bailing!" && exit 42)
+git diff HEAD --exit-code > /dev/null 2>&1 || (echo "Your git tree is dirty; bailing!" && exit 42)
 
 make grapl-template-generator
 
