2eb92278886b67ef7ff95f79c48dbe95b97fdbff
Thomas Wunderlich <thomas@graplsecurity.com>
2021-08-13 15:29:02 -0400

    Add more typing

--- pulumi/tests/test_network.py
+++ pulumi/tests/test_network.py
@@ -28,7 +28,7 @@ class TestingNetwork(unittest.TestCase):
     def test_number_of_subnets(self) -> pulumi.Output:
         network = Network("test_public_subnets")
 
-        def check_number_of_subnets(args) -> None:
+        def check_number_of_subnets(args: pulumi.Input) -> None:
             public_subnets, private_subnets = args
             assert (
                 len(public_subnets) == 2
@@ -45,7 +45,7 @@ class TestingNetwork(unittest.TestCase):
     def test_public_subnet_tags(self) -> pulumi.Output:
         network = Network("test_public_subnet_tags")
 
-        def check_public_subnet_tags(args) -> None:
+        def check_public_subnet_tags(args: pulumi.Input) -> None:
             urn, tags = args
             assert tags, f"Subnet {urn} must have tags"
             assert "Name" in tags, f"Subnet {urn} must have a Name tag"
@@ -58,7 +58,7 @@ class TestingNetwork(unittest.TestCase):
     def test_private_subnet_tags(self) -> pulumi.Output:
         network = Network("test_private_subnet_tags")
 
-        def check_private_subnet_tags(args) -> None:
+        def check_private_subnet_tags(args: pulumi.Input) -> None:
             urn, tags = args
             assert tags, f"Subnet {urn} must have tags"
             assert "Name" in tags, f"Subnet {urn} must have a Name tag"
