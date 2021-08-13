9b956bd635bff35c8048361df49877c9ea735082
Thomas Wunderlich <twunderlich@attentivemobile.com>
2021-08-13 12:57:16 -0400

    Convert fstrings to normal strings since there are no params

--- pulumi/tests/test_network.py
+++ pulumi/tests/test_network.py
@@ -39,9 +39,9 @@ class TestingNetwork(unittest.TestCase):
 
         def check_number_of_subnets(args):
             public_subnets, private_subnets = args
-            assert len(public_subnets) == 2, f'There should be 2 public subnets since desired_az_spread is 2'
+            assert len(public_subnets) == 2, "There should be 2 public subnets since desired_az_spread is 2"
             assert len(public_subnets) == len(private_subnets), \
-                f'There should be the same number of public subnets and private subnets'
+                "There should be the same number of public subnets and private subnets"
 
         return pulumi.Output.all(network.public_subnets, network.private_subnets).apply(check_number_of_subnets)
 
