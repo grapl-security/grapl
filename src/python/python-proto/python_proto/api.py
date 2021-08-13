225c5834a77e271050cddd46882c4349bf05df61
jgrillo <jgrillo@graplsecurity.com>
2021-08-13 18:52:48 +0100

    add python-proto library

---
+++ src/python/python-proto/python_proto/api.py
@@ -0,0 +1,10 @@
+from graplinc.grapl.api.graph.v1beta1.types_pb2 import MergedEdgeList, MergedGraph
+
+from python_proto import SerDe
+
+
+class MergedEdgeList(SerDe):
+    # TODO: implement this (and others) and seed to places where they're used:
+    # /src/python/grapl_analyzerlib/grapl_analyzerlib/view_from_proto.py
+    # /src/python/grapl_analyzerlib/grapl_analyzerlib/subgraph_view.py
+    pass
