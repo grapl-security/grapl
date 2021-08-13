e3e0c7037aecc949c8a6cc706974b0b891f6f766
Thomas Wunderlich <twunderlich@attentivemobile.com>
2021-08-13 13:00:38 -0400

    Black formatting

--- pulumi/tests/test_network.py
+++ pulumi/tests/test_network.py
@@ -1,4 +1,3 @@
-import os
 import pulumi
 import sys
 import unittest
@@ -8,30 +7,26 @@ from unittest.mock import MagicMock, patch
 class MyMocks(pulumi.runtime.Mocks):
     def new_resource(self, args: pulumi.runtime.MockResourceArgs):
         outputs = args.inputs
-        return [args.name + '_id', outputs]
+        return [args.name + "_id", outputs]
 
     def call(self, args: pulumi.runtime.MockCallArgs):
         if args.token == "aws:index/getAvailabilityZones:getAvailabilityZones":
-            return {
-                "names": ["a", "b", "c"],
-                "state": "available"
-            }
+            return {"names": ["a", "b", "c"], "state": "available"}
         # This is necessary since infra.config calls this
         if args.token == "aws:index/getCallerIdentity:getCallerIdentity":
-            return {
-                "account_id": "000000000000"
-            }
+            return {"account_id": "000000000000"}
         return {}
+
 
 pulumi.runtime.set_mocks(MyMocks())
 
-sys.modules['infra.network.infra.config'] = MagicMock()
+sys.modules["infra.network.infra.config"] = MagicMock()
 
 # mocks must be defined prior to importing pulumi resources
 from infra.network import Network
 
 
-@patch('infra.config.DEPLOYMENT_NAME', 'unit-test-grapl')
+@patch("infra.config.DEPLOYMENT_NAME", "unit-test-grapl")
 class TestingNetwork(unittest.TestCase):
     @pulumi.runtime.test
     def test_number_of_subnets(self):
@@ -39,11 +34,16 @@ class TestingNetwork(unittest.TestCase):
 
         def check_number_of_subnets(args):
             public_subnets, private_subnets = args
-            assert len(public_subnets) == 2, "There should be 2 public subnets since desired_az_spread is 2"
-            assert len(public_subnets) == len(private_subnets), \
-                "There should be the same number of public subnets and private subnets"
+            assert (
+                len(public_subnets) == 2
+            ), "There should be 2 public subnets since desired_az_spread is 2"
+            assert len(public_subnets) == len(
+                private_subnets
+            ), "There should be the same number of public subnets and private subnets"
 
-        return pulumi.Output.all(network.public_subnets, network.private_subnets).apply(check_number_of_subnets)
+        return pulumi.Output.all(network.public_subnets, network.private_subnets).apply(
+            check_number_of_subnets
+        )
 
     @pulumi.runtime.test
     def test_public_subnet_tags(self):
@@ -51,10 +51,12 @@ class TestingNetwork(unittest.TestCase):
 
         def check_public_subnet_tags(args):
             urn, tags = args
-            assert tags, f'Subnet {urn} must have tags'
-            assert "Name" in tags, f'Subnet {urn} must have a Name tag'
+            assert tags, f"Subnet {urn} must have tags"
+            assert "Name" in tags, f"Subnet {urn} must have a Name tag"
 
-        return pulumi.Output.all(network.public_subnets[0].urn, network.public_subnets[0].tags).apply(check_public_subnet_tags)
+        return pulumi.Output.all(
+            network.public_subnets[0].urn, network.public_subnets[0].tags
+        ).apply(check_public_subnet_tags)
 
     @pulumi.runtime.test
     def test_private_subnet_tags(self):
@@ -62,7 +64,9 @@ class TestingNetwork(unittest.TestCase):
 
         def check_private_subnet_tags(args):
             urn, tags = args
-            assert tags, f'Subnet {urn} must have tags'
-            assert "Name" in tags, f'Subnet {urn} must have a Name tag'
+            assert tags, f"Subnet {urn} must have tags"
+            assert "Name" in tags, f"Subnet {urn} must have a Name tag"
 
-        return pulumi.Output.all(network.private_subnets[0].urn, network.public_subnets[0].tags).apply(check_private_subnet_tags)
+        return pulumi.Output.all(
+            network.private_subnets[0].urn, network.public_subnets[0].tags
+        ).apply(check_private_subnet_tags)
