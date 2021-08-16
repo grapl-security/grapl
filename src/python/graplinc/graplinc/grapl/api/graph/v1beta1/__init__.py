9a7adfadc7da44b4dddecd4ee3181cce1a165c7c
Max Wittek <wimax@graplsecurity.com>
2021-08-16 10:00:30 -0700

    Remove src/python/graplinc; clean up protobuf codegen

--- src/python/graplinc/graplinc/grapl/api/graph/v1beta1/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file will make the directory be recognized as a Python
-# package. Pants will drop additional Python modules into this directory
-# when it does Protobuf code generation, but we are responsible for
-# making sure the whole thing is a proper package.
-#
-# See https://www.pantsbuild.org/docs/protobuf for more.
