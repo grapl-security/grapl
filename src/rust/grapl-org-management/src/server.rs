906c807649eab6ca9891433a0047881e32b8cb3f
andrea-grapl <andrea@graplsecurity.com>
2021-08-25 02:41:18 -0600

    start adding ddb

--- src/rust/grapl-org-management/src/server.rs
+++ src/rust/grapl-org-management/src/server.rs
@@ -1,11 +1,14 @@
 use tonic::{transport::Server, Request, Response, Status};
+use uuid::Uuid;
+
+
 use crate::org_management::organization_manager_server::{OrganizationManager, OrganizationManagerServer};
 use crate::org_management;
 use org_management::{CreateOrgReply, CreateOrgRequest};
 
-
 #[derive(Debug, Default)]
 pub struct Organization {}
+pub struct User {}
 
 #[tonic::async_trait]
 impl OrganizationManager for Organization {
@@ -13,15 +16,20 @@ impl OrganizationManager for Organization {
         &self,
         request: Request<CreateOrgRequest>,
     ) -> Result<Response<CreateOrgReply>, Status> {
-        println!("Got a request: {:?}", request);
+        println!("Org request data: {:?}", request); // don't actually print this
+
+        let org_id =  Uuid::new_v4();
+
+        // store data in dynamo db with new org id
 
         let reply = CreateOrgReply {
-            message: format!("Hello {}!", request.into_inner().name).into(),
+            organization_id: format!("Org Id {} Created", org_id).into(),
         };
 
         Ok(Response::new(reply))
     }
 }
+
 
 pub async fn start_server() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
