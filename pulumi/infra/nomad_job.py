e90dccd3eb8f9a2495f7becc949bf70373496111
Thomas Wunderlich <thomas@graplsecurity.com>
2021-08-26 11:42:59 -0400

    Refactor file to string functionality into a small helper function, and switch the path to relativ so pulumi can be executed outside of the container

--- pulumi/infra/nomad_job.py
+++ pulumi/infra/nomad_job.py
@@ -8,14 +8,17 @@ class NomadJob(pulumi.ComponentResource):
     def __init__(self, name: str, vars: Dict[str, str], opts: Optional[pulumi.ResourceOptions] = None) -> None:
         super().__init__("grapl:NomadJob", name, None, opts)
 
-        nomad_file = "/home/grapl/nomad/grapl-core.nomad"
-        with open(nomad_file) as f:
-            jobspec = f.read()
-
         self.grapl_core = nomad.Job(
             resource_name=f"{DEPLOYMENT_NAME}-grapl-core-job",
-            jobspec=jobspec,
+            jobspec=self._file("../../nomad/grapl-core.nomad"),
             hcl2=nomad.JobHcl2Args(enabled=True, vars=vars),
             opts=pulumi.ResourceOptions(parent=self)
         )
+
+    def _file(self, nomad_file: str):
+        with open(nomad_file) as f:
+            jobspec = f.read()
+            f.close()
+            return jobspec
+
 
