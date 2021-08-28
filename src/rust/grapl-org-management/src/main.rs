7676d0a0ab9659af4c0d1e982af6c90867aa740a
andrea-grapl <andrea@graplsecurity.com>
2021-08-28 15:50:31 -0600

    start adding sql

--- src/rust/grapl-org-management/src/main.rs
+++ src/rust/grapl-org-management/src/main.rs
@@ -1,9 +1,25 @@
+use sqlx::postgres::PgPoolOptions;
 use grapl_org_management::server;
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     server::start_server().await?;
+    create_db_connection().await?;
     println!("Test");
+    Ok(())
+}
+
+async fn create_db_connection() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:password@localhost/test").await?;
+
+    // Make a simple query to return the given parameter
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool).await?;
+
+    assert_eq!(row.0, 150);
+
     Ok(())
 }
