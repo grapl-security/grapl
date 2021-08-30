9f2e3bd10cec1da53eefaf66b505f8e8a378f89c
Thomas Wunderlich <thomas@graplsecurity.com>
2021-08-30 17:25:22 -0400

    Use readonly mode when opening a file and rename method

--- pulumi/infra/nomad_job.py
+++ pulumi/infra/nomad_job.py
@@ -17,13 +17,13 @@ class NomadJob(pulumi.ComponentResource):
 
         self.grapl_core = nomad.Job(
             resource_name=f"{DEPLOYMENT_NAME}-grapl-core-job",
-            jobspec=self._file("../../nomad/grapl-core.nomad"),
+            jobspec=self._file_contents("../../nomad/grapl-core.nomad"),
             hcl2=nomad.JobHcl2Args(enabled=True, vars=vars),
             opts=pulumi.ResourceOptions(parent=self),
         )
 
-    def _file(self, nomad_file: str) -> str:
-        with open(nomad_file) as f:
+    def _file_contents(self, nomad_file: str) -> str:
+        with open(nomad_file, "r") as f:
             jobspec = f.read()
             f.close()
             return jobspec
