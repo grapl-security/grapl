9470472db940424e8a7ba1a7c0bb885d5baf66d8
andrea-grapl <andrea@graplsecurity.com>
2021-08-31 16:55:50 -0600

    fixed connection pools, removed default prev from ddb, will be working on sql queries next

--- src/rust/grapl-org-management/src/main.rs
+++ src/rust/grapl-org-management/src/main.rs
@@ -1,17 +1,11 @@
-use sqlx::postgres::PgPoolOptions;
+// use sqlx::postgres::PgPoolOptions;
 use grapl_org_management::server;
-use sqlx::{Postgres, Pool};
+// use sqlx::{Postgres, Pool};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    server::start_server().await?;
-
-    let row: (i64, ) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&pool).await?;
-
-    assert_eq!(row.0, 150);
-
-    println!("Test");
+    server::start_server()
+        .await?;
+    
     Ok(())
 }
--- src/rust/grapl-org-management/src/server.rs
+++ src/rust/grapl-org-management/src/server.rs
@@ -4,12 +4,13 @@ use uuid::Uuid;
 use crate::org_management::organization_manager_server::{OrganizationManager, OrganizationManagerServer};
 use crate::org_management;
 use org_management::{CreateOrgReply, CreateOrgRequest};
-use sqlx::{Pool, Postgres};
-use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool};
+use sqlx::postgres::{PgPoolOptions, Postgres};
 
-#[derive(Debug, Default)]
+
+#[derive(Debug)]
 pub struct OrganizationManagerRpc {
-    pool: Pool<Postgres>
+    pool: Pool<Postgres>,
 }
 
 #[tonic::async_trait]
@@ -20,14 +21,14 @@ impl OrganizationManager for OrganizationManagerRpc {
     ) -> Result<Response<CreateOrgReply>, Status> {
         println!("Org request data: {:?}", request); // don't actually print this
 
-        let org_id =  Uuid::new_v4();
-        let admin_id =  Uuid::new_v4();
+        let org_id = Uuid::new_v4();
+        let admin_id = Uuid::new_v4();
 
-        // store data in dynamo db with new org id
+        // store data in sql with new org id
 
         let reply = CreateOrgReply {
             organization_id: format!("Org Id {} Created", org_id).into(),
-            admin_user_id:  format!("Org Id {} Created", admin_id).into(),
+            admin_user_id: format!("Org Id {} Created", admin_id).into(),
         };
 
         Ok(Response::new(reply))
@@ -37,10 +38,12 @@ impl OrganizationManager for OrganizationManagerRpc {
 
 pub async fn start_server() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let org = OrganizationManagerRpc::default();
-
     let pool =
-        create_db_connection().await?; // move into this file, 
+        create_db_connection()
+            .await?;
+    let org = OrganizationManagerRpc{pool};
+
+
 
     Server::builder()
         .add_service(OrganizationManagerServer::new(org))
@@ -54,6 +57,8 @@ async fn create_db_connection() -> Result<Pool<Postgres>, sqlx::Error> {
     let url = std::env::var("POSTGRES_URL").expect("POSTGRES_URL");
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect(&url).await?;
+        .connect(&url)
+        .await?;
+
     Ok(pool)
 }
