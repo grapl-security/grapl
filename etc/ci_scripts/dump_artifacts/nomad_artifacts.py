8e8502a42806b5f7519ad2b8389d68956b797a25
Max Wittek <wimax@graplsecurity.com>
2021-09-07 15:37:17 -0700

    Introduce python-nomad to codebase

--- etc/ci_scripts/dump_artifacts/nomad_artifacts.py
+++ etc/ci_scripts/dump_artifacts/nomad_artifacts.py
@@ -3,6 +3,7 @@
 import logging
 import shutil
 import sys
+import nomad
 from pathlib import Path
 
 LOGGER = logging.getLogger(__name__)
@@ -10,8 +11,9 @@ LOGGER.setLevel(logging.INFO)
 LOGGER.addHandler(logging.StreamHandler(stream=sys.stdout))
 
 
+nomad_agent_log_path = Path("/tmp/nomad-agent.log").resolve()
+consul_agent_log_path = Path("/tmp/consul-agent.log").resolve()
+
 def dump_all_nomad_consul_logs(artifacts_dir: Path) -> None:
-    nomad_log_path = Path("/tmp/nomad-agent.log").resolve()
-    consul_log_path = Path("/tmp/consul-agent.log").resolve()
-    shutil.copy2(nomad_log_path, artifacts_dir)
-    shutil.copy2(consul_log_path, artifacts_dir)
+    shutil.copy2(nomad_agent_log_path, artifacts_dir)
+    shutil.copy2(consul_agent_log_path, artifacts_dir)
