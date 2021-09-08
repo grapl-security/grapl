eec8e83ab9141addceead3f5d710d2396ad98c4d
wimax-grapl <69007229+wimax-grapl@users.noreply.github.com>
2021-09-08 17:23:06 -0400

    Kafka now works in Nomad with Pulumi (#1161)
    * Getting closer, but the Kafka tests (obviously) dont work. Annoying.
    
    * git add -u! approaching kafka usability but it keeps not setting up the controller correctly
    
    * ok it finally works haha
    
    * remove a TODO, add a TODO

--- pulumi/grapl/__main__.py
+++ pulumi/grapl/__main__.py
@@ -140,10 +140,7 @@ def main() -> None:
         analyzer_executor_queue = ServiceQueue("analyzer-executor")
         analyzer_executor_queue.subscribe_to_emitter(dispatched_analyzer_emitter)
 
-        # Temporarily disabled due to Nomad migration
-        # https://github.com/grapl-security/issue-tracker/issues/670
-        if False:
-            kafka = Kafka("kafka")
+        kafka = Kafka("kafka")
 
         grapl_core_job_vars = pulumi.Output.all(
             analyzer_bucket=analyzers_bucket.bucket,
