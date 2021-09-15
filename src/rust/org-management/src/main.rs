b15db3e530179f65ba85bc4e2c428390ce3517cb
andrea-grapl <andrea@graplsecurity.com>
2021-09-14 20:26:27 -0600

    working in docker

--- src/rust/org-management/src/main.rs
+++ src/rust/org-management/src/main.rs
@@ -1,9 +1,9 @@
-// use sqlx::postgres::PgPoolOptions;
 use org_management::server;
-// use sqlx::{Postgres, Pool};
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
     let (_env, _guard) = grapl_config::init_grapl_env!();
     println!("TESTING: TODO REMOVE THIS println");
     tracing::info!(message="Started org-manager");
