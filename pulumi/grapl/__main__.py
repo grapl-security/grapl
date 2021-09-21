85c325d7aebfadd5c0774cbfebf7f3e58002ff85
Max Wittek <wimax@graplsecurity.com>
2021-09-20 21:18:41 -0700

    Format

--- pulumi/grapl/__main__.py
+++ pulumi/grapl/__main__.py
@@ -36,6 +36,7 @@ from infra.osquery_generator import OSQueryGenerator
 from infra.pipeline_dashboard import PipelineDashboard
 from infra.provision_lambda import Provisioner
 from infra.quiet_docker_build_output import quiet_docker_output
+
 # TODO: temporarily disabled until we can reconnect the ApiGateway to the new
 # web UI.
 # from infra.secret import JWTSecret, TestUserPassword
