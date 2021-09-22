59bf8f6c1e5df1ca00cf51b63ee8a1c67a2b124a
Max Wittek <wimax@graplsecurity.com>
2021-09-21 23:43:41 -0700

    format

--- pulumi/grapl/__main__.py
+++ pulumi/grapl/__main__.py
@@ -221,7 +221,7 @@ def main() -> None:
 
                 integration_test_only_job_vars = {
                     "_kafka_endpoint": kafka_endpoint,
-                    "grapl_root": os.environ["GRAPL_ROOT"]
+                    "grapl_root": os.environ["GRAPL_ROOT"],
                 }
                 return {**subset, **integration_test_only_job_vars}
 
