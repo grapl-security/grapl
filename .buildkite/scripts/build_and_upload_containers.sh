1a53a72e3b616d5f7a97f1a9b78dcb9f4e4f813b
jgrillo <jgrillo@graplsecurity.com>
2021-09-23 19:07:05 +0100

    remove some unused makefile targets, try to make the merge pipeline work

--- .buildkite/scripts/build_and_upload_containers.sh
+++ .buildkite/scripts/build_and_upload_containers.sh
@@ -41,8 +41,8 @@ cloudsmith_tag() {
     local -r tag="${2}"
     echo "${CLOUDSMITH_DOCKER_REGISTRY}/${service}:${tag}"
 }
-echo "--- :python: Building required Python wheels"
-make build-python-wheels
+echo "--- :python: Building analyzer-executor pex binary"
+make build-analyzer-executor
 
 for service in "${services[@]}"; do
     # Build a single service
