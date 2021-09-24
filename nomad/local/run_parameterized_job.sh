e099a1017a6bb7eb8d1e0ae177e3ba72b430d432
Max Wittek <wimax@graplsecurity.com>
2021-09-24 15:15:36 -0700

    Alright - integration tests WORK

--- nomad/local/run_parameterized_job.sh
+++ nomad/local/run_parameterized_job.sh
@@ -10,6 +10,7 @@ source "${THIS_DIR}/nomad_cli_tools.sh"
 # Pulumi __main__.py's `NomadJob(...)` calls.
 
 readonly job_to_dispatch="${1}"
+readonly mins_to_wait="${2}"
 
 # Now we have to actually dispatch a job; Pulumi simply uploaded
 # the jobspec, since it's a Parameterized Batch Job.
@@ -21,7 +22,7 @@ echo "You can view job progress at $(url_to_nomad_job_in_ui "${job_id}")"
 dispatch_timed_out=0
 await_nomad_dispatch_finish \
     "${job_id}" \
-    $((6 * 60)) \
+    $(($mins_to_wait * 60)) \
     "${job_to_dispatch}" ||
     dispatch_timed_out=1
 
