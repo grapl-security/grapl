42d8ed939686d3c502fc5074f23f91adf85f1940
Thomas Wunderlich <thomas@graplsecurity.com>
2021-10-06 12:51:21 -0400

    Remove remains of grapl-network

--- nomad/local/nomad_run_local_infra.sh
+++ nomad/local/nomad_run_local_infra.sh
@@ -9,9 +9,6 @@ echo "--- Deploying Nomad local infrastructure."
 # shellcheck disable=SC2016
 timeout 120 bash -c -- 'while [[ -z $(nomad status 2>&1 | grep running) ]]; do printf "Waiting for nomad-agent\n";sleep 1;done'
 
-# Before we deploy Localstack, we need to ensure the Docker network exists
-docker network create grapl-network || true
-
 nomad job run \
     -var "KAFKA_JMX_PORT=${KAFKA_JMX_PORT}" \
     -var "KAFKA_BROKER_PORT=${KAFKA_BROKER_PORT}" \
