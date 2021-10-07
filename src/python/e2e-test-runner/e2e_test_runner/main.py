fb15127803dfdad3841ef1f41f9ad9f4f27a8f8f
Max Wittek <wimax@graplsecurity.com>
2021-10-06 17:18:23 -0700

    jesus christ, moving e2e to a pex is hard

--- src/python/e2e-test-runner/e2e_test_runner/main.py
+++ src/python/e2e-test-runner/e2e_test_runner/main.py
@@ -1,7 +1,4 @@
-import os
-import shutil
 import sys
-from typing import Any
 
 from grapl_common.debugger.vsc_debugger import wait_for_vsc_debugger
 from grapl_common.grapl_logger import get_module_grapl_logger
@@ -9,14 +6,8 @@ from grapl_common.grapl_logger import get_module_grapl_logger
 LOGGER = get_module_grapl_logger()
 
 
-def lambda_handler(event: Any, context: Any) -> None:
+def main() -> None:
     wait_for_vsc_debugger("grapl_e2e_tests")
-    LOGGER.info("staging schemas")
-    if os.path.exists("/tmp/schemas"):
-        shutil.rmtree("/tmp/schemas")
-    os.mkdir("/tmp/schemas")
-    shutil.copyfile("e2e_test_runner/schemas.py", "/tmp/schemas/schemas.py")
-    LOGGER.info("staged schemas")
 
     LOGGER.info("executing pytest")
 
@@ -26,7 +17,4 @@ def lambda_handler(event: Any, context: Any) -> None:
 
     LOGGER.info(f"tests completed with status code {result}")
 
-    if result == 0:
-        return  # Lambda runtime will flip out if sys.exit(0) -_-
-    else:
-        sys.exit(result)
+    sys.exit(result)
