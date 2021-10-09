7157773a041a1cecd4194a3d6bf104422c8a6415
Max Wittek <wimax@graplsecurity.com>
2021-10-08 17:03:57 -0700

    Okay, now I have a robust nomad job plan thing

--- nomad/local/nomad_run_local_infra.sh
+++ nomad/local/nomad_run_local_infra.sh
@@ -5,6 +5,16 @@ THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
 GRAPL_ROOT="$(git rev-parse --show-toplevel)"
 NOMAD_FILE="${GRAPL_ROOT}/nomad/local/grapl-local-infra.nomad"
 
+declare -a NOMAD_VARS=(
+    -var "KAFKA_JMX_PORT=${KAFKA_JMX_PORT}"
+    -var "KAFKA_BROKER_PORT=${KAFKA_BROKER_PORT}"
+    -var "KAFKA_BROKER_PORT_FOR_HOST_OS=${KAFKA_BROKER_PORT_FOR_HOST_OS}"
+    -var "LOCALSTACK_PORT=${LOCALSTACK_PORT}"
+    -var "ZOOKEEPER_PORT=${ZOOKEEPER_PORT}"
+    -var "FAKE_AWS_ACCESS_KEY_ID=${FAKE_AWS_ACCESS_KEY_ID}"
+    -var "FAKE_AWS_SECRET_ACCESS_KEY=${FAKE_AWS_SECRET_ACCESS_KEY}"
+)
+
 # shellcheck source-path=SCRIPTDIR
 source "${THIS_DIR}/nomad_cli_tools.sh"
 
@@ -14,14 +24,23 @@ echo "--- Deploying Nomad local infrastructure."
 # shellcheck disable=SC2016
 timeout 60 bash -c -- 'while [[ -z $(nomad node status 2>&1 | grep ready) ]]; do printf "Waiting for nomad-agent\n";sleep 1;done'
 
+# Okay, now the Nomad agent is up, but it might not be ready to accept jobs.
+# Just loop on `nomad job plan` until it can.
+attemptPlan() {
+    # shellcheck disable=SC2068
+    nomad job plan ${NOMAD_VARS[@]} "${NOMAD_FILE}" > /dev/null 2>&1
+    echo "$?"
+}
+
+# fyi: Exit code 1 means "Allocations created or destroyed" and is what we want
+while [[ $(attemptPlan) -ne 1 ]]; do
+    echo "Waiting to be able to do a nomad-job-run"
+    sleep 1
+done
+
+# shellcheck disable=SC2068
 nomad job run -verbose \
-    -var "KAFKA_JMX_PORT=${KAFKA_JMX_PORT}" \
-    -var "KAFKA_BROKER_PORT=${KAFKA_BROKER_PORT}" \
-    -var "KAFKA_BROKER_PORT_FOR_HOST_OS=${KAFKA_BROKER_PORT_FOR_HOST_OS}" \
-    -var "LOCALSTACK_PORT=${LOCALSTACK_PORT}" \
-    -var "ZOOKEEPER_PORT=${ZOOKEEPER_PORT}" \
-    -var "FAKE_AWS_ACCESS_KEY_ID=${FAKE_AWS_ACCESS_KEY_ID}" \
-    -var "FAKE_AWS_SECRET_ACCESS_KEY=${FAKE_AWS_SECRET_ACCESS_KEY}" \
+    ${NOMAD_VARS[@]} \
     "${NOMAD_FILE}"
 
 echo "Nomad Job Run complete, checking for task failures"
