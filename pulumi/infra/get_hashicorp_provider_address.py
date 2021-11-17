87c7835de218dba12de39272475cd9cb3f2099f1
Thomas Wunderlich <thomas@graplsecurity.com>
2021-11-17 13:51:33 -0500

    Fix typing for providers

--- pulumi/infra/get_hashicorp_provider_address.py
+++ pulumi/infra/get_hashicorp_provider_address.py
@@ -9,7 +9,7 @@ def get_hashicorp_provider_address(
     pulumi_class: Any,
     provider_type: str,
     stack: pulumi.StackReference,
-    **additional_arguments: Optional[str]
+    additional_arguments: Optional[dict] = None,
 ) -> Any:
     """
     This supports getting a Provider object with an explicit address set.
@@ -18,7 +18,7 @@ def get_hashicorp_provider_address(
     :param pulumi_class: Should be one of pulumi_consul or pulumi_nomad
     :param provider_type: One of "consul", "nomad"
     :param stack: The corresponding stack reference
-    :param additional_arguments: dict of keyword settings for the provider. ie for consul you can pass in token: "NOT_A_REAL_TOKEN"
+    :param additional_arguments: dict of keyword settings for the provider. ie for consul you can pass in {token: "NOT_A_REAL_TOKEN"}
     :return: pulumi.providerReference. Should be used in opts
     """
     override_address = pulumi.Config(provider_type).get("address")
