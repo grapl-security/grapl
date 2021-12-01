c9c58f631a591f279fa3e4ea42d26b87c4e13a4d
jgrillo <jgrillo@graplsecurity.com>
2021-12-01 09:33:57 -0500

    WIP trying to consume stack output

--- pulumi/grapl/__main__.py
+++ pulumi/grapl/__main__.py
@@ -1,3 +1,4 @@
+import asyncio
 import sys
 from pathlib import Path
 from typing import Any, Mapping, Set, cast
@@ -189,15 +190,24 @@ def main() -> None:
         user_session_table=dynamodb_tables.user_session_table.name,
     )
 
-    kafka = Kafka(
-        "kafka",
-        confluent=ConfluentOutput.from_json(
-            cast(
-                pulumi.Output[Mapping[str, Any]],
-                StackReference("grapl/ccloud-bootstrap/ccloud-bootstrap").get_output("confluent"),
+    confluent_stack_output: ConfluentOutput = (
+        asyncio.get_running_loop().run_until_complete(
+            ConfluentOutput.from_json(
+                cast(
+                    pulumi.Output[Mapping[str, Any]],
+                    StackReference(
+                        "grapl/ccloud-bootstrap/ccloud-bootstrap"
+                    ).get_output("confluent"),
+                )
             )
+        )
+    )
+
+    Kafka(
+        "kafka",
+        confluent_environment=confluent_stack_output.get_environment(
+            pulumi_config.require("confluent-environment-name")
         ),
-        confluent_environment_name=pulumi_config.require("confluent-environment-name"),
     )
 
     if config.LOCAL_GRAPL:
