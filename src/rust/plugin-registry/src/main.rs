f57b2f692773858ad69eb0505938f2406aad5688
Colin O'Brien <colin@graplsecurity.com>
2021-12-01 14:26:47 -0800

    Add a main stub

--- src/rust/plugin-registry/src/main.rs
+++ src/rust/plugin-registry/src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use plugin_registry::server::exec_service;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (_env, _guard) = grapl_config::init_grapl_env!();
+    exec_service().await?;
+    Ok(())
+}
