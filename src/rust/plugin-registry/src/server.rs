dfbee9f93c7be4e449eeeaab81b602356d30dc03
Colin O'Brien <colin@graplsecurity.com>
2021-12-01 17:26:32 -0800

    Add a basic implementation of CreatePlugin

--- src/rust/plugin-registry/src/server.rs
+++ src/rust/plugin-registry/src/server.rs
@@ -1,3 +1,10 @@
+#![allow(warnings)]
+
+use rusoto_s3::{
+    PutObjectRequest,
+    S3Client,
+    S3,
+};
 use rust_proto::plugin_registry::{
     plugin_registry_service_server::{
         PluginRegistryService,
@@ -38,7 +45,10 @@ use tonic::{
 #[derive(Debug, thiserror::Error)]
 pub enum PluginRegistryServiceError {}
 
-pub struct PluginRegistry {}
+pub struct PluginRegistry {
+    pool: sqlx::PgPool,
+    s3: S3Client,
+}
 
 impl PluginRegistry {
     #[allow(dead_code)]
@@ -46,6 +56,46 @@ impl PluginRegistry {
         &self,
         _request: CreatePluginRequest,
     ) -> Result<CreatePluginResponse, PluginRegistryServiceError> {
+        let mut hasher = blake3::Hasher::new();
+        hasher.update(_request.tenant_id.as_bytes());
+        hasher.update(_request.plugin_artifact.as_slice());
+        let artifact_id = hasher.finalize().to_hex();
+
+        let s3_key = format!(
+            "bucketname/{}/{}-plugins/{}.bin",
+            _request.tenant_id,
+            _request.plugin_type.type_name(),
+            &artifact_id,
+        );
+
+        self.s3.put_object(PutObjectRequest {
+            content_length: Some(_request.plugin_artifact.len() as i64),
+            body: Some(_request.plugin_artifact.into()),
+            bucket: "todo!".to_string(),
+            key: s3_key.clone(),
+            expected_bucket_owner: Some("todo!".to_string()),
+            metadata: None,
+            ..Default::default()
+        });
+
+        sqlx::query(
+            r"
+            INSERT INTO plugin_artifacts (
+                artifact_id,
+                artifact_version,
+                artifact_s3_key
+            )
+            VALUES ($1, $2, $3)
+            ON CONFLICT DO NOTHING;
+            ",
+        )
+        .bind(artifact_id.as_str())
+        .bind(0)
+        .bind(s3_key)
+        .fetch_one(&self.pool)
+        .await
+        .expect("todo");
+
         todo!()
     }
 
@@ -142,7 +192,7 @@ pub async fn exec_service() -> Result<(), Box<dyn std::error::Error>> {
         .await;
 
     let addr = "[::1]:50051".parse().unwrap();
-    let plugin_work_queue = PluginRegistry {};
+    let plugin_work_queue: PluginRegistry = todo!();
 
     tracing::info!(
         message="HealthServer + PluginRegistry listening",
