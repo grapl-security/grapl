4aacaf2077d540cd0694c6c79912c48388af6181
Max Wittek <wimax@graplsecurity.com>
2021-12-06 12:54:38 -0500

    Remove the lib file. also i dont love readonly

--- .buildkite/scripts/grapl_testing_stack/run_parameterized_job.sh
+++ .buildkite/scripts/grapl_testing_stack/run_parameterized_job.sh
@@ -8,18 +8,24 @@
 ##########
 set -euo pipefail
 
-# shellcheck source-path=SCRIPTDIR
-source "$(dirname "${BASH_SOURCE[0]}")/lib.sh"
+STACK="grapl/nomad/testing"
+readonly STACK
+
+REPOSITORY_ROOT=$(git rev-parse --show-toplevel)
+readonly REPOSITORY_ROOT
+
+_NOMAD_ADDRESS=$(pulumi stack output address --stack="${STACK}")
+readonly _NOMAD_ADDRESS
 
 # Ensure we call dumpArtifacts even after test failure, and return exit code from
 # the test, not the stop command.
 dump_artifacts() {
     (
         cd "${REPOSITORY_ROOT}" &&
-            NOMAD_ADDRESS="${NOMAD_ADDRESS}" make dump-artifacts
+            NOMAD_ADDRESS="${_NOMAD_ADDRESS}" make dump-artifacts
     )
 }
 trap dump_artifacts EXIT
 
-NOMAD_ADDRESS="${NOMAD_ADDRESS}" \
+NOMAD_ADDRESS="${_NOMAD_ADDRESS}" \
     "${REPOSITORY_ROOT}/nomad/bin/run_parameterized_job.sh" "${@}"
