c0b19cbafce4beab0812ef63691778b6f3c01fcc
Colin O'Brien <colin@graplsecurity.com>
2021-12-31 02:51:43 -0800

    Revert "Move public key into aad. Standardize on json for aad encoding."
    This reverts commit 0f2f83ded1819194e55ced67a79b442e83c6607d.

--- src/rust/grapl-crypto/src/aead/xchacha_blake3.rs
+++ src/rust/grapl-crypto/src/aead/xchacha_blake3.rs
@@ -29,9 +29,7 @@ pub enum AeadError {
     #[error("AuthenticationError")]
     AuthenticationError,
     #[error("LoopError")]
-    LoopError,
-    #[error("InvalidAadFormat")]
-    InvalidAad(#[from] serde_json::Error)
+    LoopError
 }
 
 impl From<chacha20::cipher::errors::LoopError> for AeadError {
@@ -61,11 +59,6 @@ impl Hasher {
     }
 }
 
-pub trait Aad: serde::Serialize + serde::de::DeserializeOwned {
-    fn recipient(&self) -> &str;
-    fn sender(&self) -> &str;
-}
-
 #[derive(Clone)]
 pub struct EncryptedData {
     ciphertext: Vec<u8>,
@@ -89,21 +82,19 @@ impl ChaChaBlake3 {
         }
     }
 
-    pub fn encrypt<A: Aad>(
+    pub fn encrypt(
         &mut self,
         msg: Vec<u8>,
-        aad: A,
+        aad: &[u8],
         key: &[u8; KEY_SIZE],
     ) -> Result<EncryptedData, AeadError> {
-        let aad = serde_json::to_vec(&aad)?;
-
         let mut nonce = [0; TOTAL_NONCE_SIZE];
         OsRng.fill_bytes(&mut nonce);
 
         let (ciphertext, mac) = self.encrypt_raw(
             msg,
             &nonce,
-            &aad,
+            aad,
             &key,
         )?;
 
@@ -217,21 +208,32 @@ mod tests {
     use rand_core::{OsRng, RngCore};
     use quickcheck_macros::quickcheck;
 
-    #[derive(serde::Serialize, serde::Deserialize)]
-    struct MyAad {
-        recipient: String,
-        sender: String,
-        other_data: Vec<u8>,
-    }
+    #[quickcheck]
+    fn encrypt_decrypt_raw(msg: Vec<u8>, aad: Vec<u8>) {
+        let mut aead = ChaChaBlake3::new();
+        if msg.is_empty() { return; }
+        let mut nonce = [0; TOTAL_NONCE_SIZE];
+        OsRng.fill_bytes(&mut nonce);
+        let mut key = [0; 32];
+        OsRng.fill_bytes(&mut key);
 
-    impl Aad for MyAad {
-        fn recipient(&self) -> &str {
-            &self.recipient
-        }
+        let (encrypted, mac) = aead.encrypt_raw(
+            msg.clone(),
+            &nonce,
+            &aad,
+            &key,
+        ).expect("encrypted failed");
+        let decrypted = aead.decrypt_raw(
+            encrypted.clone(),
+            &mac,
+            &nonce,
+            &aad,
+            &key,
+        ).expect("decrypted failed");
 
-        fn sender(&self) -> &str {
-            &self.sender
-        }
+        assert_ne!(encrypted, decrypted);
+        assert_ne!(msg, encrypted);
+        assert_eq!(msg, decrypted);
     }
 
     #[quickcheck]
@@ -242,15 +244,9 @@ mod tests {
         let mut key = [0; 32];
         OsRng.fill_bytes(&mut key);
 
-        let aad = MyAad {
-            recipient: "encrypt_decrypt_recipient".to_owned(),
-            sender: "encrypt_decrypt_sender".to_owned(),
-            other_data: aad,
-        };
-
         let encrypted = aead.encrypt(
             msg.clone(),
-            aad,
+            &aad,
             &key,
         ).expect("encrypted: failed");
 
@@ -272,15 +268,9 @@ mod tests {
         let mut key = [0; 32];
         OsRng.fill_bytes(&mut key);
 
-        let aad = MyAad {
-            recipient: "encrypt_decrypt_fail_aad_recipient".to_owned(),
-            sender: "encrypt_decrypt_fail_aad_sender".to_owned(),
-            other_data: aad,
-        };
-
         let mut encrypted = aead.encrypt(
             msg.clone(),
-            aad,
+            &aad,
             &key,
         ).expect("encrypted: failed");
 
@@ -306,15 +296,9 @@ mod tests {
         let mut key = [0; 32];
         OsRng.fill_bytes(&mut key);
 
-        let aad = MyAad {
-            recipient: "encrypt_decrypt_fail_cipher_recipient".to_owned(),
-            sender: "encrypt_decrypt_fail_cipher_sender".to_owned(),
-            other_data: aad,
-        };
-
         let mut encrypted = aead.encrypt(
             msg.clone(),
-            aad,
+            &aad,
             &key,
         ).expect("encrypted: failed");
 
