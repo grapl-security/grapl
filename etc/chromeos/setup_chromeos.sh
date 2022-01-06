3d630a3c352837905c9e09073fb6b5dab8e67aa9
wimax-grapl <69007229+wimax-grapl@users.noreply.github.com>
2022-01-05 20:19:08 -0500

    setup_chromeos: fix nvm and pyenv installs on base chromeos; enable -euo pipefail (#1338)
    * setup_chromeos: fix nvm and pyenv installs on base chromeos. enable -euo pipefail.
    
    * Format. Add docker-compose.
    
    * pass linter
    
    * Add echo_banner(), fix cni_plugins, change docker-compose to curl install
    
    * wrap VERSION with {}

--- etc/chromeos/setup_chromeos.sh
+++ etc/chromeos/setup_chromeos.sh
@@ -2,13 +2,14 @@
 # This is a WIP set up script for chromeOS. Some commands may break by the time the script is next used
 # TODO ensure idempotency
 
+set -euo pipefail
+
 source etc/chromeos/lib/installs.sh
 
 echo "Starting ChromeOS automated setup"
 update_linux
 fix_shell_completion
 install_build_tooling
-install_docker
 install_rust_and_utilities
 install_pyenv
 install_nvm
@@ -19,3 +20,4 @@ install_hashicorp_tools
 install_cni_plugins
 install_nomad_chromeos_workaround
 install_git_hooks
+install_docker # Do this last since it's not idempotent
