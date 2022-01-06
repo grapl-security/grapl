966b365c41f9607e60852426685bed399f1b576f
wimax-grapl <69007229+wimax-grapl@users.noreply.github.com>
2022-01-06 16:58:18 -0500

    Precommit will ignore files that are not pants-managed (#1345)
    * Precommit will ignore files that are not pants-managed
    
    * Used pants list --changed-since instead
    
    * cm requests. im curious why thomas didn't do this in the first place given it was a comment at the bottom of the file

--- etc/hooks/pre-commit.sh
+++ etc/hooks/pre-commit.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 
-# The sed here is to deal with any files with spaces in the names
-FILES=$(git diff --cached --name-only --diff-filter=ACMR | sed 's| |\\ |g')
-[ -z "$FILES" ] && exit 0
-
-# Prettify all selected files
-echo "$FILES" | xargs ./pants fmt
+# Prettify all changed files managed by Pants
+./pants fmt --changed-since=HEAD
 
 # Add back the modified files to staging
-echo "$FILES" | xargs git add
+git add --update
 
 exit 0
-
-# This runs pants formatting and adds it to the commit
-#./pants --changed-since=HEAD fmt
