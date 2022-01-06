620fc60e192b03a95119d86235f093c9c28a6083
Max Wittek <wimax@graplsecurity.com>
2022-01-05 19:10:35 -0500

    Add echo_banner(), fix cni_plugins, change docker-compose to curl install

--- etc/chromeos/lib/installs.sh
+++ etc/chromeos/lib/installs.sh
@@ -12,6 +12,12 @@ source_profile() {
     source "$HOME/.profile"
 }
 
+echo_banner() {
+    echo -e "\n\n========================================"
+    echo "==> ${1} "
+    echo -e "========================================\n"
+}
+
 get_latest_release() {
     curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
         grep '"tag_name":' |                                          # Get tag line
@@ -26,33 +32,42 @@ update_linux() {
 
 fix_shell_completion() {
     # TODO add support for other shells like zsh
-    echo "Fix bash completion"
+    echo_banner "Fix bash completion"
     sudo apt-get install --reinstall bash-completion
 }
 
 install_build_tooling() {
-    echo "Install build tooling"
-    sudo apt install -y apt-utils build-essential libclang1
+    echo_banner "Install build tooling"
+    tools=(
+        apt-utils
+        build-essential
+        libclang1
+        lsb-release
+        software-properties-common # for `apt-add-repository``
+    )
+    sudo apt install -y "${tools[@]}"
 }
 
 # potentially replace with podman in the future?
 install_docker() {
-    echo "Install docker"
+    echo_banner "Install docker"
     curl -sSL https://get.docker.com/ | sh
     sudo usermod -a -G docker "$USER"
 
-    echo "Install docker-compose"
-    pip3 install docker-compose
+    echo_banner "Install docker-compose"
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+        -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 }
 
 install_rust_and_utilities() {
-    echo "Installing rust toolchain"
+    echo_banner "Installing rust toolchain"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     # Shellcheck can't follow $HOME or other vars like $USER so we disable the check here
     # shellcheck disable=SC1091
     source "$HOME/.cargo/env"
 
-    echo "Installing rust utilities (ripgrep, fd-find, dua and bat)"
+    echo_banner "Installing rust utilities (ripgrep, fd-find, dua and bat)"
     cargo install -f ripgrep
     cargo install -f fd-find
     cargo install -f dua
@@ -60,7 +75,7 @@ install_rust_and_utilities() {
 }
 
 install_pyenv() {
-    echo "Install pyenv and set python version to ${PYENV_PYTHON_VERSION}"
+    echo_banner "Install pyenv and set python version to ${PYENV_PYTHON_VERSION}"
     sudo apt-get install -y make libssl-dev zlib1g-dev libbz2-dev \
         libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
         xz-utils tk-dev libffi-dev liblzma-dev
@@ -102,7 +117,7 @@ install_pyenv() {
 }
 
 install_nvm() {
-    echo "Installing nvm"
+    echo_banner "Installing nvm"
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
     source_profile
 
@@ -117,54 +132,56 @@ install_nvm() {
 }
 
 install_awsv2() {
-    echo "Installing awscliv2"
+    echo_banner "Installing awscliv2"
     (
         cd /tmp
         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
         unzip awscliv2.zip
         sudo ./aws/install --update
-        sudo rm awscliv2.zip
+        sudo rm ./awscliv2.zip
+        sudo rm -rf ./aws
     )
 }
 install_pulumi() {
-    echo "Install pulumi"
+    echo_banner "Install pulumi"
     curl -fsSL https://get.pulumi.com | sh
 }
 
 install_utilities() {
-    echo "Install useful utilities"
+    echo_banner "Install useful utilities"
     sudo apt-get install -y jq dnsutils
 }
 
 install_hashicorp_tools() {
-    echo "Installing hashicorp tools: consul nomad packer"
-    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-    sudo apt-get install -y lsb-release software-properties-common # for `lsb_release` and `apt-add-repository``
+    echo_banner "Installing hashicorp tools: consul nomad packer"
+    curl --proto '=https' --tlsv1.3 -sSf https://apt.releases.hashicorp.com/gpg |
+        sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/hashicorp-apt.gpg --import &&
+        sudo chmod 644 /etc/apt/trusted.gpg.d/hashicorp-apt.gpg
     sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
     sudo apt-get update
     sudo apt-get install -y consul nomad packer
 }
 
 install_cni_plugins() {
-    echo "Installing CNI plugins required for Nomad bridge networks"
+    echo_banner "Installing CNI plugins required for Nomad bridge networks"
     sudo mkdir -p /opt/cni/bin
     cd /opt/cni/bin || exit 2
     VERSION=$(get_latest_release containernetworking/plugins)
-    sudo wget "https://github.com/containernetworking/plugins/releases/download/$VERSION/cni-plugins-linux-amd64-$VERSION.tgz"
-    curl -sL https://github.com/containernetworking/plugins/releases/latest | grep "linux-amd64" | grep -v "sha" | grep -v "span" | awk '{ print $2 }' | awk -F'"' '{ print "https://github.com"$2 }' | wget -qi -
-    sudo tar -xf "cni-plugins-linux-amd64-$VERSION.tgz"
-    sudo rm "cni-plugins-linux-amd64-$VERSION.tgz"
+    TGZ_NAME="cni-plugins-linux-amd64-${VERSION}.tgz"
+    sudo wget "https://github.com/containernetworking/plugins/releases/download/$VERSION/${TGZ_NAME}"
+    sudo tar -xf "${TGZ_NAME}"
+    sudo rm "${TGZ_NAME}"
 }
 
 install_nomad_chromeos_workaround() {
-    echo "Setting up workaround for Nomad linux fingerprinting bug"
+    echo_banner "Setting up workaround for Nomad linux fingerprinting bug"
     echo "See https://github.com/hashicorp/nomad/issues/10902 for more context"
     sudo mkdir -p "/lib/modules/$(uname -r)/"
     echo '_/bridge.ko' | sudo tee -a "/lib/modules/$(uname -r)/modules.builtin"
 }
 
 install_git_hooks() {
-    echo "Installing git hooks"
+    echo_banner "Installing git hooks"
     GIT_ROOT=$(git rev-parse --show-toplevel)
     ln --symbolic --relative --force "$GIT_ROOT/etc/hooks/pre-commit.sh" "$GIT_ROOT/.git/hooks/pre-commit"
 }
