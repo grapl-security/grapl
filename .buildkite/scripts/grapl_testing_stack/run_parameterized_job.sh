49f9c759877c42393fc6a8eac0380e911112f132
Max Wittek <wimax@graplsecurity.com>
2022-01-11 16:07:17 -0500

    Grapl Testing Stack dump_artifacts is now a Metahook post-command

--- .buildkite/scripts/grapl_testing_stack/run_parameterized_job.sh
+++ .buildkite/scripts/grapl_testing_stack/run_parameterized_job.sh
@@ -3,8 +3,6 @@
 ##########
 # Wrapper around `nomad/bin/run_parameterized_job.sh` that binds it to
 # the `grapl/nomad/testing` stack.
-
-# Automatically run `dumpArtifacts` afterwards.
 ##########
 set -euo pipefail
 
@@ -15,18 +13,6 @@ readonly REPOSITORY_ROOT
 
 _NOMAD_ADDRESS=$(pulumi stack output address --stack="${STACK}")
 readonly _NOMAD_ADDRESS
-
-# Ensure we call dumpArtifacts even after test failure, and return exit code from
-# the test, not the stop command.
-dump_artifacts() {
-    (
-        cd "${REPOSITORY_ROOT}"
-        NOMAD_ADDRESS="${_NOMAD_ADDRESS}" ./pants run \
-            ./etc/ci_scripts/dump_artifacts -- \
-            --no-dump-agent-logs
-    )
-}
-trap dump_artifacts EXIT
 
 NOMAD_ADDRESS="${_NOMAD_ADDRESS}" \
     "${REPOSITORY_ROOT}/nomad/bin/run_parameterized_job.sh" "${@}"
