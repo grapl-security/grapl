7f0ef38d801ec02cb04db3d79802080e988c5091
Max Wittek <wimax@graplsecurity.com>
2022-01-11 23:59:23 -0500

    remove one comment

--- src/rust/plugin-registry/tests/test_nomad_client.rs
+++ src/rust/plugin-registry/tests/test_nomad_client.rs
@@ -2,8 +2,6 @@
 
 use plugin_registry::nomad_client;
 
-/// For now, this is just a smoke test. This test can and should evolve as
-/// the service matures.
 #[test_log::test(tokio::test)]
 async fn test_nomad_client() -> Result<(), Box<dyn std::error::Error>> {
     let client = nomad_client::NomadClient::from_env();
