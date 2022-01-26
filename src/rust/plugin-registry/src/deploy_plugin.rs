4788bdacf2fe7268db841bcab24c8e33d3cb5304
Max Wittek <wimax@graplsecurity.com>
2022-01-26 17:25:17 -0500

    More commenting about next steps

--- src/rust/plugin-registry/src/deploy_plugin.rs
+++ src/rust/plugin-registry/src/deploy_plugin.rs
@@ -12,12 +12,7 @@ pub async fn deploy_plugin(
     client: nomad_client::NomadClient,
     plugin_id: uuid::Uuid,
 ) -> Result<(), nomad_client::NomadClientError> {
-    let namespace_name = "cool-namespace";
-    // 1. Deploy namespace
-    client.create_namespace(namespace_name).await?;
-    // TODO: What if the namespace already exists?
-
-    // 2. Convert HCL to JSON Job model
+    // Convert HCL to JSON Job model
     let job_file_hcl = static_files::PLUGIN_JOB;
     let job_file_vars: nomad_cli::NomadVars = HashMap::from([
         ("plugin_id".to_owned(), format!("{}", plugin_id)),
@@ -30,13 +25,20 @@ pub async fn deploy_plugin(
 
     // TODO: Should I "nomad job plan" to make sure we have enough memory/cpu for the task?
 
-    // 3. Start the job
+    // Deploy namespace
+    let namespace_name = "todo-fill-this-in-with-real-plugin-name";
+    client.create_namespace(namespace_name).await?;
+    // TODO: What if the namespace already exists?
+
+    // Start the job
     let _job = client
         .create_job(job, Some(namespace_name.to_owned()))
         .await?;
     // There's no guarantee that the job is *healthy*, just that it's been deployed.
 
-    // 4. If success, mark plugin as being deployed in `plugins` table
-    // TODO next CR
+    // If success, mark plugin as being deployed in `plugins` table
+
+    // TODO next CR. Right now all the plugins table interop is in the main 
+    //      server controller, gross!
     Ok(())
 }
