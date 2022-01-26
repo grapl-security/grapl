41acc6993ebb8bd3e6e844bf4b9cfa42733d6fe0
Colin O'Brien <colin@graplsecurity.com>
2022-01-26 09:40:50 -0800

    Improve some names. Simplify a bit of code. Assume directories exist already in OS.

--- src/rust/plugin-bootstrap/README.md
+++ src/rust/plugin-bootstrap/README.md
@@ -1,13 +1,24 @@
 ## Plugin Bootstrapping
 
-Plugin execution is broken up into two systemd services 'plugin-bootstrap-init' and 'plugin'.
+Plugin execution is broken up into two systemd services 'plugin-bootstrap-init'
+and 'plugin'.
 
-'plugin-bootstrap-init' executes once. It reaches out to the plugin-bootstrap service and
-retrieves the plugin binary and client certificate artifacts.
+'plugin-bootstrap-init' executes once. It reaches out to the plugin-bootstrap
+service and retrieves the plugin binary and client certificate artifacts.
 
 It writes them off to the disk and then exits.
 
-The 'plugin' service depends on the 'plugin-bootstrap-init' service and won't execute until
-'plugin-bootstrap-init' has completed.
+The 'plugin' service depends on the 'plugin-bootstrap-init' service and won't
+execute until 'plugin-bootstrap-init' has completed.
 
 The plugin service manages the execution of the plugin itself.
+
+### Notes
+
+The following paths are required for the init process to function properly.
+
+```
+/usr/local/bin/
+/etc/ssl/private/
+/etc/systemd/system/plugin.service.d/
+```
--- src/rust/plugin-bootstrap/src/init.rs
+++ src/rust/plugin-bootstrap/src/init.rs
@@ -8,9 +8,12 @@ use rust_proto::plugin_bootstrap::{
 
 static PLUGIN_BINARY_PATH: &str = "/usr/local/bin/grapl-plugin";
 static CLIENT_CERTIFICATE_PATH: &str = "/etc/ssl/private/plugin-client-cert.pem";
+static PLUGIN_CONFIG_PATH: &str = "/etc/systemd/system/plugin.service.d/override.conf";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (_env, _guard) = grapl_config::init_grapl_env!();
+
     let mut bootstrap_client = PluginBootstrapClient::from_env().await?;
     let GetBootstrapInfoResponse {
         plugin_payload,
@@ -26,7 +29,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     )?;
 
     std::fs::set_permissions(PLUGIN_BINARY_PATH, std::fs::Permissions::from_mode(0o755))?;
-    std::fs::set_permissions(PLUGIN_BINARY_PATH, std::fs::Permissions::from_mode(0o400))?;
+    std::fs::set_permissions(
+        CLIENT_CERTIFICATE_PATH,
+        std::fs::Permissions::from_mode(0o400),
+    )?;
+
+    let config_file = std::fs::File::create(PLUGIN_CONFIG_PATH)?;
+    config_file.set_permissions(std::fs::Permissions::from_mode(0o755))?;
 
     Ok(())
 }
