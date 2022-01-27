fdc6a2c126b1a9393632d03d7b6b414a39ba4b70
Max Wittek <wimax@graplsecurity.com>
2022-01-27 18:36:35 -0500

    Rename test_smoketest; fix the namespace used in test_nomad_client_create_namespace

--- src/rust/plugin-registry/tests/test_nomad_client.rs
+++ src/rust/plugin-registry/tests/test_nomad_client.rs
@@ -6,7 +6,7 @@ use plugin_registry::nomad_client;
 async fn test_nomad_client_create_namespace() -> Result<(), Box<dyn std::error::Error>> {
     let client = nomad_client::NomadClient::from_env();
     client
-        .create_namespace("test_nomad_client_create_namespace")
+        .create_namespace("test-nomad-client-create-namespace")
         .await?;
     Ok(())
 }
