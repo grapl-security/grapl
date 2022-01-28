a42276e0fd03391c25738e8c1357f3263f7f1ae8
Max Wittek <69007229+wimax-grapl@users.noreply.github.com>
2022-01-27 18:17:03 -0800

    Update dump_artifacts to account for multiple Nomad namespaces (plugins) (#1418)
    * Update dump_artifacts to account for multi-namespace
    
    * Rename test_smoketest; fix the namespace used in test_nomad_client_create_namespace

--- src/rust/plugin-registry/tests/test_nomad_client.rs
+++ src/rust/plugin-registry/tests/test_nomad_client.rs
@@ -3,8 +3,10 @@
 use plugin_registry::nomad_client;
 
 #[test_log::test(tokio::test)]
-async fn test_nomad_client() -> Result<(), Box<dyn std::error::Error>> {
+async fn test_nomad_client_create_namespace() -> Result<(), Box<dyn std::error::Error>> {
     let client = nomad_client::NomadClient::from_env();
-    client.create_namespace("hello").await?;
+    client
+        .create_namespace("test-nomad-client-create-namespace")
+        .await?;
     Ok(())
 }
