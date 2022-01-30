2387f08eace13970c5d034458f46edf1c1de43a4
Colin O'Brien <colin@graplsecurity.com>
2022-01-29 17:29:03 -0800

    Remove empty test

--- src/rust/plugin-sdk/generator-sdk/src/lib.rs
+++ src/rust/plugin-sdk/generator-sdk/src/lib.rs
@@ -2,12 +2,3 @@ pub mod server;
 
 #[cfg(feature = "client")]
 pub mod client;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
