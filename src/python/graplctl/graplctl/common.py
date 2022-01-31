7e5ccc0d18950e13c5c1fd8f6054e65394a089ac
Colin O'Brien <colin@graplsecurity.com>
2022-01-31 14:32:26 -0800

    Remove unused import

--- src/python/graplctl/graplctl/common.py
+++ src/python/graplctl/graplctl/common.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import dataclasses
 import time
-from typing import TYPE_CHECKING, Dict, Iterator, List, Tuple
+from typing import TYPE_CHECKING, Iterator, List, Tuple
 
 import click
 from grapl_common.grapl_logger import get_module_grapl_logger
