5984b5e7bb945f00c302986f1a0bd71d2100f699
jgrillo <jgrillo@graplsecurity.com>
2022-02-01 14:38:35 -0500

    remove unnecessary docker volume rm invocations

--- nomad/local/stop_detach.sh
+++ nomad/local/stop_detach.sh
@@ -4,6 +4,3 @@ set -euo pipefail
 
 sudo killall --wait nomad || true
 killall --wait consul || true
-docker volume rm local-grapl-zk-data --force
-docker volume rm local-grapl-zk-txn-logs --force
-docker volume rm local-grapl-kafka-data --force
