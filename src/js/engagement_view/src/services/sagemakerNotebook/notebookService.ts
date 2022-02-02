9d777ececf6c3690b582dc8d0cf065ea00667d0a
Thomas Wunderlich <thomas@graplsecurity.com>
2022-02-02 13:14:29 -0500

    Rip out sagemaker from engagement view
    We're replacing sagemaker and jupyter notebook shortly. As such I'm
    deleting the old code that's to be replaced.
    Note that I've left the front-end design the same for now, I'm not
    messing around with css today, at least not without talking to Andrea
    first. Maybe next week?

--- src/js/engagement_view/src/services/sagemakerNotebook/notebookService.ts
+++
@@ -1,8 +0,0 @@
-import { apiFetchReq } from "../fetch";
-import DEV_API_EDGES from "../constants";
-
-export const getNotebookUrl = async (): Promise<string> => {
-    return apiFetchReq(`${DEV_API_EDGES.auth}/getNotebook`, "post").then(
-        (result) => result.success.notebook_url
-    );
-};
