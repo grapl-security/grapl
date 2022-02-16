87c039e3bcbe34863b8e6d38c73f6c165b79d2dc
jgrillo <jgrillo@graplsecurity.com>
2022-02-16 15:07:39 -0500

    make format

--- src/python/python-proto/python_proto/tests/helpers.py
+++ src/python/python-proto/python_proto/tests/helpers.py
@@ -13,13 +13,20 @@ def check_encode_decode_invariant(
         serde_value: Union[SerDe[P], SerDeWithInner[P, I]]
     ) -> None:
         if isinstance(serde_value, SerDeWithInner):
-            transformed_with_inner = cast(SerDeWithInner[P, I], serde_value.__class__.deserialize(
-                serde_value.serialize(),
-                inner_cls=cast(Type, serde_value.inner_message.__class__),  # lol what
-            ))
+            transformed_with_inner = cast(
+                SerDeWithInner[P, I],
+                serde_value.__class__.deserialize(
+                    serde_value.serialize(),
+                    inner_cls=cast(
+                        Type, serde_value.inner_message.__class__
+                    ),  # lol what
+                ),
+            )
             assert transformed_with_inner == serde_value
         else:
-            transformed = cast(SerDe[P], serde_value.__class__.deserialize(serde_value.serialize()))
+            transformed = cast(
+                SerDe[P], serde_value.__class__.deserialize(serde_value.serialize())
+            )
             assert transformed == serde_value
 
     _check_encode_decode_invariant()
