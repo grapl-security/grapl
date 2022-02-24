59dfc01dc39c940b500f8af3ec849eddd9f4f620
Max Wittek <wimax@graplsecurity.com>
2022-02-24 15:24:25 -0500

    Introduce test_user_creds.py; remove STACK_NAME from python code for provisioner, graphql_endpoint_tests

--- src/python/grapl-tests-common/grapl_tests_common/clients/grapl_web_client.py
+++ src/python/grapl-tests-common/grapl_tests_common/clients/grapl_web_client.py
@@ -1,11 +1,9 @@
-import os
 from http import HTTPStatus
 from typing import Optional
 
-import boto3
 import requests
-from grapl_common.env_helpers import SecretsManagerClientFactory
 from grapl_common.grapl_logger import get_module_grapl_logger
+from grapl_common.test_user_creds import get_test_user_creds
 from grapl_tests_common.clients.common import endpoint_url
 
 _JSON_CONTENT_TYPE_HEADERS = {"Content-type": "application/json"}
@@ -17,15 +15,6 @@ class GraplWebClientException(Exception):
     pass
 
 
-# TODO: Replace with passing in the password ID verbatim
-def _get_test_user_password(deployment_name: str) -> str:
-    secretsmanager = SecretsManagerClientFactory(boto3).from_env()
-    LOGGER.debug(f"retrieving {deployment_name}-TestUserPassword")
-    return secretsmanager.get_secret_value(
-        SecretId=f"{deployment_name}-TestUserPassword"
-    )["SecretString"]
-
-
 class GraplWebClient:
     def __init__(self) -> None:
         self.endpoint = endpoint_url(suffix="")
@@ -33,8 +22,7 @@ class GraplWebClient:
 
     def get_actix_session(self) -> str:
         LOGGER.debug("retrieving actix cookie")
-        username = os.environ["GRAPL_TEST_USER_NAME"]
-        password = _get_test_user_password(os.environ["STACK_NAME"])
+        username, password = get_test_user_creds()
 
         resp = requests.post(
             f"{self.endpoint}/auth/login",
@@ -54,7 +42,7 @@ class GraplWebClient:
         return cookie
 
     def real_user_fake_password(self) -> requests.Response:
-        username = os.environ["GRAPL_TEST_USER_NAME"]
+        username, _ = get_test_user_creds()
         resp = requests.post(
             f"{self.endpoint}/auth/login",
             json={
@@ -88,8 +76,7 @@ class GraplWebClient:
         return resp
 
     def no_content_type(self) -> requests.Response:
-        username = os.environ["GRAPL_TEST_USER_NAME"]
-        password = _get_test_user_password(os.environ["STACK_NAME"])
+        username, password = get_test_user_creds()
 
         resp = requests.post(
             f"{self.endpoint}/auth/login",
