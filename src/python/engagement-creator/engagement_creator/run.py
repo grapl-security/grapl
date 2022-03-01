04c987b74576522fae299c572bce7d1fb290b527
Thomas Wunderlich <thomas@graplsecurity.com>
2022-03-01 11:24:47 -0500

    Manual instrumentation of tracing works
    We're starting off with manual instrumentation for the moment since
    we don't use a framework like Flask or Django. It may be possible to
    autoinstrument our python code :fingers_crossed:

--- src/python/engagement-creator/engagement_creator/run.py
+++ src/python/engagement-creator/engagement_creator/run.py
@@ -5,6 +5,7 @@ import boto3
 from engagement_creator.lib.engagement_creator import lambda_handler
 from grapl_common.env_helpers import SQSClientFactory
 from grapl_common.grapl_logger import get_module_grapl_logger
+from grapl_common.grapl_tracer import get_tracer
 from grapl_common.sqs.event_retriever import EventRetriever
 from grapl_common.sqs.sqs_timeout_manager import (
     SqsTimeoutManager,
@@ -12,6 +13,7 @@ from grapl_common.sqs.sqs_timeout_manager import (
 )
 
 LOGGER = get_module_grapl_logger()
+TRACER = get_tracer(service_name="engagement_creator", module_name=__name__)
 
 
 async def _main() -> None:
@@ -19,27 +21,28 @@ async def _main() -> None:
     Some TODOs to bring this inline with sqs-executor in Rust:
     - add the shortcut-to-DEAD_LETTER_QUEUE_URL case
     """
-    queue_url = os.environ["SOURCE_QUEUE_URL"]
-    retriever = EventRetriever(queue_url=queue_url)
-    sqs_client = SQSClientFactory(boto3).from_env()
+    with TRACER.start_as_current_span("engagement_creator"):
+        queue_url = os.environ["SOURCE_QUEUE_URL"]
+        retriever = EventRetriever(queue_url=queue_url)
+        sqs_client = SQSClientFactory(boto3).from_env()
 
-    for sqs_message in retriever.retrieve():
-        # We'd feed this coroutine into the timeout manager.
-        message_handle_coroutine = lambda_handler(sqs_message.body, {})
-        # While we're waiting for that future to complete, keep telling SQS
-        # "hey, we're working on it" so it doesn't become visible on the
-        # input queue again.
-        timeout_manager = SqsTimeoutManager(
-            sqs_client=sqs_client,
-            queue_url=queue_url,
-            receipt_handle=sqs_message.receipt_handle,
-            message_id=sqs_message.message_id,
-            coroutine=message_handle_coroutine,
-        )
-        try:
-            await timeout_manager.keep_alive()
-        except SqsTimeoutManagerException:
-            LOGGER.error("SQS Timeout Manager exception", exc_info=True)
+        for sqs_message in retriever.retrieve():
+            # We'd feed this coroutine into the timeout manager.
+            message_handle_coroutine = lambda_handler(sqs_message.body, {})
+            # While we're waiting for that future to complete, keep telling SQS
+            # "hey, we're working on it" so it doesn't become visible on the
+            # input queue again.
+            timeout_manager = SqsTimeoutManager(
+                sqs_client=sqs_client,
+                queue_url=queue_url,
+                receipt_handle=sqs_message.receipt_handle,
+                message_id=sqs_message.message_id,
+                coroutine=message_handle_coroutine,
+            )
+            try:
+                await timeout_manager.keep_alive()
+            except SqsTimeoutManagerException:
+                LOGGER.error("SQS Timeout Manager exception", exc_info=True)
 
 
 def run() -> None:
