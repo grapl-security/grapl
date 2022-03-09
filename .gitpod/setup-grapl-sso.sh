fb7a9e123b8f854201ee9744cf0c302a8cbf1174
Max Wittek <wimax@graplsecurity.com>
2022-03-09 12:04:19 -0800

    Initial Gitpod support, mostly taken from https://github.com/grapl-security/pipeline-infrastructure/pull/175/files

---
+++ .gitpod/setup-grapl-sso.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Automatically configure AWS SSO for all the accounts you have 
+# access to.
+#
+# Note that `AWS_DEFAULT_SSO_START_URL` and `AWS_DEFAULT_SSO_REGION`
+# appear to be specific to `aws-sso-util`, not to the AWS CLI itself.
+#
+# These values can be set in your Gitpod Variables: 
+# https://gitpod.io/variables
+
+# The region that we'll be logging into for each account.
+readonly region="us-east-1"
+
+aws-sso-util configure populate \
+	--sso-start-url="${AWS_DEFAULT_SSO_START_URL}" \
+	--sso-region="${AWS_DEFAULT_SSO_REGION}" \
+	--components=account_name \
+    --region="${region}" \
+	--components=account_name \
+	--trim-account-name="'" \
+	--trim-account-name="_"
