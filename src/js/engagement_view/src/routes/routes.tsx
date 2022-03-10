c2c670d7b3d26ae33e59c21135c6765ad5e5cda5
Andrea <andrea@graplsecurity.com>
2022-03-10 14:21:51 -0700

    update react router to use routes instead of switch

--- src/js/engagement_view/src/routes/routes.tsx
+++ src/js/engagement_view/src/routes/routes.tsx
@@ -4,19 +4,19 @@ import { LogIn } from "../components/login/Login";
 import { EngagementUx } from "../components/engagementView/EngagementView";
 import Dashboard from "../components/dashboard/Dashboard";
 import UploadPlugin from "../components/uploadPlugin/UploadPluginView";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-export default function Routes() {
+export default function GraplRoutes() {
     console.log("Grapl loaded");
 
     return (
         <HashRouter>
-            <Switch>
-                <Route exact path="/login" component={LogIn} />
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/plugins" component={UploadPlugin} />
-                <Route exact path="/engagements" component={EngagementUx} />
-            </Switch>
+            <Routes>
+                <Route path="/login" element={LogIn} />
+                <Route path="/" element={Dashboard} />
+                <Route path="/plugins" element={UploadPlugin} />
+                <Route path="/engagements" element={EngagementUx} />
+            </Routes>
         </HashRouter>
     );
 }
