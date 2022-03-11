fb527c90128a46e296943392fc3a157d4118ec2f
Andrea <andrea@graplsecurity.com>
2022-03-11 15:18:12 -0700

    wip - consolidating routes into one file

--- src/js/engagement_view/src/index.tsx
+++ src/js/engagement_view/src/index.tsx
@@ -1,29 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { HashRouter } from "react-router-dom";
 
-const darkTheme = createMuiTheme({
-    palette: {
-        type: "dark",
-        primary: {
-            main: "#373740",
-        },
-    },
-});
+import "./components/login/LogIn.css";
+import { LogIn } from "./components/login/Login";
+import { EngagementUx } from "./components/engagementView/EngagementView";
+import Dashboard from "./components/dashboard/Dashboard";
+import UploadPlugin from "./components/uploadPlugin/UploadPluginView";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
     <React.StrictMode>
-        <HashRouter>
-            <ThemeProvider theme={darkTheme}>
-                <App />,
-            </ThemeProvider>
-        </HashRouter>
+            <HashRouter>
+                <Routes>
+                    <Route path="/login" element={LogIn} />
+                    <Route path="/" element={Dashboard} />
+                    <Route path="/plugins" element={UploadPlugin} />
+                    <Route path="/engagements" element={EngagementUx} />
+                </Routes>
+            </HashRouter>
     </React.StrictMode>,
     rootElement
 );
