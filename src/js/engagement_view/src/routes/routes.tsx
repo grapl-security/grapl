4047a01de985f29831f705947fc429d80fc5dce6
Andrea <andrea@graplsecurity.com>
2022-03-11 17:30:50 -0700

    make format

--- src/js/engagement_view/src/routes/routes.tsx
+++ src/js/engagement_view/src/routes/routes.tsx
@@ -4,7 +4,7 @@ import { LogIn } from "../components/login/Login";
 import { EngagementUx } from "../components/engagementView/EngagementView";
 import Dashboard from "../components/dashboard/Dashboard";
 import UploadPlugin from "../components/uploadPlugin/UploadPluginView";
-import {  Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 
 export default function GraplRoutes() {
