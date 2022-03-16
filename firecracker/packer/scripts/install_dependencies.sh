2ba8b7c344f23f2de1c576c031479817aea07951
Max Wittek <wimax@graplsecurity.com>
2022-03-16 12:38:03 -0400

    Remove apt cache madison

--- firecracker/packer/scripts/install_dependencies.sh
+++ firecracker/packer/scripts/install_dependencies.sh
@@ -7,5 +7,4 @@ set -o xtrace
 # Install dependencies (currently, just debootstrap)
 ########################################
 sudo apt-get update
-sudo apt-cache madison debootstrap
 sudo apt-get install debootstrap=1.0.118ubuntu1
