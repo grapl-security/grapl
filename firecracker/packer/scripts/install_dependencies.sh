80d2da1446e944b3791d81e4d0c7465fd85e240f
Max Wittek <wimax@graplsecurity.com>
2022-03-17 12:31:23 -0400

    why does apt cache madison have no output wtf

--- firecracker/packer/scripts/install_dependencies.sh
+++ firecracker/packer/scripts/install_dependencies.sh
@@ -10,5 +10,6 @@ sudo apt-get update
 # Print available versions for debugging purposes.
 # (You might say, "madison? what's madison?" Just a badly named apt command.)
 sudo apt-cache madison debootstrap
+sleep 5
 sudo apt-get install --yes --no-install-recommends \
-    debootstrap=1.0.118ubuntu1
+    debootstrap=1.0.118ubuntu1.6
