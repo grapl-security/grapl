9ffd28212743685cdc6a2324e0acbd03a47ad042
Max Wittek <wimax@graplsecurity.com>
2022-03-17 14:18:11 -0400

    Revert "Go back to apt-get but with an option for dpkg lock"
    This reverts commit 9123a0c16031d64ae090bf40ac748e9db7bc87bd.

--- firecracker/packer/scripts/install_dependencies.sh
+++ firecracker/packer/scripts/install_dependencies.sh
@@ -6,12 +6,11 @@ set -o xtrace
 ########################################
 # Install dependencies (currently, just debootstrap)
 ########################################
-readonly APT_GET_WITH_LOCK=(apt-get -o dpkg::lock::timeout=10)
-sudo "${APT_GET_WITH_LOCK[@]}" update
+sudo apt update
 
 # Print available versions for debugging purposes.
 # (You might say, "madison? what's madison?" Just a badly named apt command.)
 sudo apt-cache madison debootstrap
 
-sudo "${APT_GET_WITH_LOCK[@]}" install --yes --no-install-recommends \
+sudo apt install --yes --no-install-recommends \
     debootstrap=1.0.118ubuntu1.6
