c94aa84e72778c9d3e1af606b643a7adcaf386a6
Max Wittek <wimax@graplsecurity.com>
2022-04-04 16:40:07 -0400

    Add a comment, remove an &&

--- firecracker/kernel/build.sh
+++ firecracker/kernel/build.sh
@@ -34,13 +34,15 @@ readonly BUILD_DIR
 ########################################
 # Copy kernel into dist.
 ########################################
+# this name/dir are determined by Firecracker's devtool script, and are
+# not something under our control
 readonly KERNEL_BIN_DIR="${BUILD_DIR}/firecracker/build/kernel/linux-${KERNEL_VERSION}/"
 readonly KERNEL_BIN_FILE="${KERNEL_BIN_DIR}/vmlinux-${KERNEL_VERSION}-x86_64.bin"
 
 if [[ ! -f "${KERNEL_BIN_FILE}" ]]; then
     # https://github.com/firecracker-microvm/firecracker/issues/2912
-    echo "Couldn't find ${KERNEL_BIN_FILE}; the kernel build likely failed" &&
-        exit 42
+    echo "Couldn't find ${KERNEL_BIN_FILE}; the kernel build likely failed"
+    exit 42
 fi
 
 # Move kernel to a stable name, so that Nomad knows which file in the
