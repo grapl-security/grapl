238512d012b059173a2cd031d72a5b5e5c93ae9a
Andrea <andrea@graplsecurity.com>
2022-04-05 13:09:03 -0600

    remove commented out code

--- src/rust/grapl-web-ui/src/services/plugin_registry.rs
+++ src/rust/grapl-web-ui/src/services/plugin_registry.rs
@@ -1,4 +1,5 @@
 use std::ops::Deref;
+
 use actix_web::{
     post,
     web,
@@ -6,6 +7,7 @@ use actix_web::{
     HttpResponse,
     Result,
 };
+
 use crate::authn::AuthenticatedUser;
 
 // We have a new type for this to differentiate between the URL for this backend service and that
@@ -44,54 +46,3 @@ pub(crate) async fn handler(
 
     super::fwd_request_to_backend_service(req, payload, url, client.get_ref().clone()).await
 }
-
-
-// pub(crate) fn config(cfg: &mut web::ServiceConfig) {
-//     cfg.service(
-//         web::resource("/plugin_registry")
-//             .route(web::post().to(create_plugin_post))
-//             .guard(guard::Post())
-//             .guard(guard::Header("content-type", "application/json")), // .guard(guard::Header("X-Requested-With", "XMLHttpRequest")),
-//     );
-// }
-
-// async fn create_plugin_post<T>(
-//     req: HttpRequest,
-//     payload: web::Payload,
-//     // backend_url: web::Data<ModelPluginDeployerEndpoint>,
-//     client: web::Data<PluginRegistryServiceClient<T>>,
-//     _user: AuthenticatedUser,
-// ) -> Result<HttpResponse>
-//     // T:
-// {
-//     tracing::debug!(
-//         env=?std::env::args(),
-//     );
-
-//     let mut client = PluginRegistryServiceClient::from_env().await?;
-//     let tenant_id = uuid::Uuid::new_v4();
-
-//     let request = CreatePluginRequest {
-//         plugin_artifact: b"???????".to_vec(),
-//         tenant_id, // todo(AP - Add Tenant ID)
-//         display_name: "test_for_now".to_owned(),
-//         plugin_type: PluginType::Generator,
-//     };
-
-//     let response = client
-//         .create_plugin(request)
-//         .timeout(std::time::Duration::from_secs(5))
-//         .await??;
-
-//     let plugin_id = response.plugin_id;
-
-//     let get_response: GetPluginResponse = client
-//         .get_plugin(GetPluginRequest {
-//             plugin_id,
-//             tenant_id,
-//         })
-//         .timeout(std::time::Duration::from_secs(5))
-//         .await??;
-
-//     todo!()
-// }
