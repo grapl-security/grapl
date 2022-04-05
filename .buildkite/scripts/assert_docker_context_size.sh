a2bfc1e691c97aa88210669a48d5c89647c5fb8d
Max Wittek <wimax@graplsecurity.com>
2022-04-05 13:15:45 -0400

    Describe size limit in MB. Automate get-all-docker-contexts.

--- .buildkite/scripts/assert_docker_context_size.sh
+++ .buildkite/scripts/assert_docker_context_size.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 # Ensure the Docker context remains a manageable, reasonable size.
 # (This helps catch missed renames that should be caught in the .dockerignore.)
 ################################################################################
-readonly GRAPL_ROOT="${PWD}"
+# This can change in the future, but this seems like a reasonable starter limit
+readonly ARBITRARY_SIZE_LIMIT_MB=$((300))
 
 get_size_of_context_kb() {
     # Based on https://stackoverflow.com/a/61821807
@@ -17,29 +18,26 @@ get_size_of_context_kb() {
             --file - \
             --output "${temp_dir}" \
             .
-        echo >&2 "Dumped ${context_dir} context to ${temp_dir}"
+        echo "Dumped ${context_dir} context to ${temp_dir}" >&2
         cd "${temp_dir}"
         du --max-depth=0 | awk '{print $1;}'
     )
 }
 
-# This can change in the future, but this seems like a reasonable starter limit
-readonly ARBITRARY_SIZE_LIMIT_KB=$((300 * 1024))
-
 # anything that'll be declared as a context in docker-bake.hcl
-readonly DIRS_TO_CHECK=(
-    "${GRAPL_ROOT}"
-    "${GRAPL_ROOT}/src"
-    "${GRAPL_ROOT}/src/js/graphql_endpoint"
-    "${GRAPL_ROOT}/localstack"
-    "${GRAPL_ROOT}/postgres"
-)
+all_docker_contexts() {
+    docker buildx bake all --print |
+        jq --raw-output '.target[].context' |
+        sort --unique
+}
+
+mapfile -t DIRS_TO_CHECK < <(all_docker_contexts)
 
 for dir in "${DIRS_TO_CHECK[@]}"; do
     echo "--- Checking Docker context size of ${dir}"
-    size="$(get_size_of_context_kb "${dir}")"
-    echo "Docker context size of ${dir} is ${size}kb"
-    if [[ "${size}" -gt "${ARBITRARY_SIZE_LIMIT_KB}" ]]; then
+    size_kb="$(get_size_of_context_kb "${dir}")"
+    echo "Docker context size of ${dir} is ${size_kb}kb"
+    if [[ "${size_kb}" -gt "$((ARBITRARY_SIZE_LIMIT_MB * 1024))" ]]; then
         echo "That's too big! Maybe you need to modify the .dockerignore?"
         exit 42
     fi
