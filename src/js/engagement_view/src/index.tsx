e544d5ff7a08d269414dd45bd794524f1255cd1b
Max Wittek <wimax@graplsecurity.com>
2022-04-06 16:13:43 -0400

    Remove an unused import in index.tsx

--- src/js/engagement_view/src/index.tsx
+++ src/js/engagement_view/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 
 import App from "./App";
 import theme from "./theme";
