d891ccdde99f33372c0d3e35d80a83d2c8356ecd
Max Wittek <69007229+wimax-grapl@users.noreply.github.com>
2022-04-12 18:55:46 -0400

    Provision a dev box on EC2 (#1613)
    * Initial stab at devbox provisioner
    
    * fuck me, it works
    
    * Add the SSH policy. Add the SSH config change to provision script.
    
    * Write a config to ~/.grapl_devbox. Read that config from devbox/ssh.sh
    
    * format
    
    * Oh allow egress to every port
    
    * Remove some comments
    
    * add CODEOWNERS
    
    * POTENTIALLY REVERT - ugh i hate this shit
    
    * Revert "POTENTIALLY REVERT - ugh i hate this shit"
    
    This reverts commit efb589a4cbc97f78c036ea68fb072a72e99009a5.
    
    * Double module named __main__
    
    * Get imports and typecheck working
    
    * Fix pants tailor check
    
    * Move devbox provision pulumi gitignore to toplevel gitignore
    
    * Add devbox to .dockerignore
    
    * Use a better name for ssh key
    
    * Echo the stack name when it already exists
    
    * Add note about updating the ubuntu here and there
    
    * Pass tags everywhere
    
    * Toss the json file, use a .env file
    
    * Pulumi-export the username for the devbox (just hardcoded to ubuntu for now)
    
    * pls work

---
+++ devbox/provision/infra/policies.py
@@ -0,0 +1,104 @@
+import json
+
+import pulumi_aws as aws
+
+import pulumi
+
+
+def build_ssm_policy(opts: pulumi.ResourceOptions = None) -> aws.iam.Policy:
+    return aws.iam.Policy(
+        "demanaged-AmazonSSMManagedInstanceCore",
+        opts=opts,
+        policy=json.dumps(
+            {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Action": [
+                            "ssm:DescribeAssociation",
+                            "ssm:GetDeployablePatchSnapshotForInstance",
+                            "ssm:GetDocument",
+                            "ssm:DescribeDocument",
+                            "ssm:GetManifest",
+                            "ssm:GetParameter",
+                            "ssm:GetParameters",
+                            "ssm:ListAssociations",
+                            "ssm:ListInstanceAssociations",
+                            "ssm:PutInventory",
+                            "ssm:PutComplianceItems",
+                            "ssm:PutConfigurePackageResult",
+                            "ssm:UpdateAssociationStatus",
+                            "ssm:UpdateInstanceAssociationStatus",
+                            "ssm:UpdateInstanceInformation",
+                        ],
+                        "Resource": "*",
+                    },
+                    {
+                        "Effect": "Allow",
+                        "Action": [
+                            "ssmmessages:CreateControlChannel",
+                            "ssmmessages:CreateDataChannel",
+                            "ssmmessages:OpenControlChannel",
+                            "ssmmessages:OpenDataChannel",
+                        ],
+                        "Resource": "*",
+                    },
+                    {
+                        "Effect": "Allow",
+                        "Action": [
+                            "ec2messages:AcknowledgeMessage",
+                            "ec2messages:DeleteMessage",
+                            "ec2messages:FailMessage",
+                            "ec2messages:GetEndpoint",
+                            "ec2messages:GetMessages",
+                            "ec2messages:SendReply",
+                        ],
+                        "Resource": "*",
+                    },
+                ],
+            }
+        ),
+    )
+
+
+def build_ssm_ssh_policy(opts: pulumi.ResourceOptions = None) -> aws.iam.Policy:
+    return aws.iam.Policy(
+        "allow-ssh-over-ssm",
+        opts=opts,
+        policy=json.dumps(
+            {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Action": [
+                            "ssm:StartSession",
+                        ],
+                        "Resource": "arn:aws:ssm:*:*:document/AWS-StartSSHSession",
+                    },
+                ],
+            }
+        ),
+    )
+
+
+def attach_policy(
+    policy: aws.iam.Policy, role: aws.iam.Role
+) -> aws.iam.RolePolicyAttachment:
+    """Attaches the `policy` to the `role`.
+
+    The resulting `RolePolicyAttachment` is created as a child
+    resource of the policy. Naming of the resource is also handled.
+
+    Prefer this over the direct creation of a `RolePolicyAttachment`
+    whenever possible to promote consistency across our
+    infrastructure.
+
+    """
+    return aws.iam.RolePolicyAttachment(
+        f"attach-{policy._name}-to-{role._name}",
+        role=role.name,
+        policy_arn=policy.arn,
+        opts=pulumi.ResourceOptions(parent=policy),
+    )
---
+++ devbox/provision/provision.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly GRAPL_ROOT="${PWD}"
+readonly GRAPL_DEVBOX_DIR="${HOME}/.grapl_devbox"
+readonly GRAPL_DEVBOX_CONFIG="${GRAPL_DEVBOX_DIR}/config.env"
+
+mkdir -p "${GRAPL_DEVBOX_DIR}"
+
+########################################
+# Helper functions
+########################################
+has_key() {
+    local -r input_json="${1}"
+    local -r key="${2}"
+    jq --exit-status ".[\"${key}\"]" <<< "${input_json}" > /dev/null
+}
+
+########################################
+# Ensure we're in the Pulumi-enabled venv
+########################################
+if [ ! -f build-support/venv/bin/activate ]; then
+    echo "Set up your virtualenv with 'build-support/manage_virtualenv.sh'"
+    exit 42
+fi
+# shellcheck disable=SC1091
+source build-support/venv/bin/activate
+
+########################################
+# Set up an SSH key specifically for the Devbox
+########################################
+
+readonly SSH_PRIVATE_KEY_FILE="${GRAPL_DEVBOX_DIR}/devbox_ed25519_ssh"
+readonly SSH_PUBLIC_KEY_FILE="${SSH_PRIVATE_KEY_FILE}.pub"
+
+if [ ! -f "${SSH_PRIVATE_KEY_FILE}" ]; then
+    ssh-keygen -t ed25519 -a 100 -C "${USER}@graplsecurity.com" -f "${SSH_PRIVATE_KEY_FILE}"
+else
+    echo "SSH Key already exists @ ${SSH_PRIVATE_KEY_FILE}"
+fi
+
+########################################
+# Set up Pulumi stack
+########################################
+
+(
+    cd "${GRAPL_ROOT}/devbox/provision"
+    STACK_NAME="grapl/${USER}-devbox"
+
+    if ! pulumi stack --show-name --non-interactive; then
+        pulumi stack init "${STACK_NAME}"
+    else
+        echo "Stack ${STACK_NAME} already exists"
+    fi
+
+    config=$(pulumi config --json)
+    if ! has_key "${config}" "devbox:public-key}"; then
+        pulumi config set devbox:public-key -- < "${SSH_PUBLIC_KEY_FILE}"
+    fi
+    if ! has_key "${config}" "devbox:instance-type}"; then
+        # 32GB RAM
+        # $5.80 daily reserved cost
+        pulumi config set devbox:instance-type "m5.2xlarge"
+    fi
+    if ! has_key "${config}" "aws:region"; then
+        echo "Hey there! You should go to '$(pwd)' and run "
+        echo "'pulumi config set aws:region <value>'"
+        echo "Choose well - responsiveness is a genuine concern here!"
+        echo "  ex: us-east-2, us-west-2, ap-east-1"
+        exit 42
+    fi
+)
+
+########################################
+# Provision an EC2 instance with Pulumi
+########################################
+pulumi update --yes --cwd="${GRAPL_ROOT}/devbox/provision"
+
+########################################
+# Copy some config stuff to a .env file consumed by ssh.sh
+########################################
+(
+    cd "${GRAPL_ROOT}/devbox/provision"
+
+    CONTENTS="$(
+        cat << EOF
+GRAPL_DEVBOX_REGION="$(pulumi config get aws:region)"
+GRAPL_DEVBOX_INSTANCE_ID="$(pulumi stack output devbox-instance-id)"
+GRAPL_DEVBOX_USER="$(pulumi stack output devbox-user)"
+GRAPL_DEVBOX_PRIVATE_KEY_FILE="${SSH_PRIVATE_KEY_FILE}"
+EOF
+    )"
+    echo "${CONTENTS}" > "${GRAPL_DEVBOX_CONFIG}"
+)
+
+########################################
+# Tell SSH to use SSM trickery on hosts starting with `i-`
+########################################
+
+(
+    # Taken from https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-getting-started-enable-ssh-connections.html
+    SSH_CONFIG_APPEND="$(
+        cat << 'EOF'
+host i-* mi-*
+    ProxyCommand sh -c "aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p'"
+EOF
+    )"
+    touch ~/.ssh/config
+    if ! grep --quiet "${SSH_CONFIG_APPEND}" ~/.ssh/config; then
+        echo "${SSH_CONFIG_APPEND}" >> ~/.ssh/config
+    fi
+)
