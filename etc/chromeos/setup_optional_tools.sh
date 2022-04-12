c20b6b3f656f2bee4b2525c0632b3fe6c9d1629c
Thomas Wunderlich <thomas@graplsecurity.com>
2022-04-12 16:43:40 -0400

    Download the tc-redirect-tap plugin from cloudsmith
    Instead of compiling this from source which requires golang installed
    and set up properly, we're just throwing this into cloudsmith for the
    moment and downloading it directly from there. Long-term, either
    the project will create github releases or we can fork and create the
    releases ourselves. However, this changes rarely (4 merged PRs in all
    of 2021), so we can update this manually ourselves if necessary

--- etc/chromeos/setup_optional_tools.sh
+++ etc/chromeos/setup_optional_tools.sh
@@ -7,5 +7,3 @@ source etc/chromeos/lib/installs.sh
 
 echo "Starting Grapl OPTIONAL tooling installation"
 install_git_hooks
-# TODO once golang is autoinstalled move this to the standard install
-install_tc_redirect_tap
