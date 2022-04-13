25def46b3bf30b4e2e731d5d2ed637985aca243d
Andrea <andrea@graplsecurity.com>
2022-04-13 12:16:55 -0600

    make format, remove console.logs, remove broken test

--- src/js/engagement_view/src/tests/labels.test.tsx
+++ src/js/engagement_view/src/tests/labels.test.tsx
@@ -1,15 +1,9 @@
-import {
-    getLinkLabel,
-    getNodeLabel,
-} from "components/graphDisplay/graphLayout/labels";
+import { getNodeLabel } from "components/graphDisplay/graphLayout/labels";
 
 import { processNode } from "./engagementView/testData/baseData";
 
 // Test graph styling for high risk process node
 // Assuming if process works, formatting for other node types also works
-test("link label for processes", () => {
-    expect(getLinkLabel("asset_processes")).toBe("asset_processes");
-});
 
 test("get node label", () => {
     expect(getNodeLabel("Process", processNode as any)).toBe("Process");
