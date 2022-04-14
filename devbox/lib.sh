45fb4f190bd40537fc184b9131874c3abdff22f1
Max Wittek <wimax@graplsecurity.com>
2022-04-14 12:11:21 -0400

    make lint happy

--- devbox/lib.sh
+++ devbox/lib.sh
@@ -14,6 +14,6 @@ source "$(dirname "${BASH_SOURCE[0]}")"/../src/sh/shell_color.sh
 # I'm using HTML <h1>, <h2> terminology for "just how big is this?"
 echo_h1() {
     echo -e "\n========================================"
-    echo -e "==> ${@}"
+    echo -e "==>" "${@}"
     echo -e "========================================"
 }
