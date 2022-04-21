1fedfa5c095dd9ab16cda839820b16bbb7d45d90
Max Wittek <69007229+wimax-grapl@users.noreply.github.com>
2022-04-21 15:03:51 -0400

    forward_common_ports won't open an interactive bash session (#1640)
    * Forward common ports won't open an ssh session
    
    * formatted

--- devbox/forward_common_ports.sh
+++ devbox/forward_common_ports.sh
@@ -3,11 +3,20 @@ set -euo pipefail
 
 THIS_DIR=$(dirname "${BASH_SOURCE[0]}")
 readonly THIS_DIR
+# shellcheck source-path=SCRIPTDIR
+source "${THIS_DIR}/lib.sh"
 
 # 4646 = Nomad
 # 8500 = Consul
 # 1234 = grapl-web-ui
 # 16686 = Jaeger
 
-FORWARD_PORTS="4646,8500,1234,16686" \
-    "${THIS_DIR}"/ssh.sh
+export FORWARD_PORTS="4646,8500,1234,16686"
+
+"${THIS_DIR}"/ssh.sh -t "$(
+    cat << EOF
+    echo -e "Forwarding ports from devbox: $(bright_green "${FORWARD_PORTS}")."
+    echo "Hit <ENTER> to end forwarding.";
+    read
+EOF
+)"
