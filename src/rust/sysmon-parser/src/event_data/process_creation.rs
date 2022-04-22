e050adbcb8a1e9975f8f3aefd85f6b625a9fa893
Ian Nickles <inickles@graplsecurity.com>
2022-04-22 15:14:07 -0700

    Now that Rust 1.60 brings new syntax to Cargo features, undo the serde crate name hack.

--- src/rust/sysmon-parser/src/event_data/process_creation.rs
+++ src/rust/sysmon-parser/src/event_data/process_creation.rs
@@ -29,11 +29,7 @@ use crate::{
 /// <https://docs.microsoft.com/en-us/sysinternals/downloads/sysmon#event-id-1-process-creation>
 #[non_exhaustive]
 #[derive(Debug, Clone, PartialEq, Hash, IntoOwned)]
-#[cfg_attr(
-    feature = "serde",
-    derive(serde_crate::Serialize, serde_crate::Deserialize),
-    serde(crate = "serde_crate")
-)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct ProcessCreateEventData<'a> {
     /// <data name="RuleName" inType="win:UnicodeString" outType="xs:string" />
     pub rule_name: Option<Cow<'a, str>>,
