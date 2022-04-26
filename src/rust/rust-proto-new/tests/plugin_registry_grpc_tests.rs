f63222f5c74fb4955a71261a89ac7879adbcd5c3
Max Wittek <wimax@graplsecurity.com>
2022-04-26 19:48:21 -0400

    Add some TODOs

---
+++ src/rust/rust-proto-new/tests/plugin_registry_grpc_tests.rs
@@ -0,0 +1,5 @@
+/// TODO
+/// We already have integration tests, but would be desirable to test that
+/// gRPC API contracts are upheld wrt errors, etc, given changes to the 
+/// underlying gRPC server code
+
