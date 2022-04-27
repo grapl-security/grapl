fb5e95d7be493a983af4d342fb6813bcbf2ef398
Colin O'Brien <colin@graplsecurity.com>
2022-04-26 21:00:50 -0700

    Remove unused status codes

--- src/rust/rust-proto-new/src/protocol/status.rs
+++ src/rust/rust-proto-new/src/protocol/status.rs
@@ -1,5 +1,7 @@
-use tonic::{Code as GrpcCode, Status as GrpcStatus};
-
+use tonic::{
+    Code as GrpcCode,
+    Status as GrpcStatus,
+};
 
 /// Status codes used by [`Status`].
 ///
@@ -11,53 +13,35 @@ pub enum Code {
     /// The operation completed successfully.
     Ok = 0,
 
-    /// The operation was cancelled.
-    Cancelled = 1,
-
     /// Unknown error.
-    Unknown = 2,
+    Unknown = 1,
 
     /// Client specified an invalid argument.
-    InvalidArgument = 3,
-
-    /// Deadline expired before operation could complete.
-    DeadlineExceeded = 4,
+    InvalidArgument = 2,
 
     /// Some requested entity was not found.
-    NotFound = 5,
+    NotFound = 3,
 
     /// Some entity that we attempted to create already exists.
-    AlreadyExists = 6,
+    AlreadyExists = 4,
 
     /// The caller does not have permission to execute the specified operation.
-    PermissionDenied = 7,
-
-    /// Some resource has been exhausted.
-    ResourceExhausted = 8,
+    PermissionDenied = 5,
 
     /// The system is not in a state required for the operation's execution.
-    FailedPrecondition = 9,
-
-    /// The operation was aborted.
-    Aborted = 10,
-
-    /// Operation was attempted past the valid range.
-    OutOfRange = 11,
+    FailedPrecondition = 6,
 
     /// Operation is not implemented or not supported.
-    Unimplemented = 12,
+    Unimplemented = 7,
 
     /// Internal error.
-    Internal = 13,
+    Internal = 8,
 
     /// The service is currently unavailable.
-    Unavailable = 14,
-
-    /// Unrecoverable data loss or corruption.
-    DataLoss = 15,
+    Unavailable = 9,
 
     /// The request does not have valid authentication credentials
-    Unauthenticated = 16,
+    Unauthenticated = 10,
 }
 
 impl Code {
@@ -76,25 +60,19 @@ impl Code {
     pub fn description(&self) -> &'static str {
         match self {
             Code::Ok => "The operation completed successfully",
-            Code::Cancelled => "The operation was cancelled",
             Code::Unknown => "Unknown error",
             Code::InvalidArgument => "Client specified an invalid argument",
-            Code::DeadlineExceeded => "Deadline expired before operation could complete",
             Code::NotFound => "Some requested entity was not found",
             Code::AlreadyExists => "Some entity that we attempted to create already exists",
             Code::PermissionDenied => {
                 "The caller does not have permission to execute the specified operation"
             }
-            Code::ResourceExhausted => "Some resource has been exhausted",
             Code::FailedPrecondition => {
                 "The system is not in a state required for the operation's execution"
             }
-            Code::Aborted => "The operation was aborted",
-            Code::OutOfRange => "Operation was attempted past the valid range",
             Code::Unimplemented => "Operation is not implemented or not supported",
             Code::Internal => "Internal error",
             Code::Unavailable => "The service is currently unavailable",
-            Code::DataLoss => "Unrecoverable data loss or corruption",
             Code::Unauthenticated => "The request does not have valid authentication credentials",
         }
     }
@@ -110,21 +88,15 @@ impl From<Code> for GrpcCode {
     fn from(code: Code) -> Self {
         match code {
             Code::Ok => GrpcCode::Ok,
-            Code::Cancelled => GrpcCode::Cancelled,
             Code::Unknown => GrpcCode::Unknown,
             Code::InvalidArgument => GrpcCode::InvalidArgument,
-            Code::DeadlineExceeded => GrpcCode::DeadlineExceeded,
             Code::NotFound => GrpcCode::NotFound,
             Code::AlreadyExists => GrpcCode::AlreadyExists,
             Code::PermissionDenied => GrpcCode::PermissionDenied,
-            Code::ResourceExhausted => GrpcCode::ResourceExhausted,
             Code::FailedPrecondition => GrpcCode::FailedPrecondition,
-            Code::Aborted => GrpcCode::Aborted,
-            Code::OutOfRange => GrpcCode::OutOfRange,
             Code::Unimplemented => GrpcCode::Unimplemented,
             Code::Internal => GrpcCode::Internal,
             Code::Unavailable => GrpcCode::Unavailable,
-            Code::DataLoss => GrpcCode::DataLoss,
             Code::Unauthenticated => GrpcCode::Unauthenticated,
         }
     }
@@ -134,21 +106,21 @@ impl From<GrpcCode> for Code {
     fn from(code: GrpcCode) -> Self {
         match code {
             GrpcCode::Ok => Code::Ok,
-            GrpcCode::Cancelled => Code::Cancelled,
+            GrpcCode::Cancelled => Code::Internal,
             GrpcCode::Unknown => Code::Unknown,
             GrpcCode::InvalidArgument => Code::InvalidArgument,
-            GrpcCode::DeadlineExceeded => Code::DeadlineExceeded,
+            GrpcCode::DeadlineExceeded => Code::Internal,
             GrpcCode::NotFound => Code::NotFound,
             GrpcCode::AlreadyExists => Code::AlreadyExists,
             GrpcCode::PermissionDenied => Code::PermissionDenied,
-            GrpcCode::ResourceExhausted => Code::ResourceExhausted,
+            GrpcCode::ResourceExhausted => Code::Internal,
             GrpcCode::FailedPrecondition => Code::FailedPrecondition,
-            GrpcCode::Aborted => Code::Aborted,
-            GrpcCode::OutOfRange => Code::OutOfRange,
+            GrpcCode::Aborted => Code::Internal,
+            GrpcCode::OutOfRange => Code::Internal,
             GrpcCode::Unimplemented => Code::Unimplemented,
             GrpcCode::Internal => Code::Internal,
             GrpcCode::Unavailable => Code::Unavailable,
-            GrpcCode::DataLoss => Code::DataLoss,
+            GrpcCode::DataLoss => Code::Internal,
             GrpcCode::Unauthenticated => Code::Unauthenticated,
         }
     }
@@ -175,11 +147,6 @@ impl Status {
         Status::new(Code::Ok, message)
     }
 
-    /// The operation was cancelled (typically by the caller).
-    pub fn cancelled(message: impl Into<String>) -> Status {
-        Status::new(Code::Cancelled, message)
-    }
-
     /// Unknown error. An example of where this error may be returned is if a
     /// `Status` value received from another address space belongs to an error-space
     /// that is not known in this address space. Also errors raised by APIs that
@@ -194,15 +161,6 @@ impl Status {
     /// name).
     pub fn invalid_argument(message: impl Into<String>) -> Status {
         Status::new(Code::InvalidArgument, message)
-    }
-
-    /// Deadline expired before operation could complete. For operations that
-    /// change the state of the system, this error may be returned even if the
-    /// operation has completed successfully. For example, a successful response
-    /// from a server could have been delayed long enough for the deadline to
-    /// expire.
-    pub fn deadline_exceeded(message: impl Into<String>) -> Status {
-        Status::new(Code::DeadlineExceeded, message)
     }
 
     /// Some requested entity (e.g., file or directory) was not found.
@@ -225,12 +183,6 @@ impl Status {
         Status::new(Code::PermissionDenied, message)
     }
 
-    /// Some resource has been exhausted, perhaps a per-user quota, or perhaps
-    /// the entire file system is out of space.
-    pub fn resource_exhausted(message: impl Into<String>) -> Status {
-        Status::new(Code::ResourceExhausted, message)
-    }
-
     /// Operation was rejected because the system is not in a state required for
     /// the operation's execution. For example, directory to be deleted may be
     /// non-empty, an rmdir operation is applied to a non-directory, etc.
@@ -247,32 +199,6 @@ impl Status {
     ///     fixed up the directory by deleting files from it.
     pub fn failed_precondition(message: impl Into<String>) -> Status {
         Status::new(Code::FailedPrecondition, message)
-    }
-
-    /// The operation was aborted, typically due to a concurrency issue like
-    /// sequencer check failures, transaction aborts, etc.
-    ///
-    /// See litmus test above for deciding between `FailedPrecondition`,
-    /// `Aborted`, and `Unavailable`.
-    pub fn aborted(message: impl Into<String>) -> Status {
-        Status::new(Code::Aborted, message)
-    }
-
-    /// Operation was attempted past the valid range. E.g., seeking or reading
-    /// past end of file.
-    ///
-    /// Unlike `InvalidArgument`, this error indicates a problem that may be
-    /// fixed if the system state changes. For example, a 32-bit file system will
-    /// generate `InvalidArgument if asked to read at an offset that is not in the
-    /// range [0,2^32-1], but it will generate `OutOfRange` if asked to read from
-    /// an offset past the current file size.
-    ///
-    /// There is a fair bit of overlap between `FailedPrecondition` and
-    /// `OutOfRange`. We recommend using `OutOfRange` (the more specific error)
-    /// when it applies so that callers who are iterating through a space can
-    /// easily look for an `OutOfRange` error to detect when they are done.
-    pub fn out_of_range(message: impl Into<String>) -> Status {
-        Status::new(Code::OutOfRange, message)
     }
 
     /// Operation is not implemented or not supported/enabled in this service.
@@ -295,11 +221,6 @@ impl Status {
         Status::new(Code::Unavailable, message)
     }
 
-    /// Unrecoverable data loss or corruption.
-    pub fn data_loss(message: impl Into<String>) -> Status {
-        Status::new(Code::DataLoss, message)
-    }
-
     /// The request does not have valid authentication credentials for the
     /// operation.
     pub fn unauthenticated(message: impl Into<String>) -> Status {
@@ -309,12 +230,7 @@ impl Status {
 
 impl std::fmt::Display for Status {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "status: {:?}, message: {:?}",
-            self.code,
-            self.message,
-        )
+        write!(f, "status: {:?}, message: {:?}", self.code, self.message,)
     }
 }
 
@@ -344,20 +260,3 @@ impl From<GrpcStatus> for Status {
         Status::new(status.code().into(), status.message())
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
