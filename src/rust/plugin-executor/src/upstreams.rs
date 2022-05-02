fab346423aaa0c05cc05c6d0a09c83a99eb440ac
Max Wittek <wimax@graplsecurity.com>
2022-05-02 17:56:16 -0400

    Add the Docker iamge - but not to Pulumi yet.

--- src/rust/plugin-executor/src/upstreams.rs
+++ src/rust/plugin-executor/src/upstreams.rs
@@ -3,7 +3,8 @@ use std::time::Duration;
 use plugin_work_queue::client::PluginWorkQueueServiceClient;
 use tonic::transport::Endpoint;
 
-pub async fn plugin_work_queue_client_from_env() -> Result<PluginWorkQueueServiceClient, Box<dyn std::error::Error>> {
+pub async fn plugin_work_queue_client_from_env(
+) -> Result<PluginWorkQueueServiceClient, Box<dyn std::error::Error>> {
     const ADDRESS_ENV_VAR: &'static str = "PLUGIN_WORK_QUEUE_CLIENT_ADDRESS";
     let address = std::env::var(ADDRESS_ENV_VAR).expect(ADDRESS_ENV_VAR);
     let endpoint = Endpoint::from_shared(address)?
