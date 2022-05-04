7b441171df61b01762e8a5cc687dcf57bfcce3f3
Max Wittek <wimax@graplsecurity.com>
2022-05-04 10:35:16 -0400

    add a VERBOSE_DEVBOX_SYNC=1 to devbox-do/devbox-sync

--- devbox/devbox-sync.sh
+++ devbox/devbox-sync.sh
@@ -8,9 +8,18 @@ source "${THIS_DIR}/lib.sh"
 # shellcheck disable=SC1090
 source "${GRAPL_DEVBOX_CONFIG}"
 
+readonly VERBOSE_DEVBOX_SYNC="${VERBOSE_DEVBOX_SYNC:-0}"
+
+rsync_progress_args=()
+if [[ ${VERBOSE_DEVBOX_SYNC} -ne 0 ]]; then
+    rsync_progress_args+=(--progress --verbose)
+else
+    rsync_progress_args+=(--info=progress2)
+fi
+
 # the --include stuff was inspired by https://stackoverflow.com/posts/63438492/revisions
 
-if ! rsync --archive --info=progress2 \
+if ! rsync --archive "${rsync_progress_args[@]}" \
     --include='**.gitignore' --exclude='**/.git' --filter=':- .gitignore' --delete-after \
     --rsh "${THIS_DIR}/ssh.sh" \
     "${GRAPL_DEVBOX_LOCAL_GRAPL}/" \
