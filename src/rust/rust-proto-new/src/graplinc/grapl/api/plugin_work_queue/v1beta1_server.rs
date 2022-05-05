ba6e21c14174a6d9344a2ab6c2f94b91c2c36ce5
Max Wittek <wimax@graplsecurity.com>
2022-05-04 21:20:15 -0400

    beginning apifn stuff

--- src/rust/rust-proto-new/src/graplinc/grapl/api/plugin_work_queue/v1beta1_server.rs
+++ src/rust/rust-proto-new/src/graplinc/grapl/api/plugin_work_queue/v1beta1_server.rs
@@ -1,1 +1,31 @@
+use crate::graplinc::grapl::api::plugin_registry::v1beta1 as native;
 
+/// Implement this trait to define the API business logic
+#[tonic::async_trait]
+pub trait PluginWorkQueueApi<E>
+where
+    E: Into<tonic::Status>,
+{
+    /*
+    async fn create_plugin(&self, request: CreatePluginRequest) -> Result<CreatePluginResponse, E>;
+
+    async fn get_plugin(&self, request: GetPluginRequest) -> Result<GetPluginResponse, E>;
+
+    async fn deploy_plugin(&self, request: DeployPluginRequest) -> Result<DeployPluginResponse, E>;
+
+    async fn tear_down_plugin(
+        &self,
+        request: TearDownPluginRequest,
+    ) -> Result<TearDownPluginResponse, E>;
+
+    async fn get_generators_for_event_source(
+        &self,
+        request: GetGeneratorsForEventSourceRequest,
+    ) -> Result<GetGeneratorsForEventSourceResponse, E>;
+
+    async fn get_analyzers_for_tenant(
+        &self,
+        request: GetAnalyzersForTenantRequest,
+    ) -> Result<GetAnalyzersForTenantResponse, E>;
+    */
+}
