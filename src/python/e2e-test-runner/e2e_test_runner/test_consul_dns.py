90d3fe87978ef3142bfd5f562d3aad6b055d4d9a
Thomas Wunderlich <thomas@graplsecurity.com>
2022-05-23 14:04:58 -0400

    Skip consul dns test
    There's a fix for this behavior in an upcoming Nomad release, but this
    is currently causing e2e tests to fail in AWS. Since this is coming
    soon, we should skip the test for now

--- src/python/e2e-test-runner/e2e_test_runner/test_consul_dns.py
+++ src/python/e2e-test-runner/e2e_test_runner/test_consul_dns.py
@@ -3,7 +3,11 @@ Smoke test to ensure that consul dns is available from within the service mesh
 """
 import socket
 
+import pytest
 
+
+# TODO re-enable this after the fix to Nomad DNS is released
+@pytest.mark.skip(reason="Waiting on a Nomad release with the fix for this")
 def test_if_consul_dns_resolves() -> None:
     try:
         ipaddress = socket.gethostbyname("web-ui.service.consul")
