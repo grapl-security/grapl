75b85142cfc027fff2a81af23eb127e1bc8aa260
Max Wittek <wimax@graplsecurity.com>
2022-05-26 16:45:16 -0400

    remove test-threads???

--- test/run/rust-integration-tests.sh
+++ test/run/rust-integration-tests.sh
@@ -13,7 +13,7 @@ declare -a FAILING_TESTS=()
 for test in $(find /tests -type f -executable -exec readlink -f {} \;); do
     echo "--- Executing ${test}"
     # Redirect stderr so it's inline with stdout
-    "${test}" --test-threads=1
+    "${test}"
     exit_code=$?
     if [[ ${exit_code} -ne 0 ]]; then
         FAILING_TESTS+=("${test}")
