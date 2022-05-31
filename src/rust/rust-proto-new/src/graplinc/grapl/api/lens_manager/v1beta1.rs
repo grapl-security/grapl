fef87c25e0fa4828cbb48162002da4973405f80a
Andrea <andrea@graplsecurity.com>
2022-05-30 18:51:29 -0600

    add lens manager proto file with  adding mesasges and comments

---
+++ src/rust/rust-proto-new/src/graplinc/grapl/api/lens_manager/v1beta1.rs
@@ -0,0 +1,11 @@
+use bytes::Bytes;
+
+use crate::{
+    graplinc::common::v1beta1::{
+        SystemTime,
+        Uuid
+    },
+    protobufs::graplinc::grapl::api::lens_manager::v1beta1::{
+
+    }
+}
