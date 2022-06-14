b74ec836908c5c37c1668a3be61049dd5a5313c1
Thomas Wunderlich <thomas@graplsecurity.com>
2022-06-14 13:16:46 -0400

    Update docs for ssm_ssh

--- bin/aws/ssm_ssh.sh
+++ bin/aws/ssm_ssh.sh
@@ -3,7 +3,8 @@
 # This script sets up ssh via ssm to a server type (One of Consul Server, Nomad Agent or Nomad Server).
 # This is a temporary workaround until we set up a VPN
 #
-# Input (optional): Port to forward to. This will be both the port on the server to forward and the local port that is forwarded to. Defaults to port 1234
+# Input: Server type. This should be one of "Consul Server", "Nomad Agent" or "Nomad Server"
+# Usage: ./bin/aws/ssm_ssh.sh "Nomad Server"
 
 set -euo pipefail
 
