9e4369939595a93a20835ac644e2203639d7ea0e
jgrillo <jgrillo@graplsecurity.com>
2022-06-28 17:51:44 -0400

    port osquery-generator to new rust-proto

--- src/rust/generators/osquery-generator/src/main.rs
+++ src/rust/generators/osquery-generator/src/main.rs
@@ -1,25 +1,63 @@
-use graph_generator_lib::*;
-use osquery_generator_lib::{
-    generator::OSQueryGenerator,
-    metrics::OSQueryGeneratorMetrics,
+use opentelemetry::{
+    global,
+    sdk::propagation::TraceContextPropagator,
 };
+use rust_proto::graplinc::grapl::{
+    api::graph::v1beta1::GraphDescription,
+    pipeline::{
+        v1beta1::RawLog,
+        v1beta2::Envelope,
+    },
+};
+use thiserror::Error;
+use tracing_subscriber::{
+    prelude::*,
+    EnvFilter,
+};
+
+mod parsers;
+
+#[non_exhaustive]
+#[derive(Debug, Error)]
+pub enum OsqueryGeneratorError {
+    #[error("error configuring tracing {0}")]
+    TraceError(#[from] opentelemetry::trace::TraceError),
+}
+
 #[tokio::main]
 #[tracing::instrument]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (env, _guard) = grapl_config::init_grapl_env!();
-    let service_name = env.service_name.clone();
+async fn main() -> Result<(), OsqueryGeneratorError> {
+    let (non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
 
-    tracing::info!(
-        message = "Starting generator.",
-        name =% service_name
-    );
+    // initialize json logging layer
+    let log_layer = tracing_subscriber::fmt::layer()
+        .json()
+        .with_writer(non_blocking);
 
-    run_graph_generator(
-        env,
-        move |cache| OSQueryGenerator::new(cache, OSQueryGeneratorMetrics::new(&service_name)),
-        grapl_service::decoder::NdjsonDecoder::default(),
-    )
-    .await;
+    // initialize tracing layer
+    global::set_text_map_propagator(TraceContextPropagator::new());
+    let tracer = opentelemetry_jaeger::new_pipeline()
+        .with_service_name("pipeline-ingress")
+        .install_batch(opentelemetry::runtime::Tokio)?;
 
+    // register a subscriber
+    let filter = EnvFilter::from_default_env();
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(log_layer)
+        .with(tracing_opentelemetry::layer().with_tracer(tracer))
+        .init();
+
+    tracing::info!("logger configured successfully");
+
+    // TODO: actually do something here
     Ok(())
+}
+
+// TODO: when we have a plugin SDK, hook this binary into it here
+#[allow(dead_code)]
+async fn event_handler(
+    _event: Envelope<RawLog>,
+) -> Result<Envelope<GraphDescription>, OsqueryGeneratorError> {
+    todo!();
 }
