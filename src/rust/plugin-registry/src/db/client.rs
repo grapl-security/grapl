aea2df435467dcc43a5a993b84f204f123dfca76
jgrillo-grapl <64786504+jgrillo-grapl@users.noreply.github.com>
2022-06-30 17:23:10 -0400

    Internal 719 get generator rpc (#1784)
    This PR makes some protocol buffer updates to simplify the plugin-registry's data model. We've consolidated the concept of PluginMetadata and removed the ability to download the plugin binary via RPC because it'll never be used.
    
    This PR also implements the GetGeneratorsForEventSource RPC method, which necessitated a database schema migration to add event_source_id to the plugins table and associated updates to the data access layer.

--- src/rust/plugin-registry/src/db/client.rs
+++ src/rust/plugin-registry/src/db/client.rs
@@ -4,6 +4,7 @@ use rust_proto::graplinc::grapl::api::plugin_registry::v1beta1::PluginType;
 use super::models::{
     PluginDeploymentRow,
     PluginDeploymentStatus,
+    PluginIdRow,
     PluginRow,
 };
 
@@ -15,9 +16,30 @@ pub struct DbCreatePluginArgs {
     pub tenant_id: uuid::Uuid,
     pub display_name: String,
     pub plugin_type: PluginType,
+    pub event_source_id: Option<uuid::Uuid>,
 }
 
 impl PluginRegistryDbClient {
+    #[tracing::instrument(skip(self), err)]
+    pub async fn get_generators_for_event_source(
+        &self,
+        event_source_id: &uuid::Uuid,
+    ) -> Result<Vec<PluginIdRow>, sqlx::Error> {
+        sqlx::query_as!(
+            PluginIdRow,
+            r"
+            SELECT
+            plugin_id
+            FROM plugins
+            WHERE event_source_id = $1 AND plugin_type = $2;
+            ",
+            event_source_id,
+            PluginType::Generator.type_name(),
+        )
+        .fetch_all(&self.pool)
+        .await
+    }
+
     #[tracing::instrument(skip(self), err)]
     pub async fn get_plugin(&self, plugin_id: &uuid::Uuid) -> Result<PluginRow, sqlx::Error> {
         sqlx::query_as!(
@@ -28,7 +50,8 @@ impl PluginRegistryDbClient {
             tenant_id,
             display_name,
             plugin_type,
-            artifact_s3_key
+            artifact_s3_key,
+            event_source_id
             FROM plugins
             WHERE plugin_id = $1
             ",
@@ -68,24 +91,46 @@ impl PluginRegistryDbClient {
         args: DbCreatePluginArgs,
         s3_key: &str,
     ) -> Result<(), sqlx::Error> {
-        sqlx::query!(
-            r"
-            INSERT INTO plugins (
+        match args.event_source_id {
+            Some(event_source_id) => sqlx::query!(
+                r"
+                INSERT INTO plugins (
+                    plugin_id,
+                    plugin_type,
+                    display_name,
+                    tenant_id,
+                    artifact_s3_key,
+                    event_source_id
+                )
+                VALUES ($1::uuid, $2, $3, $4::uuid, $5, $6::uuid)
+                ON CONFLICT DO NOTHING;
+                ",
                 plugin_id,
-                plugin_type,
-                display_name,
-                tenant_id,
-                artifact_s3_key
-            )
-            VALUES ($1::uuid, $2, $3, $4::uuid, $5)
-            ON CONFLICT DO NOTHING;
-            ",
-            plugin_id,
-            &args.plugin_type.type_name(),
-            &args.display_name,
-            &args.tenant_id,
-            s3_key,
-        )
+                &args.plugin_type.type_name(),
+                &args.display_name,
+                &args.tenant_id,
+                s3_key,
+                event_source_id,
+            ),
+            None => sqlx::query!(
+                r"
+                INSERT INTO plugins (
+                    plugin_id,
+                    plugin_type,
+                    display_name,
+                    tenant_id,
+                    artifact_s3_key
+                )
+                VALUES ($1::uuid, $2, $3, $4::uuid, $5)
+                ON CONFLICT DO NOTHING;
+                ",
+                plugin_id,
+                &args.plugin_type.type_name(),
+                &args.display_name,
+                &args.tenant_id,
+                s3_key,
+            ),
+        }
         .execute(&self.pool)
         .await
         .map(|_| ()) // Toss result
---
+++ src/rust/plugin-registry/tests/test_get_generators_for_event_source.rs
@@ -0,0 +1,83 @@
+#![cfg(feature = "integration_tests")]
+
+use grapl_utils::future_ext::GraplFutureExt;
+use plugin_registry::client::FromEnv;
+use rust_proto::graplinc::grapl::api::plugin_registry::v1beta1::{
+    GetGeneratorsForEventSourceRequest,
+    PluginMetadata,
+    PluginRegistryServiceClient,
+    PluginType,
+};
+
+#[test_log::test(tokio::test)]
+async fn test_get_generators_for_event_source() -> Result<(), Box<dyn std::error::Error>> {
+    tracing::debug!(
+        env=?std::env::args(),
+    );
+
+    let mut client = PluginRegistryServiceClient::from_env().await?;
+
+    let tenant_id = uuid::Uuid::new_v4();
+    let generator1_display_name = "my first generator".to_string();
+    let generator2_display_name = "my second generator".to_string();
+    let analyzer_display_name = "my analyzer".to_string();
+    let event_source_id = uuid::Uuid::new_v4();
+
+    let generator1_metadata = PluginMetadata {
+        tenant_id: tenant_id.clone(),
+        display_name: generator1_display_name.clone(),
+        plugin_type: PluginType::Generator,
+        event_source_id: Some(event_source_id),
+    };
+    let generator2_metadata = PluginMetadata {
+        tenant_id: tenant_id.clone(),
+        display_name: generator2_display_name.clone(),
+        plugin_type: PluginType::Generator,
+        event_source_id: Some(event_source_id),
+    };
+
+    let analyzer_metadata = PluginMetadata {
+        tenant_id: tenant_id.clone(),
+        display_name: analyzer_display_name.clone(),
+        plugin_type: PluginType::Analyzer,
+        event_source_id: None,
+    };
+
+    let chunk = b"chonk".to_vec();
+
+    let create_generator1_response = client
+        .create_plugin(generator1_metadata, chunk.clone().into_iter())
+        .timeout(std::time::Duration::from_secs(5))
+        .await??;
+    let generator1_plugin_id = create_generator1_response.plugin_id;
+
+    let create_generator2_response = client
+        .create_plugin(generator2_metadata, chunk.clone().into_iter())
+        .timeout(std::time::Duration::from_secs(5))
+        .await??;
+    let generator2_plugin_id = create_generator2_response.plugin_id;
+
+    let create_analyzer_response = client
+        .create_plugin(analyzer_metadata, chunk.clone().into_iter())
+        .timeout(std::time::Duration::from_secs(5))
+        .await??;
+    let analyzer_plugin_id = create_analyzer_response.plugin_id;
+
+    let generators_for_event_source_response = client
+        .get_generators_for_event_source(GetGeneratorsForEventSourceRequest { event_source_id })
+        .timeout(std::time::Duration::from_secs(5))
+        .await??;
+
+    assert_eq!(generators_for_event_source_response.plugin_ids.len(), 2);
+    assert!(generators_for_event_source_response
+        .plugin_ids
+        .contains(&generator1_plugin_id));
+    assert!(generators_for_event_source_response
+        .plugin_ids
+        .contains(&generator2_plugin_id));
+    assert!(!generators_for_event_source_response
+        .plugin_ids
+        .contains(&analyzer_plugin_id));
+
+    Ok(())
+}
