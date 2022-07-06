76bef998cd0bd25a1f7e51a1764f95af25de466e
Andrea <andrea@graplsecurity.com>
2022-07-06 12:52:09 -0600

    WIP - add client & server, add rust-proto-new to Cargo.toml instead of rust-proto

--- src/js/engagement_view/src/components/login/Login.tsx
+++ src/js/engagement_view/src/components/login/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+    import React from "react";
 import * as Yup from "yup";
 import { Field, Form, Formik } from "formik";
 import { loginService } from "services/login/loginService";
