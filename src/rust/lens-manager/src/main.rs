127f2bcb6b38f935992c684205c5ace341020481
Andrea <andrea@graplsecurity.com>
2022-07-06 12:52:09 -0600

    WIP

--- src/rust/lens-manager/src/main.rs
+++ src/rust/lens-manager/src/main.rs
@@ -1,28 +1,11 @@
-use rust_proto_new::graplinc::grapl::api::graph_mutation::v1beta1::client::GraphMutationClient;
-use rust_proto_new::graplinc::grapl::api::graph_mutation::v1beta1::messages::CreateNodeRequest;
-use rust_proto_new::graplinc::grapl::common::v1beta1::NodeType;
+use std::error::Error;
 
-struct LensManager {
-    graph_mutation_client: GraphMutationClient,
-}
+// hook to grpc
+// write off to kafka
+// pipeline ingress uses kafka crate src/rust
 
-impl LensManager {
-
-    async fn create_lens(&self, tenant_id: uuid::Uuid) -> Result<Uid, Box<dyn std::error::Error>> {
-        self.graph_mutation_client.create_node(
-            CreateNodeRequest {
-                tenant_id,
-                NodeType { value: "Lens".to_owned() },
-            }
-        ).await?;
-
-        // todo: Publish update to lens-updates topic
-
-        todo!()
-    }
-}
-
-
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    println!("Testing");
+    Ok(())
 }
