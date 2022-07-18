763bc5d8c148a822f9d6efbdb57fc1d4a8473799
Max Wittek <wimax@graplsecurity.com>
2022-07-18 17:12:29 -0400

    ugh

--- src/rust/plugin-execution-sidecar/generator-execution-sidecar.rs
+++ src/rust/plugin-execution-sidecar/generator-execution-sidecar.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (_env, _guard) = grapl_config::init_grapl_env!();
     let plugin_executor_config = PluginExecutorConfig::parse();
 
-    let generator_work_processor = GeneratorWorkProcessor::new().await?;
+    let generator_work_processor = GeneratorWorkProcessor::new(&plugin_executor_config).await?;
     let mut plugin_executor =
         PluginExecutor::new(plugin_executor_config, generator_work_processor).await?;
     plugin_executor.main_loop().await
