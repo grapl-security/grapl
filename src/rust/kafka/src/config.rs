2ee1696e1194f370e5ea88e5107db4edf884dba9
Max Wittek <wimax@graplsecurity.com>
2022-07-19 16:52:20 -0400

    Flesh out with_topic et al

--- src/rust/kafka/src/config.rs
+++ src/rust/kafka/src/config.rs
@@ -15,10 +15,11 @@ pub struct ConsumerConfig {
 }
 
 impl ConsumerConfig {
-    // In some applications, where we need to produce to >1 topic,
-    // we'll specify the env-var containing the topic name instead of depending
-    // on the default KAFKA_PRODUCER_TOPIC.
-    pub fn with_topic(topic: &'static str) -> Self {
+    // In some test cases, where we need to consume from >1 topic,
+    // we'll manually specify the topic name in code instead of using
+    // the default KAFKA_CONSUMER_TOPIC.
+    // (Example: rust-integration-tests consumes from many topics)
+    pub fn with_topic(topic: &str) -> Self {
         let app_args = std::env::args();
         let extra_args = ["--topic".to_string(), topic.to_string()];
         Self::parse_from(app_args.chain(extra_args.into_iter()))
@@ -55,10 +56,15 @@ impl ProducerConfig {
     // In some applications, where we need to produce to >1 topic,
     // we'll specify the env-var containing the topic name instead of depending
     // on the default KAFKA_PRODUCER_TOPIC.
+    // (Example: plugin-work-queue produces to both generator and analyzer)
     pub fn with_topic_env_var(key: &'static str) -> Self {
-        let app_args = std::env::args();
         let topic = std::env::var(key).expect(key);
-        let extra_args = ["--topic".to_string(), topic];
+        Self::with_topic(&topic)
+    }
+
+    fn with_topic(topic: &str) -> Self {
+        let app_args = std::env::args();
+        let extra_args = ["--topic".to_string(), topic.to_string()];
         Self::parse_from(app_args.chain(extra_args.into_iter()))
     }
 }
