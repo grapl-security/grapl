010a8a3fdd825dbb6a698b342171e92d7fd7a634
inickles-grapl <inickles@graplsecurity.com>
2022-07-27 16:05:24 -0700

    migrate to clap.

--- src/rust/grapl-web-ui/src/main.rs
+++ src/rust/grapl-web-ui/src/main.rs
@@ -2,7 +2,7 @@
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (_env, _guard) = grapl_config::init_grapl_env!();
 
-    let config = grapl_web_ui::ConfigBuilder::from_env()?.build()?;
+    let config = grapl_web_ui::Config::from_env()?;
 
     grapl_web_ui::run(config)?.await?;
 
