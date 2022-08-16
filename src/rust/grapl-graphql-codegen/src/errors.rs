0de01e1e56e709f98bdcd4432c3fab6d43bd1ee4
Max Wittek <69007229+wimax-grapl@users.noreply.github.com>
2022-08-16 18:42:04 -0400

    A grab-bag of Grapl Graphql Codegen improvements, mostly around removing <'a>s  (#1896)
    * A grab-bag of Grapl Graphql Codegen improvements
    
    * revert a cargo toml change
    
    * Revert a change in the external_helpers

--- src/rust/grapl-graphql-codegen/src/errors.rs
+++ src/rust/grapl-graphql-codegen/src/errors.rs
@@ -3,24 +3,24 @@ use graphql_parser::schema::Directive;
 use crate::node_type::MergeFailure;
 
 #[derive(Debug, thiserror::Error)]
-pub enum CodeGenError<'a> {
+pub enum CodeGenError {
     #[error("NodeTypeParseError")]
     NodeTypeParseError,
     #[error("Unsupported ConflictResolution")]
     UnsupportedConflictResolution {
-        directives: Vec<Directive<'a, &'a str>>,
+        directives: Vec<Directive<'static, String>>,
     },
     #[error("Missing Node IdentificationAlgorithm")]
     MissingNodeIdentificationAlgorithm {
-        directives: Vec<Directive<'a, &'a str>>,
+        directives: Vec<Directive<'static, String>>,
     },
     #[error("Missing Grapl Directive")]
     MissingGraplDirective {
-        directives: Vec<Directive<'a, &'a str>>,
+        directives: Vec<Directive<'static, String>>,
     },
     #[error("Missing Arguments for Grapl Directive")]
     MissingGraplDirectiveArguments {
-        directives: Vec<Directive<'a, &'a str>>,
+        directives: Vec<Directive<'static, String>>,
     },
     #[error("Failed to extend node schema")]
     MergeFailure(#[from] MergeFailure),
