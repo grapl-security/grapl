010d43882a34eaa0baf76b7e9ce18734dc281505
Max Wittek <wimax@graplsecurity.com>
2022-08-17 13:24:19 -0400

    Move more out to db/

--- src/rust/schema-manager/src/db/models.rs
+++ src/rust/schema-manager/src/db/models.rs
@@ -22,3 +22,67 @@ impl From<StoredEdgeCardinality> for EdgeCardinality {
         }
     }
 }
+
+#[derive(sqlx::Type, Copy, Clone, Debug)]
+#[sqlx(type_name = "property_type")]
+enum StoredPropertyType {
+    ImmutableString,
+    ImmutableI64,
+    MaxI64,
+    MinI64,
+    ImmutableU64,
+    MaxU64,
+    MinU64,
+}
+
+#[derive(sqlx::Type, Clone, Debug)]
+struct NodeIdentityRow {
+    identity_algorithm: String,
+    tenant_id: uuid::Uuid,
+    node_type: String,
+    schema_version: i16,
+}
+
+#[derive(sqlx::Type, Clone, Debug)]
+struct SessionIdentityRow {
+    tenant_id: uuid::Uuid,
+    identity_algorithm: String,
+    node_type: String,
+    schema_version: i16,
+    pseudo_key_properties: Vec<String>,
+    negation_key_properties: Vec<String>,
+    creation_timestamp_property: String,
+    last_seen_timestamp_property: String,
+    termination_timestamp_property: String,
+}
+
+#[derive(sqlx::Type, Clone, Debug)]
+struct NodeSchemaRow {
+    tenant_id: sqlx::types::uuid::Uuid,
+    identity_algorithm: String,
+    node_type: String,
+    schema_version: i16,
+    deployment_timestamp: sqlx::types::time::PrimitiveDateTime,
+    schema_type: String,
+}
+
+#[derive(sqlx::Type, Clone, Debug)]
+struct PropertySchemaRow {
+    tenant_id: sqlx::types::uuid::Uuid,
+    node_type: String,
+    schema_version: i16,
+    property_name: String,
+    property_type: StoredPropertyType,
+    identity_only: bool,
+}
+
+#[derive(sqlx::Type, Clone, Debug)]
+struct EdgeSchemaRow {
+    tenant_id: sqlx::types::uuid::Uuid,
+    node_type: String,
+    schema_version: i16,
+    forward_edge_name: String,
+    reverse_edge_name: String,
+    forward_edge_cardinality: StoredEdgeCardinality,
+    reverse_edge_cardinality: StoredEdgeCardinality,
+}
