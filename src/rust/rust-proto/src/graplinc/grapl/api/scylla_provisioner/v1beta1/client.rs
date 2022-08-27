0a8f336eb206260a565deadc16381118c2fe1e72
Max Wittek <wimax@graplsecurity.com>
2022-08-27 12:25:33 -0400

    Update the scylla provisioner client to wimax style

--- src/rust/rust-proto/src/graplinc/grapl/api/scylla_provisioner/v1beta1/client.rs
+++ src/rust/rust-proto/src/graplinc/grapl/api/scylla_provisioner/v1beta1/client.rs
@@ -1,55 +1,61 @@
+use std::time::Duration;
+
+use client_executor::{Executor, ExecutorConfig};
+use tonic::transport::Endpoint;
+
 use crate::{
     graplinc::grapl::api::scylla_provisioner::v1beta1::messages::{
-        DeployGraphSchemasRequest,
-        DeployGraphSchemasResponse,
+        self as native,
     },
     protobufs::graplinc::grapl::api::scylla_provisioner::v1beta1::{
         scylla_provisioner_service_client::ScyllaProvisionerServiceClient,
-        DeployGraphSchemasRequest as DeployGraphSchemasRequestProto,
+        self as proto,
     },
-    protocol::status::Status,
-    SerDeError,
+    protocol::{error::GrpcClientError, service_client::{Connectable, ConnectError}},
+    create_proto_client, execute_client_rpc, client_macros::ExecuteClientRpcOptions,
 };
 
-#[derive(thiserror::Error, Debug)]
-pub enum ScyllaProvisionerClientError {
-    #[error("Failed to deserialize response {0}")]
-    SerDeError(#[from] SerDeError),
-    #[error("Status {0}")]
-    Status(#[from] Status),
-    #[error("ConnectError")]
-    ConnectError(tonic::transport::Error),
+pub type ScyllaProvisionerClientError = GrpcClientError;
+
+
+pub struct ScyllaProvisionerClient {
+    proto_client: ScyllaProvisionerServiceClient<tonic::transport::Channel>,
+    executor: Executor,
 }
 
-#[derive(Clone)]
-pub struct ScyllaProvisionerClient {
-    inner: ScyllaProvisionerServiceClient<tonic::transport::Channel>,
+#[async_trait::async_trait]
+impl Connectable for ScyllaProvisionerClient {
+    const SERVICE_NAME: &'static str =
+        "graplinc.grapl.api.scylla_provisioner.v1beta1.ScyllaProvisionerService";
+
+    #[tracing::instrument(err)]
+    async fn connect(endpoint: Endpoint) -> Result<Self, ConnectError> {
+        let executor = Executor::new(ExecutorConfig::new(Duration::from_secs(30)));
+        let proto_client = create_proto_client!(
+            executor,
+            ScyllaProvisionerServiceClient<tonic::transport::Channel>,
+            endpoint,
+        );
+
+        Ok(Self {
+            proto_client,
+            executor,
+        })
+    }
 }
 
 impl ScyllaProvisionerClient {
-    pub async fn connect<T>(endpoint: T) -> Result<Self, ScyllaProvisionerClientError>
-    where
-        T: TryInto<tonic::transport::Endpoint>,
-        T::Error: std::error::Error + Send + Sync + 'static,
-    {
-        Ok(ScyllaProvisionerClient {
-            inner: ScyllaProvisionerServiceClient::connect(endpoint)
-                .await
-                .map_err(ScyllaProvisionerClientError::ConnectError)?,
-        })
-    }
-
-    pub async fn query_graph_with_uid(
+    pub async fn deploy_graph_schemas(
         &mut self,
-        request: DeployGraphSchemasRequest,
-    ) -> Result<DeployGraphSchemasResponse, ScyllaProvisionerClientError> {
-        let request: DeployGraphSchemasRequestProto = request.into();
-        Ok(self
-            .inner
-            .deploy_graph_schemas(request)
-            .await
-            .map_err(Status::from)?
-            .into_inner()
-            .try_into()?)
+        request: native::DeployGraphSchemasRequest,
+    ) -> Result<native::DeployGraphSchemasResponse, ScyllaProvisionerClientError> {
+        execute_client_rpc!(
+            self,
+            request,
+            deploy_graph_schemas,
+            proto::DeployGraphSchemasRequest,
+            native::DeployGraphSchemasResponse,
+            ExecuteClientRpcOptions::default(),
+        )
     }
 }
