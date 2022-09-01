222ad4c530518a9cc6f30fe5877bc21fdc313f72
Christopher Maier <chris@graplsecurity.com>
2022-09-01 13:34:11 -0400

    Add a `bk-run` command to trigger Buildkite pipelines
    In order to more easily execute our integration and end-to-end
    tests (which have become more difficult to reliably execute on our
    Chromebooks lately), we have decided to take advantage of our existing
    Buildkite infrastructure to serve as a kind of on-demand testing
    cluster.
    
    To facilitate this, we introduce here a `bk-run` command that will
    trigger a Buildkite pipeline based on the current state of your local
    Git repository. It uses Buildkite's own `bk` CLI tool under the hood
    to perform the heavy lifting, but with some intelligent wrapping logic
    to make things as straightforward and "do what I mean" as possible.
    
    Assuming that your current branch has been pushed to Github, you can
    simply run:
    
        ./pants bk-run
    
    to trigger a "verify" pipeline run based on that code (no pull request
    required!).
    
    (If you haven't pushed, adding the `--push` option will perform a `git
    push` that effectively resets the remote branch to your current commit
    before triggering the pipeline.)
    
    There are additional options that you may or may not be interested in;
    run `./pants bk-run --help` to learn more. However, the intention is
    that for the overwhelming majority of cases, the most you'll ever need
    to do is run `./pants bk-run --push`.
    
    Installation of the `bk` tool has been added to our ChromeOS setup
    scripts; run them again to install `bk`. Then, run `bk configure` to
    set up the appropriate credentials you will need.
    
    Signed-off-by: Christopher Maier <chris@graplsecurity.com>

---
+++ build-support/bk-run.py
@@ -0,0 +1,179 @@
+import re
+import shutil
+import subprocess
+import sys
+from os import path
+from typing import Final, Optional
+
+import typer
+from rich.console import Console
+
+DEFAULT_BUILDKITE_ORGANIZATION: Final[str] = "grapl"
+DEFAULT_PIPELINE_STAGE: Final[str] = "verify"
+
+err_console = Console(stderr=True)
+app = typer.Typer(add_completion=False, rich_markup_mode="rich")
+
+
+def _ensure_bk() -> None:
+    """
+    Ensure that the `bk` CLI tool is present; if it's not, there's not much we can do!
+    """
+    if not shutil.which("bk"):
+        err_console.print(
+            f":cross_mark: Cannot find [bold white]bk[/bold white] in $PATH. Download it from https://github.com/buildkite/cli/releases. Be sure to run [bold white]bk configure[/bold white]!"
+        )
+        sys.exit(1)
+
+    bk_config_file = "~/.buildkite/config.json"
+    if not path.exists(path.expanduser(bk_config_file)):
+        err_console.print(
+            f":cross_mark: Cannot find [bold white]{bk_config_file}[/bold white]. Please run [bold white]bk configure[/bold white] first."
+        )
+        sys.exit(1)
+
+
+def _repository_name() -> str:
+    """Extract the repository name from the Git remote URL. Assumes the
+    remote is named "origin".
+
+    """
+    result = subprocess.run(
+        ["git", "config", "remote.origin.url"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    url = result.stdout
+    # Strip off any ".git" suffix; this simplifies the subsequent
+    # regular expression
+    url = re.sub(r"\.git$", "", url)
+    regex = re.compile("^(git@github.com:|https://github.com/)([^/]+)/(?P<repo>.+)")
+    if match := regex.search(url):
+        return match["repo"]
+    else:
+        raise Exception("Could not extract repository name!")
+
+
+def _message(ref: str) -> str:
+    """
+    Extract the commit title of the given reference to use as a pipeline message.
+    """
+    result = subprocess.run(
+        ["git", "show", "--pretty=format:%s", "--no-patch", ref],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    return result.stdout
+
+
+def _current_branch() -> str:
+    """
+    Determine the name of the currently checked-out Git branch.
+    """
+    result = subprocess.run(
+        ["git", "rev-parse", "--abbrev-ref", "HEAD"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    return result.stdout.strip()
+
+
+def _push(commit: str, branch: str) -> None:
+    """
+    Force-push COMMIT to BRANCH on the `origin` upstream.
+    """
+    command = [
+        "git",
+        "push",
+        "--set-upstream",
+        "--force-with-lease",
+        "origin",
+        f"{commit}:{branch}",
+    ]
+    err_console.print(f"Executing: [bold white]{' '.join(command)}[/bold white]")
+    subprocess.run(command, check=True)
+
+
+def _trigger(pipeline: str, branch: str, commit: str, message: str) -> None:
+    """
+    Trigger a Buildkite pipeline run using the `bk` CLI tool.
+    """
+    command = [
+        "bk",
+        "build",
+        "create",
+        "--pipeline",
+        pipeline,
+        "--branch",
+        branch,
+        "--commit",
+        commit,
+        "--message",
+        message,
+    ]
+    err_console.print(f"Executing: [bold white]{' '.join(command)}[/bold white]")
+    subprocess.run(command, check=True)
+
+
+@app.command()
+def main(
+    commit: str = typer.Option("HEAD", help="The commit to execute in the pipeline"),
+    branch: Optional[str] = typer.Option(
+        None,
+        help="The branch on which to execute the pipeline. If not specified, uses the current local branch.",
+    ),
+    message: Optional[str] = typer.Option(
+        None,
+        help="The message to use for the pipeline execution. If not specified, uses the specified commit title.",
+    ),
+    push: bool = typer.Option(
+        False,
+        help="Whether or not to push the specified commit/branch to before triggering a pipeline",
+    ),
+    buildkite_organization: str = typer.Option(
+        DEFAULT_BUILDKITE_ORGANIZATION,
+        help="The Buildkite organization to interact with.",
+    ),
+    pipeline_stage: str = typer.Option(
+        DEFAULT_PIPELINE_STAGE, help="The pipeline stage to trigger."
+    ),
+) -> None:
+    """Trigger a Buildkite pipeline run.
+
+    By default, triggers a pipeline run based on the current branch
+    and commit. You can pass the [bold white]--push[/bold white]
+    option to perform a [bold white]git push[/bold white] before
+    triggering the pipeline.
+
+    Assumes that Buildkite pipeline slugs are set up per our local
+    convention of [bold white]REPOSITORY-STAGE[/bold white], where
+    [bold white]STAGE[/bold white] is [bold white]verify[/bold white],
+    [bold white]merge[/bold white], etc.
+
+    """
+    _ensure_bk()
+
+    repo = _repository_name()
+    pipeline = f"{buildkite_organization}/{repo}-{pipeline_stage}"
+    if not branch:
+        branch = _current_branch()
+    if not message:
+        message = _message(commit)
+
+    err_console.print(f"Git Repository: [bold white]{repo}[/bold white]")
+    err_console.print(f"Git Branch: [bold white]{branch}[/bold white]")
+    err_console.print(f"Git Commit: [bold white]{commit}[/bold white]")
+    err_console.print(f"Buildkite Pipeline: [bold white]{pipeline}[/bold white]")
+    err_console.print(f"Pipeline Message: [bold white]{message}[/bold white]")
+
+    if push:
+        _push(branch=branch, commit=commit)
+
+    _trigger(pipeline=pipeline, branch=branch, commit=commit, message=message)
+
+
+if __name__ == "__main__":
+    app(prog_name="bk-run")
