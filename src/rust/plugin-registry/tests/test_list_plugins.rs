f049053e0cfb28970b095283f1d085f1a99909be
jgrillo <jgrillo@graplsecurity.com>
2022-09-08 11:40:54 -0400

    updates for review comments

--- src/rust/plugin-registry/tests/test_list_plugins.rs
+++ src/rust/plugin-registry/tests/test_list_plugins.rs
@@ -2,7 +2,6 @@
 
 use bytes::Bytes;
 use clap::Parser;
-use grapl_utils::future_ext::GraplFutureExt;
 use rust_proto::{
     client_factory::{
         build_grpc_client,
@@ -15,10 +14,6 @@ use rust_proto::{
     },
 };
 
-fn get_example_generator() -> Result<Bytes, std::io::Error> {
-    std::fs::read("/test-fixtures/example-generator").map(Bytes::from)
-}
-
 #[test_log::test(tokio::test)]
 async fn test_list_plugins() -> eyre::Result<()> {
     let client_config = PluginRegistryClientConfig::parse();
@@ -30,7 +25,7 @@ async fn test_list_plugins() -> eyre::Result<()> {
 
     let generator_1_create_response = {
         let display_name = "generator 1";
-        let artifact = get_example_generator()?;
+        let artifact = Bytes::from("fake");
         let metadata = PluginMetadata::new(
             tenant_id,
             display_name.to_string(),
@@ -43,15 +38,14 @@ async fn test_list_plugins() -> eyre::Result<()> {
                 metadata,
                 futures::stream::once(async move { artifact.clone() }),
             )
-            .timeout(std::time::Duration::from_secs(5))
-            .await??
+            .await?
     };
 
     let generator_1_id = generator_1_create_response.plugin_id();
 
     let generator_2_create_response = {
         let display_name = "generator 2";
-        let artifact = get_example_generator()?;
+        let artifact = Bytes::from("fake");
         let metadata = PluginMetadata::new(
             tenant_id,
             display_name.to_string(),
@@ -64,15 +58,14 @@ async fn test_list_plugins() -> eyre::Result<()> {
                 metadata,
                 futures::stream::once(async move { artifact.clone() }),
             )
-            .timeout(std::time::Duration::from_secs(5))
-            .await??
+            .await?
     };
 
     let generator_2_id = generator_2_create_response.plugin_id();
 
     let analyzer_create_response = {
         let display_name = "analyzer";
-        let artifact = get_example_generator()?;
+        let artifact = Bytes::from("fake");
         let metadata = PluginMetadata::new(
             tenant_id,
             display_name.to_string(),
@@ -85,16 +78,14 @@ async fn test_list_plugins() -> eyre::Result<()> {
                 metadata,
                 futures::stream::once(async move { artifact.clone() }),
             )
-            .timeout(std::time::Duration::from_secs(5))
-            .await??
+            .await?
     };
 
     let analyzer_id = analyzer_create_response.plugin_id();
 
     let generator_plugins = client
         .list_plugins(ListPluginsRequest::new(tenant_id, PluginType::Generator))
-        .timeout(std::time::Duration::from_secs(5))
-        .await??
+        .await?
         .plugins();
 
     assert_eq!(generator_plugins.len(), 2);
@@ -103,12 +94,27 @@ async fn test_list_plugins() -> eyre::Result<()> {
 
     let analyzer_plugins = client
         .list_plugins(ListPluginsRequest::new(tenant_id, PluginType::Analyzer))
-        .timeout(std::time::Duration::from_secs(5))
-        .await??
+        .await?
         .plugins();
 
     assert_eq!(analyzer_plugins.len(), 1);
     assert_eq!(analyzer_plugins[0].plugin_id(), analyzer_id);
+
+    Ok(())
+}
+
+#[test_log::test(tokio::test)]
+async fn test_list_plugins_not_found() -> eyre::Result<()> {
+    let client_config = PluginRegistryClientConfig::parse();
+    let mut client = build_grpc_client(client_config).await?;
+
+    let tenant_id = uuid::Uuid::new_v4();
+
+    let generator_plugins = client
+        .list_plugins(ListPluginsRequest::new(tenant_id, PluginType::Generator))
+        .await?;
+
+    assert_eq!(generator_plugins.plugins().len(), 0);
 
     Ok(())
 }
