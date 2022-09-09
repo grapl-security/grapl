a0bc51d6765b07b35541faef91ba236b2383c5f2
Andrea <andrea@graplsecurity.com>
2022-09-09 13:11:07 -0600

    update yarnrc, update eslintrc, formatting

--- src/js/frontend/src/App.test.tsx
+++ src/js/frontend/src/App.test.tsx
@@ -3,9 +3,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders grapl welcome", () => {
-    render(<App />);
-    const welcomeElement = screen.getByText(
-        /A Graph Analytics Platform for Detection and Response/i
-    );
-    expect(welcomeElement).toBeInTheDocument();
+  render(<App />);
+  const welcomeElement = screen.getByText(/A Graph Analytics Platform for Detection and Response/i);
+  expect(welcomeElement).toBeInTheDocument();
 });
