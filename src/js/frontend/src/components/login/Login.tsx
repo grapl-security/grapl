e64864e7f67d9aab1cc1737ea6a75bb28e3cfad9
Andrea <andrea@graplsecurity.com>
2022-09-20 17:47:25 -0600

    add login form (not styled)

--- src/js/frontend/src/components/login/Login.tsx
+++ src/js/frontend/src/components/login/Login.tsx
@@ -1,37 +1,75 @@
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
+import * as Yup from "yup";
+import { Field, Form, Formik } from "formik";
+
 import { signInWithGoogleService } from "../../services/login/signInWithGoogleService";
+import { loginService } from "../../services/login/loginService";
 
 const Login = () => {
   const [state, setState] = React.useState({
     loginFailed: false,
   });
 
-  return(
+  const validationSchema = Yup.object().shape({
+    userName: Yup.string().required("Username Required"),
+    password: Yup.string().required("Password Required"),
+  });
+
+  return (
     <div>
       <div className="App">
-        <GoogleLogin
-          login_uri="/api/auth/signin_with_google"
-          onSuccess={async (credentialResponse) => {
-            if (credentialResponse.credential === undefined) {
-              setState({
-                ...state,
-                loginFailed: true,
-              });
+        <div>
+          <GoogleLogin
+            login_uri="/api/auth/signin_with_google"
+            onSuccess={async (credentialResponse) => {
+              if (credentialResponse.credential === undefined) {
+                setState({
+                  ...state,
+                  loginFailed: true,
+                });
 
-              return;
-            }
+                return;
+              }
 
-            const loginSuccess = await signInWithGoogleService(
-              credentialResponse.credential
+              const loginSuccess = await signInWithGoogleService(
+                credentialResponse.credential,
+              );
+
+              if (loginSuccess === true) {
+                window.history.replaceState(
+                  "#/login",
+                  "",
+                  "#/",
+                );
+                window.location.reload();
+                console.log("Logged In");
+              } else {
+                setState({
+                  ...state,
+                  loginFailed: true,
+                });
+              }
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </div>
+      </div>
+
+      <div>
+        <Formik
+          initialValues={{ userName: "", password: "" }}
+          validationSchema={validationSchema}
+          onSubmit={async (values) => {
+            const loginSuccess = await loginService(
+              values.userName,
+              values.password,
             );
 
             if (loginSuccess === true) {
-              window.history.replaceState(
-                "#/login",
-                "",
-                "#/"
-              );
+              window.history.replaceState("#/login", "", "#/");
               window.location.reload();
               console.log("Logged In");
             } else {
@@ -41,13 +79,46 @@ const Login = () => {
               });
             }
           }}
-          onError={() => {
-            console.log("Login Failed");
-          }}
-        />
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <h3> User Login </h3>
+              <Field
+                name="userName"
+                type="text"
+                placeholder="Username"
+              />
+              {touched.userName && errors.userName && (
+                <div>
+                  {errors.userName}
+                </div>
+              )}
+              <Field
+                name="password"
+                type="password"
+                placeholder="Password"
+              />{" "}
+              <br />
+              {touched.password && errors.password && (
+                <div>
+                  {errors.password}
+                </div>
+              )}
+              <button type="submit">
+                SUBMIT
+              </button>
+              {state.loginFailed && (
+                <div>
+                  Unsuccessful Login
+                </div>
+              )}
+            </Form>
+          )}
+        </Formik>
+
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
---
+++ src/js/frontend/src/services/login/loginService.tsx
@@ -0,0 +1,20 @@
+import DEV_API_EDGES from "../constants";
+import { apiPostRequestWithBody } from "../fetch";
+
+export const loginService = async (username: string, password: string) => {
+  const loginBody = JSON.stringify({
+    username: username,
+    password: password,
+  });
+
+  try {
+    const loginData = await apiPostRequestWithBody(
+      `${DEV_API_EDGES.auth}/sign_in_with_password`,
+      loginBody
+    );
+    return loginData["success"] === true;
+  } catch (e) {
+    console.log("Login Error", e);
+    return false;
+  }
+};
