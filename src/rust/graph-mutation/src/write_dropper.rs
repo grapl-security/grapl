2333e775de685ec083d6babdf2f6e210dcd782f5
Max Wittek <wimax@graplsecurity.com>
2022-09-20 11:34:43 -0400

    make write dropper dumber

--- src/rust/graph-mutation/src/write_dropper.rs
+++ src/rust/graph-mutation/src/write_dropper.rs
@@ -6,6 +6,7 @@ use blake2::{
     Digest,
 };
 use dashmap::mapref::entry::Entry;
+use moka::future::Cache;
 use rust_proto::graplinc::grapl::common::v1beta1::types::{
     EdgeName,
     NodeType,
@@ -14,18 +15,6 @@ use rust_proto::graplinc::grapl::common::v1beta1::types::{
 };
 
 type Blake2b16 = Blake2b<U16>;
-
-macro_rules! handle_full {
-    ($self:ident, $cache:ident) => {{
-        if $self.$cache.len() >= $self.max_size {
-            // Do not 'inline' the `if let Some` or you may cause a deadlock
-            let entry = $self.$cache.iter().next().map(|i| i.key().clone());
-            if let Some(entry) = entry {
-                $self.$cache.remove(&entry);
-            }
-        }
-    }};
-}
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct NodeTypeKey {
@@ -55,35 +44,36 @@ fn edge_key(
     hasher.finalize().into()
 }
 
+#[derive(Clone, Debug)]
 pub struct WriteDropper {
-    max_i64: dashmap::DashMap<PropertyKey, i64>,
-    min_i64: dashmap::DashMap<PropertyKey, i64>,
-    imm_i64: dashmap::DashSet<PropertyKey>,
-    max_u64: dashmap::DashMap<PropertyKey, u64>,
-    min_u64: dashmap::DashMap<PropertyKey, u64>,
-    imm_u64: dashmap::DashSet<PropertyKey>,
-    imm_string: dashmap::DashSet<PropertyKey>,
-    node_type: dashmap::DashSet<NodeTypeKey>,
-    edges: dashmap::DashSet<[u8; 16], hash_hasher::HashBuildHasher>,
-    max_size: usize,
+    max_i64: Cache<PropertyKey, i64>,
+    min_i64: Cache<PropertyKey, i64>,
+    imm_i64: Cache<PropertyKey, ()>,
+    max_u64: Cache<PropertyKey, u64>,
+    min_u64: Cache<PropertyKey, u64>,
+    imm_u64: Cache<PropertyKey, ()>,
+    imm_string: Cache<PropertyKey, ()>,
+    node_type: Cache<NodeTypeKey, ()>,
+    edges: Cache<[u8; 16], ()>,
 }
 
 impl WriteDropper {
-    pub fn new(max_size: usize) -> Self {
+
+    pub fn new(max_size: u64) -> Self {
         Self {
-            max_i64: Default::default(),
-            min_i64: Default::default(),
-            imm_i64: Default::default(),
-            max_u64: Default::default(),
-            min_u64: Default::default(),
-            imm_u64: Default::default(),
-            imm_string: Default::default(),
-            node_type: Default::default(),
-            edges: Default::default(),
-            max_size,
+            max_i64: Cache::new(max_size),
+            min_i64: Cache::new(max_size),
+            imm_i64: Cache::new(max_size),
+            max_u64: Cache::new(max_size),
+            min_u64: Cache::new(max_size),
+            imm_u64: Cache::new(max_size),
+            imm_string: Cache::new(max_size),
+            node_type: Cache::new(max_size),
+            edges: Cache::new(max_size),
         }
     }
 
+    #[tracing::instrument(skip(self, callback), err)]
     pub async fn check_max_i64<T, E, Fut>(
         &self,
         tenant_id: uuid::Uuid,
@@ -94,28 +84,34 @@ impl WriteDropper {
     ) -> Result<(), E>
     where
         Fut: Future<Output = Result<T, E>>,
+        E: std::error::Error
     {
-        match self.max_i64.entry(PropertyKey {
-            tenant_id,
-            node_type,
-            property_name,
-        }) {
-            Entry::Vacant(entry) => {
-                callback().await?;
-                handle_full!(self, max_i64);
-                entry.insert(value);
-            }
-            Entry::Occupied(mut entry) => {
-                if value > *entry.get() {
-                    callback().await?;
-                    handle_full!(self, max_i64);
-                    entry.insert(value);
-                }
-            }
-        }
+
+        tracing::debug!(message="Performing insert",);
+        callback().await?;
+        tracing::debug!(message="Insert performed");
+        // match self.max_i64.entry(PropertyKey {
+        //     tenant_id,
+        //     node_type,
+        //     property_name,
+        // }) {
+        //     Entry::Vacant(entry) => {
+        //         callback().await?;
+        //         handle_full!(self, max_i64);
+        //         entry.insert(value);
+        //     }
+        //     Entry::Occupied(mut entry) => {
+        //         if value > *entry.get() {
+        //             callback().await?;
+        //             handle_full!(self, max_i64);
+        //             entry.insert(value);
+        //         }
+        //     }
+        // }
         Ok(())
     }
 
+    #[tracing::instrument(skip(self, callback), err)]
     pub async fn check_min_i64<T, E, Fut>(
         &self,
         tenant_id: uuid::Uuid,
@@ -126,27 +122,33 @@ impl WriteDropper {
     ) -> Result<(), E>
     where
         Fut: Future<Output = Result<T, E>>,
+        E: std::error::Error
     {
-        match self.min_i64.entry(PropertyKey {
-            tenant_id,
-            node_type,
-            property_name,
-        }) {
-            Entry::Vacant(entry) => {
-                callback().await?;
-                handle_full!(self, min_i64);
-                entry.insert(value);
-            }
-            Entry::Occupied(mut entry) => {
-                if value < *entry.get() {
-                    callback().await?;
-                    handle_full!(self, min_i64);
-                    entry.insert(value);
-                }
-            }
-        }
+
+        tracing::debug!(message="Performing insert",);
+        callback().await?;
+        tracing::debug!(message="Insert performed");
+        // match self.min_i64.entry(PropertyKey {
+        //     tenant_id,
+        //     node_type,
+        //     property_name,
+        // }) {
+        //     Entry::Vacant(entry) => {
+        //         callback().await?;
+        //         handle_full!(self, min_i64);
+        //         entry.insert(value);
+        //     }
+        //     Entry::Occupied(mut entry) => {
+        //         if value < *entry.get() {
+        //             callback().await?;
+        //             handle_full!(self, min_i64);
+        //             entry.insert(value);
+        //         }
+        //     }
+        // }
         Ok(())
     }
+    #[tracing::instrument(skip(self, callback), err)]
     pub async fn check_imm_i64<T, E, Fut>(
         &self,
         tenant_id: uuid::Uuid,
@@ -156,20 +158,25 @@ impl WriteDropper {
     ) -> Result<(), E>
     where
         Fut: Future<Output = Result<T, E>>,
+        E: std::error::Error
     {
-        let key = PropertyKey {
-            tenant_id,
-            node_type,
-            property_name,
-        };
-        if !self.imm_i64.contains(&key) {
-            callback().await?;
-            handle_full!(self, imm_i64);
-            self.imm_i64.insert(key);
-        }
+        tracing::debug!(message="Performing insert",);
+        callback().await?;
+        tracing::debug!(message="Insert performed");
+        // let key = PropertyKey {
+        //     tenant_id,
+        //     node_type,
+        //     property_name,
+        // };
+        // if !self.imm_i64.contains(&key) {
+        //     callback().await?;
+        //     handle_full!(self, imm_i64);
+        //     self.imm_i64.insert(key);
+        // }
         Ok(())
     }
 
+    #[tracing::instrument(skip(self, callback), err)]
     pub async fn check_max_u64<T, E, Fut>(
         &self,
         tenant_id: uuid::Uuid,
@@ -180,30 +187,34 @@ impl WriteDropper {
     ) -> Result<(), E>
     where
         Fut: Future<Output = Result<T, E>>,
+        E: std::error::Error
     {
-        match self.max_u64.entry(PropertyKey {
-            tenant_id,
-            node_type,
-            property_name,
-        }) {
-            Entry::Vacant(entry) => {
-                callback().await?;
-                handle_full!(self, max_u64);
-                entry.insert(value);
-                tracing::info!("Vacant");
-            }
-            Entry::Occupied(mut entry) => {
-                if value > *entry.get() {
-                    callback().await?;
-                    handle_full!(self, max_u64);
-                    entry.insert(value);
-                    tracing::info!("occupado");
-                }
-            }
-        }
+
+        tracing::debug!(message="Performing insert",);
+        callback().await?;
+        tracing::debug!(message="Insert performed");
+        // match self.max_u64.entry(PropertyKey {
+        //     tenant_id,
+        //     node_type,
+        //     property_name,
+        // }) {
+        //     Entry::Vacant(entry) => {
+        //         callback().await?;
+        //         handle_full!(self, max_u64);
+        //         entry.insert(value);
+        //     }
+        //     Entry::Occupied(mut entry) => {
+        //         if value > *entry.get() {
+        //             callback().await?;
+        //             handle_full!(self, max_u64);
+        //             entry.insert(value);
+        //         }
+        //     }
+        // }
         Ok(())
     }
 
+    #[tracing::instrument(skip(self, callback), err)]
     pub async fn check_min_u64<T, E, Fut>(
         &self,
         tenant_id: uuid::Uuid,
@@ -214,27 +225,33 @@ impl WriteDropper {
     ) -> Result<(), E>
     where
         Fut: Future<Output = Result<T, E>>,
+        E: std::error::Error
     {
-        match self.min_u64.entry(PropertyKey {
-            tenant_id,
-            node_type,
-            property_name,
-        }) {
-            Entry::Vacant(entry) => {
-                callback().await?;
-                handle_full!(self, min_u64);
-                entry.insert(value);
-            }
-            Entry::Occupied(mut entry) => {
-                if value < *entry.get() {
-                    callback().await?;
-                    handle_full!(self, min_u64);
-                    entry.insert(value);
-                }
-            }
-        }
+
+        tracing::debug!(message="Performing insert",);
+        callback().await?;
+        tracing::debug!(message="Insert performed");
+        // match self.min_u64.entry(PropertyKey {
+        //     tenant_id,
+        //     node_type,
+        //     property_name,
+        // }) {
+        //     Entry::Vacant(entry) => {
+        //         callback().await?;
+        //         handle_full!(self, min_u64);
+        //         entry.insert(value);
+        //     }
+        //     Entry::Occupied(mut entry) => {
+        //         if value < *entry.get() {
+        //             callback().await?;
+        //             handle_full!(self, min_u64);
+        //             entry.insert(value);
+        //         }
+        //     }
+        // }
         Ok(())
     }
+    #[tracing::instrument(skip(self, callback), err)]
     pub async fn check_imm_u64<T, E, Fut>(
         &self,
         tenant_id: uuid::Uuid,
@@ -244,20 +261,26 @@ impl WriteDropper {
     ) -> Result<(), E>
     where
         Fut: Future<Output = Result<T, E>>,
+        E: std::error::Error
     {
-        let key = PropertyKey {
-            tenant_id,
-            node_type,
-            property_name,
-        };
-        if !self.imm_u64.contains(&key) {
-            callback().await?;
-            handle_full!(self, imm_u64);
-            self.imm_u64.insert(key);
-        }
+
+        tracing::debug!(message="Performing insert",);
+        callback().await?;
+        tracing::debug!(message="Insert performed");
+        // let key = PropertyKey {
+        //     tenant_id,
+        //     node_type,
+        //     property_name,
+        // };
+        // if !self.imm_u64.contains(&key) {
+        //     callback().await?;
+        //     handle_full!(self, imm_u64);
+        //     self.imm_u64.insert(key);
+        // }
         Ok(())
     }
 
+    #[tracing::instrument(skip(self, callback), err)]
     pub async fn check_imm_string<T, E, Fut>(
         &self,
         tenant_id: uuid::Uuid,
@@ -267,20 +290,26 @@ impl WriteDropper {
     ) -> Result<(), E>
     where
         Fut: Future<Output = Result<T, E>>,
+        E: std::error::Error
     {
-        let key = PropertyKey {
-            tenant_id,
-            node_type,
-            property_name,
-        };
-        if !self.imm_string.contains(&key) {
-            callback().await?;
-            handle_full!(self, imm_string);
-            self.imm_string.insert(key);
-        }
+
+        tracing::debug!(message="Performing insert",);
+        callback().await?;
+        tracing::debug!(message="Insert performed");
+        // let key = PropertyKey {
+        //     tenant_id,
+        //     node_type,
+        //     property_name,
+        // };
+        // if !self.imm_string.contains(&key) {
+        //     callback().await?;
+        //     handle_full!(self, imm_string);
+        //     self.imm_string.insert(key);
+        // }
         Ok(())
     }
 
+    #[tracing::instrument(skip(self, callback), err)]
     pub async fn check_node_type<T, E, Fut>(
         &self,
         tenant_id: uuid::Uuid,
@@ -289,16 +318,22 @@ impl WriteDropper {
     ) -> Result<(), E>
     where
         Fut: Future<Output = Result<T, E>>,
+        E: std::error::Error
     {
-        let key = NodeTypeKey { tenant_id, uid };
-        if !self.node_type.contains(&key) {
-            callback().await?;
-            handle_full!(self, node_type);
-            self.node_type.insert(key);
-        }
+
+        tracing::debug!(message="Performing insert",);
+        callback().await?;
+        tracing::debug!(message="Insert performed");
+        // let key = NodeTypeKey { tenant_id, uid };
+        // if !self.node_type.contains(&key) {
+        //     callback().await?;
+        //     handle_full!(self, node_type);
+        //     self.node_type.insert(key);
+        // }
         Ok(())
     }
 
+    #[tracing::instrument(skip(self, callback), err)]
     pub async fn check_edges<T, E, Fut>(
         &self,
         tenant_id: uuid::Uuid,
@@ -310,19 +345,24 @@ impl WriteDropper {
     ) -> Result<(), E>
     where
         Fut: Future<Output = Result<T, E>>,
+        E: std::error::Error
     {
-        let fkey = edge_key(tenant_id, source_uid, dest_uid, &f_edge_name);
 
-        // We always insert both the forward and reverse edges in a batch insert
-        if !self.edges.contains(&fkey) {
-            handle_full!(self, edges);
-            let rkey = edge_key(tenant_id, dest_uid, source_uid, &r_edge_name);
-
-            callback(f_edge_name, r_edge_name).await?;
-
-            self.edges.insert(fkey);
-            self.edges.insert(rkey);
-        }
+        tracing::debug!(message="Performing insert",);
+        callback(f_edge_name, r_edge_name).await?;
+        tracing::debug!(message="Insert performed");
+        // let fkey = edge_key(tenant_id, source_uid, dest_uid, &f_edge_name);
+        //
+        // // We always insert both the forward and reverse edges in a batch insert
+        // if !self.edges.contains(&fkey) {
+        //     handle_full!(self, edges);
+        //     let rkey = edge_key(tenant_id, dest_uid, source_uid, &r_edge_name);
+        //
+        //     callback(f_edge_name, r_edge_name).await?;
+        //
+        //     self.edges.insert(fkey);
+        //     self.edges.insert(rkey);
+        // }
         Ok(())
     }
 }
