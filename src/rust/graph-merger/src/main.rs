06b4810959ffa649bdff75e8536bda40f59ab5e0
jgrillo <jgrillo@graplsecurity.com>
2022-09-23 14:35:25 -0400

    make graph-merger emit a stream of Update protobufs

--- src/rust/graph-merger/src/main.rs
+++ src/rust/graph-merger/src/main.rs
@@ -10,16 +10,12 @@ use kafka::{
         ProducerConfig,
     },
     StreamProcessor,
-    StreamProcessorError,
 };
 use rust_proto::{
-    graplinc::grapl::{
-        api::{
-            graph::v1beta1::IdentifiedGraph,
-            graph_mutation::v1beta1::client::GraphMutationClient,
-            plugin_sdk::analyzers::v1beta1::messages::Update,
-        },
-        pipeline::v1beta1::Envelope,
+    graplinc::grapl::api::{
+        graph::v1beta1::IdentifiedGraph,
+        graph_mutation::v1beta1::client::GraphMutationClient,
+        plugin_sdk::analyzers::v1beta1::messages::Update,
     },
     protocol::service_client::ConnectWithConfig,
 };
@@ -54,7 +50,7 @@ async fn main() -> eyre::Result<()> {
     handler(graph_merger, consumer_config, producer_config).await
 }
 
-#[tracing::instrument(skip(graph_merger))]
+#[tracing::instrument(skip(graph_merger, consumer_config, producer_config))]
 async fn handler(
     graph_merger: GraphMerger,
     consumer_config: ConsumerConfig,
@@ -75,61 +71,12 @@ async fn handler(
 
     tracing::info!(message = "kafka stream processor configured successfully",);
 
-    let stream = stream_processor.stream::<_, _, StreamProcessorError>(move |event| {
-        let mut graph_merger = graph_merger.clone();
-        async move {
-            let (span, envelope) = event?;
-            let _guard = span.enter();
-            let tenant_id = envelope.tenant_id();
-            let trace_id = envelope.trace_id();
-            let event_source_id = envelope.event_source_id();
-
-            tracing::debug!("received kafka message");
-
-            let envelopes_res = match graph_merger
-                .handle_event(tenant_id, envelope.inner_message())
-                .await
-            {
-                Ok(updates) => {
-                    // TODO: remove Updates and handle_event should just return Vec<Update>
-                    Ok(updates.updates.into_iter().map(|update| {
-                        Envelope::new(
-                            tenant_id,
-                            trace_id,
-                            event_source_id,
-                            update,
-                        )
-                    }).collect::<Vec<_>>())
-                },
-                Err(e) => match e {
-                    GraphMergerError::Unexpected(ref reason) => {
-                        tracing::error!(
-                            message = "unexpected error",
-                            reason = %reason,
-                            error = %e,
-                        );
-                        // TODO: write message to failed topic here
-                        Err(StreamProcessorError::from(e))
-                    }
-                    _ => {
-                        tracing::error!(
-                            mesage = "unexpected error",
-                            error = %e,
-                        );
-                        // TODO: write message to failed topic here
-                        Err(StreamProcessorError::from(e))
-                    }
-                },
-            };
-            envelopes_res
-        }
-        .into_stream() 
-        .flat_map(|envelope_res| { 
-            // !!!!!!!
-            // obviously doesn't work; just getting super lost in
-            // between Vecs and Results in the stream API.
-            envelope_res.into_stream()
-        })
+    let stream = stream_processor.stream::<_, _, GraphMergerError>(move |event| {
+        graph_merger
+            .clone()
+            .handle_event(event)
+            .into_stream()
+            .flat_map(|results| futures::stream::iter(results))
     });
 
     stream
