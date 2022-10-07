1a3b90cb9bf6661c8dcbc3cccae4472609b6d9d1
Max Wittek <wimax@graplsecurity.com>
2022-10-07 10:07:43 -0700

    Added python grpc health checks - so ANALYZERS RUN! WE DID IT

--- src/python/python-proto/python_proto/api/plugin_sdk/analyzers/v1beta1/server.py
+++ src/python/python-proto/python_proto/api/plugin_sdk/analyzers/v1beta1/server.py
@@ -5,11 +5,11 @@ we are ignoring *the entire file*.
 https://github.com/nipunn1313/mypy-protobuf/pull/217
 """
 
-import os
 import logging
+import os
+import sys
 from concurrent import futures
 from dataclasses import dataclass, field
-import sys
 from typing import Any, Awaitable, Protocol
 
 import grpc
@@ -18,11 +18,13 @@ from graplinc.grapl.api.plugin_sdk.analyzers.v1beta1.analyzers_pb2_grpc import (
     AnalyzerServiceServicer,
     add_AnalyzerServiceServicer_to_server,
 )
+from grpc_health.v1 import health, health_pb2_grpc
 from python_proto.api.plugin_sdk.analyzers.v1beta1 import messages as native
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(os.environ["ANALYZER_LOG_LEVEL"])
 LOGGER.addHandler(logging.StreamHandler(stream=sys.stdout))
+
 
 class AnalyzerService(Protocol):
     async def run_analyzer(
@@ -44,10 +46,12 @@ class AnalyzerServiceWrapper(AnalyzerServiceServicer):
     ) -> proto.RunAnalyzerResponse:
         native_request = native.RunAnalyzerRequest.from_proto(proto_request)
         LOGGER.info("Running analyzer")
+        LOGGER.debug(f"Analyzer request: {native_request}")
         native_response = await self.analyzer_service_impl.run_analyzer(
             native_request, context
         )
-        LOGGER.debug("Completed running analyzer", request=str(native_request), response=str(native_response))
+        LOGGER.info("Analyzer run complete")
+        LOGGER.debug(f"Analyzer response: {native_response}")
         return native_response.into_proto()
 
     async def serve(self) -> None:
@@ -56,6 +60,7 @@ class AnalyzerServiceWrapper(AnalyzerServiceServicer):
             self,
             server,
         )
+        health_pb2_grpc.add_HealthServicer_to_server(health.HealthServicer(), server)
         server.add_insecure_port(self.bind_address)
         # This shutdown stuff is suggested in the grpc example here:
         # https://github.com/grpc/grpc/blob/master/examples/python/helloworld/async_greeter_server_with_graceful_shutdown.py
@@ -66,14 +71,12 @@ class AnalyzerServiceWrapper(AnalyzerServiceServicer):
         self._cleanup_coroutines.append(server_graceful_shutdown())
 
         # Uncomment to experiment with grpc reflection.
-        """
         from grpc_reflection.v1alpha import reflection
         SERVICE_NAMES = (
             proto.DESCRIPTOR.services_by_name['AnalyzerService'].full_name,
             reflection.SERVICE_NAME,
         )
         reflection.enable_server_reflection(SERVICE_NAMES, server)
-        """
 
         LOGGER.info("Starting analyzer server")
         await server.start()
