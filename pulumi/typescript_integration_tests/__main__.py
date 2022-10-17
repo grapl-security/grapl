891dfda57ed629e940d8644f723227579f4400d6
Andrea <andrea@graplsecurity.com>
2022-10-16 18:47:36 -0600

    remove GraplStack implementation and use fommon one instead for consistency with python integration tests and rust integration tests

--- pulumi/typescript_integration_tests/__main__.py
+++ pulumi/typescript_integration_tests/__main__.py
@@ -5,7 +5,7 @@ sys.path.insert(0, "..")
 from typing import Mapping, cast
 
 import pulumi_aws as aws
-from infra import config
+from infra import config, log_levels
 from infra.artifacts import ArtifactGetter
 from infra.autotag import register_auto_tags
 from infra.config import repository_path
@@ -13,12 +13,13 @@ from infra.docker_images import DockerImageId, DockerImageIdBuilder
 from infra.hashicorp_provider import get_nomad_provider_address
 from infra.kafka import Credential, Kafka
 from infra.nomad_job import NomadJob, NomadVars
+from infra.grapl_stack import GraplStack
 
 import pulumi
 
 
 def _typescript_integration_container_images(
-    artifacts: ArtifactGetter,
+        artifacts: ArtifactGetter,
 ) -> Mapping[str, DockerImageId]:
     """
     Build a map of {task name -> docker image identifier}.
@@ -82,25 +83,76 @@ def main() -> None:
     )
 
 
-class GraplStack:
-    def __init__(self, stack_name: str) -> None:
-        self.upstream_stack_name = (
-            "local-grapl" if config.LOCAL_GRAPL else f"grapl/grapl/{stack_name}"
-        )
-        ref = pulumi.StackReference(self.upstream_stack_name)
+# class GraplStack:
+#     def __init__(self, stack_name: str) -> None:
+#         self.upstream_stack_name = (
+#             "local-grapl" if config.LOCAL_GRAPL else f"grapl/grapl/{stack_name}"
+#         )
+#         ref = pulumi.StackReference(self.upstream_stack_name)
+#
+#         def require_str(key: str) -> str:
+#             return cast(str, ref.require_output(key))
+#
+#         self.aws_env_vars_for_local = require_str("aws-env-vars-for-local")
+#         self.test_user_name = require_str("test-user-name")
+#
+#         # self.plugin_work_queue_db = cast(
+#         #     NomadServicePostgresDbArgs, ref.require_output("plugin-work-queue-db")
+#         # )
+#
+#         self.test_user_password_secret_id = require_str("test-user-password-secret-id")
 
-        def require_str(key: str) -> str:
-            return cast(str, ref.require_output(key))
+def main() -> None:
+    ##### Preamble
+    stack_name = config.STACK_NAME
 
-        self.aws_env_vars_for_local = require_str("aws-env-vars-for-local")
-        self.test_user_name = require_str("test-user-name")
+    pulumi_config = pulumi.Config()
+    artifacts = ArtifactGetter.from_config(pulumi_config)
 
-        # self.plugin_work_queue_db = cast(
-        #     NomadServicePostgresDbArgs, ref.require_output("plugin-work-queue-db")
-        # )
+    # These tags will be added to all provisioned infrastructure
+    # objects.
+    register_auto_tags(
+        {"pulumi:project": pulumi.get_project(), "pulumi:stack": stack_name}
+    )
 
-        self.test_user_password_secret_id = require_str("test-user-password-secret-id")
+    nomad_provider: pulumi.ProviderResource | None = None
+    if not config.LOCAL_GRAPL:
+        nomad_server_stack = pulumi.StackReference(f"grapl/nomad/{stack_name}")
+        nomad_provider = get_nomad_provider_address(nomad_server_stack)
 
+    ##### Business Logic
+    grapl_stack = GraplStack(stack_name)
+
+    kafka = Kafka(
+        "kafka",
+        confluent_environment_name=pulumi_config.require("confluent-environment-name"),
+        create_local_topics=False,
+    )
+
+    kafka_credentials = kafka.service_credentials("integration-tests").apply(
+        Credential.to_nomad_service_creds
+    )
+
+    typescript_integration_tests_job_vars: NomadVars = {
+        "graph_db": grapl_stack.graph_db,
+        "aws_env_vars_for_local": grapl_stack.aws_env_vars_for_local,
+        "aws_region": aws.get_region().name,
+        "container_images": _typescript_integration_container_images(artifacts),
+        "kafka_bootstrap_servers": kafka.bootstrap_servers(),
+        "kafka_consumer_group": kafka.consumer_group("integration-tests"),
+        "kafka_credentials": kafka_credentials,
+        "typescript_log": log_levels.TYPESCRIPT_LOG_LEVELS,
+        # "plugin_work_queue_db": grapl_stack.plugin_work_queue_db,
+        "test_user_name": grapl_stack.test_user_name,
+        "test_user_password_secret_id": grapl_stack.test_user_password_secret_id,
+    }
+
+    NomadJob(
+        "typescript-integration-tests",
+        jobspec=repository_path("nomad/typescript-integration-tests.nomad"),
+        vars=typescript_integration_tests_job_vars,
+        opts=pulumi.ResourceOptions(provider=nomad_provider),
+    )
 
 if __name__ == "__main__":
     main()
