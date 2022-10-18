c611380d9c1e977a137914060f5264ec21cdf72e
Thomas Wunderlich <thomas@graplsecurity.com>
2022-10-18 14:05:15 -0400

    Fix typo in docs

--- src/rust/plugin-registry/src/server/s3_uri.rs
+++ src/rust/plugin-registry/src/server/s3_uri.rs
@@ -2,7 +2,7 @@ use grapl_config::env_helpers::ENV_ENDPOINT;
 
 /// This gets a s3 URI to be used by Nomad's artifact stanza (in particular the grapl-plugin).
 /// The underlying library, Hashicorp's [go-getter](https://github.com/hashicorp/go-getter) lib,
-/// doesn't work with the `http://` schema prefix, but does work with an explicit s3 prefix `s://`
+/// doesn't work with the `http://` schema prefix, but does work with an explicit s3 prefix `s3://`
 /// or without a prefix. We've opted for the s3 prefix to be explicit.
 pub fn get_s3_uri(bucket: &str, key: &str) -> String {
     let local_endpoint = std::env::var(ENV_ENDPOINT).ok();
