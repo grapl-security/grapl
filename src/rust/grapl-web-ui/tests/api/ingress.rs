8547451efe60a80f6c3bae9ad1c693ec8719d6b0
inickles-grapl <inickles@graplsecurity.com>
2022-10-18 14:30:06 -0700

    Have web-ui ingress tests use a junk, nearly empty generator.

--- src/rust/grapl-web-ui/tests/api/ingress.rs
+++ src/rust/grapl-web-ui/tests/api/ingress.rs
@@ -1,18 +1,19 @@
 #![cfg(feature = "integration_tests")]
 
 use eyre::ContextCompat;
+use grapl_web_ui::routes::api::plugin::create::CreateResponse;
 
 use crate::test_app::TestApp;
 
 #[actix_web::test]
-async fn publish_log() -> eyre::Result<()> {
+async fn publish_log1() -> eyre::Result<()> {
     let app = TestApp::init().await?;
 
     app.login_with_test_user().await?;
 
     let plugin_name = "Test Plugin";
 
-    let create_response = crate::plugin::create_plugin(&app, plugin_name).await?;
+    let create_response = create_plugin(&app, plugin_name).await?;
 
     //TODO: this shouldn't be necessary, but we're seeing 500 errors without it.
     // I'll file a task to look into it for now so we can unblock frontend work.
@@ -38,4 +39,218 @@ async fn publish_log() -> eyre::Result<()> {
     );
 
     Ok(())
+}
+
+#[actix_web::test]
+async fn publish_log2() -> eyre::Result<()> {
+    let app = TestApp::init().await?;
+
+    app.login_with_test_user().await?;
+
+    let plugin_name = "Test Plugin";
+
+    let create_response = create_plugin(&app, plugin_name).await?;
+
+    //TODO: this shouldn't be necessary, but we're seeing 500 errors without it.
+    // I'll file a task to look into it for now so we can unblock frontend work.
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    let plugin_metadata =
+        crate::plugin::get_plugin_metadata(&app, &create_response.plugin_id).await?;
+
+    let event_source_id = plugin_metadata
+        .event_source_id
+        .wrap_err("new plugin is missing event_source_id")?;
+
+    let response = app
+        .post(format!("api/ingress/publish/{event_source_id}").as_str())
+        .body("junk data")
+        .send()
+        .await?;
+
+    eyre::ensure!(
+        response.status() == actix_web::http::StatusCode::OK,
+        "unexpected response: {:?}",
+        &response
+    );
+
+    Ok(())
+}
+
+#[actix_web::test]
+async fn publish_log3() -> eyre::Result<()> {
+    let app = TestApp::init().await?;
+
+    app.login_with_test_user().await?;
+
+    let plugin_name = "Test Plugin";
+
+    let create_response = create_plugin(&app, plugin_name).await?;
+
+    //TODO: this shouldn't be necessary, but we're seeing 500 errors without it.
+    // I'll file a task to look into it for now so we can unblock frontend work.
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    let plugin_metadata =
+        crate::plugin::get_plugin_metadata(&app, &create_response.plugin_id).await?;
+
+    let event_source_id = plugin_metadata
+        .event_source_id
+        .wrap_err("new plugin is missing event_source_id")?;
+
+    let response = app
+        .post(format!("api/ingress/publish/{event_source_id}").as_str())
+        .body("junk data")
+        .send()
+        .await?;
+
+    eyre::ensure!(
+        response.status() == actix_web::http::StatusCode::OK,
+        "unexpected response: {:?}",
+        &response
+    );
+
+    Ok(())
+}
+
+#[actix_web::test]
+async fn publish_log4() -> eyre::Result<()> {
+    let app = TestApp::init().await?;
+
+    app.login_with_test_user().await?;
+
+    let plugin_name = "Test Plugin";
+
+    let create_response = create_plugin(&app, plugin_name).await?;
+
+    //TODO: this shouldn't be necessary, but we're seeing 500 errors without it.
+    // I'll file a task to look into it for now so we can unblock frontend work.
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    let plugin_metadata =
+        crate::plugin::get_plugin_metadata(&app, &create_response.plugin_id).await?;
+
+    let event_source_id = plugin_metadata
+        .event_source_id
+        .wrap_err("new plugin is missing event_source_id")?;
+
+    let response = app
+        .post(format!("api/ingress/publish/{event_source_id}").as_str())
+        .body("junk data")
+        .send()
+        .await?;
+
+    eyre::ensure!(
+        response.status() == actix_web::http::StatusCode::OK,
+        "unexpected response: {:?}",
+        &response
+    );
+
+    Ok(())
+}
+
+#[actix_web::test]
+async fn publish_log5() -> eyre::Result<()> {
+    let app = TestApp::init().await?;
+
+    app.login_with_test_user().await?;
+
+    let plugin_name = "Test Plugin";
+
+    let create_response = create_plugin(&app, plugin_name).await?;
+
+    //TODO: this shouldn't be necessary, but we're seeing 500 errors without it.
+    // I'll file a task to look into it for now so we can unblock frontend work.
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    let plugin_metadata =
+        crate::plugin::get_plugin_metadata(&app, &create_response.plugin_id).await?;
+
+    let event_source_id = plugin_metadata
+        .event_source_id
+        .wrap_err("new plugin is missing event_source_id")?;
+
+    let response = app
+        .post(format!("api/ingress/publish/{event_source_id}").as_str())
+        .body("junk data")
+        .send()
+        .await?;
+
+    eyre::ensure!(
+        response.status() == actix_web::http::StatusCode::OK,
+        "unexpected response: {:?}",
+        &response
+    );
+
+    Ok(())
+}
+
+#[actix_web::test]
+async fn publish_log6() -> eyre::Result<()> {
+    let app = TestApp::init().await?;
+
+    app.login_with_test_user().await?;
+
+    let plugin_name = "Test Plugin";
+
+    let create_response = create_plugin(&app, plugin_name).await?;
+
+    //TODO: this shouldn't be necessary, but we're seeing 500 errors without it.
+    // I'll file a task to look into it for now so we can unblock frontend work.
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    let plugin_metadata =
+        crate::plugin::get_plugin_metadata(&app, &create_response.plugin_id).await?;
+
+    let event_source_id = plugin_metadata
+        .event_source_id
+        .wrap_err("new plugin is missing event_source_id")?;
+
+    let response = app
+        .post(format!("api/ingress/publish/{event_source_id}").as_str())
+        .body("junk data")
+        .send()
+        .await?;
+
+    eyre::ensure!(
+        response.status() == actix_web::http::StatusCode::OK,
+        "unexpected response: {:?}",
+        &response
+    );
+
+    Ok(())
+}
+
+pub async fn create_plugin(app: &TestApp, plugin_name: &str) -> eyre::Result<CreateResponse> {
+    let create_metadata_body = serde_json::json!({
+            "plugin_name": plugin_name,
+            "plugin_type": "generator",
+            "event_source_id": uuid::Uuid::new_v4()
+    });
+
+    let generator_bytes = "junk".as_bytes();
+
+    let form = reqwest::multipart::Form::new()
+        .part(
+            "metadata",
+            reqwest::multipart::Part::text(create_metadata_body.to_string()),
+        )
+        .part(
+            "plugin_artifact",
+            reqwest::multipart::Part::bytes(generator_bytes.to_vec()),
+        );
+
+    let response = app.post("api/plugin/create").multipart(form).send().await?;
+
+    eyre::ensure!(
+        response.status() == actix_web::http::StatusCode::OK,
+        "unexpected response: {:?}",
+        &response
+    );
+
+    let response_body = response.json::<CreateResponse>().await?;
+
+    println!("create response body: {:?}", response_body);
+
+    Ok(response_body)
 }
