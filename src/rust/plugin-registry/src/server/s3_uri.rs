05d156a605f4ddc1b65d11f2993f410f454afecf
Thomas Wunderlich <thomas@graplsecurity.com>
2022-10-17 20:54:00 -0400

    Use URI instead of URL to clarify usage
    Added a comment explaining what this function is and what its used by
    to replace the comment that linked to a deleted slack comversation.
    I've also renamed everything from url to uri to be more explicit
    about what to expect.

--- src/rust/plugin-registry/src/server/s3_uri.rs
+++ src/rust/plugin-registry/src/server/s3_uri.rs
@@ -1,6 +1,9 @@
 use grapl_config::env_helpers::ENV_ENDPOINT;
 
-pub fn get_s3_url(bucket: &str, key: &str) -> String {
+/// This gets a s3 URI to be used by Nomad's artifact stanza (in particular the grapl-plugin).
+/// The underlying go-getter lib, doesn't work with the `http://` schema prefix, but does work with
+/// an explicit s3 prefix `s::` or without a prefix. We've opted for the s3 prefix to be explicit.
+pub fn get_s3_uri(bucket: &str, key: &str) -> String {
     let local_endpoint = std::env::var(ENV_ENDPOINT).ok();
     // If the above is specified, we're running locally against Localhost
     // Otherwise, it's against prod S3; so we can use virtual-hosted-style URLs
