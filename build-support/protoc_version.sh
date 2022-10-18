a5b1d94aee7d5b882057716796f8338eab0f139e
Max Wittek <wimax@graplsecurity.com>
2022-10-18 13:33:18 -0400

    Fix up some comments that talked about python :)

--- build-support/protoc_version.sh
+++ build-support/protoc_version.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-# Extract the version of Python that we are using from our
-# .python-version file.
+# Extract the version of protoc that we are using from our
+# .protoc-version file.
 #
 # This can be used as a source of truth any place that need access to
 # this information (e.g., build arguments for container images to
 # ensure base images are in-sync with our Rust version)
 #
 # It can safely be invoked from anywhere in the repository, as it
-# resolves the .python-version file from the repository root.
+# resolves the .protoc-version file from the repository root.
 set -euo pipefail
 
 repo_root="$(git rev-parse --show-toplevel 2> /dev/null)"
