bbe57d6c40ad74533021683ded2aad1280a23ac2
Max Wittek <wimax@graplsecurity.com>
2022-10-19 12:26:53 -0400

    yeah use grep and explain why not another solution

--- build-support/protoc_version.sh
+++ build-support/protoc_version.sh
@@ -17,7 +17,16 @@ readonly repo_root
 # the protoc version is relatively unique and easy to grep for,
 # we'll just use that.
 
+# (Further diatribe:)
+# The problem with yq/tomlq? I'd like to install tomlq on the Buildkite agent
+# packer image, but the only way to install it is pip. Nothing else on
+# the buildkite packer uses pip right now. This seems like a serious rabbit
+# hole to go down to do something "right" when I could just grep.
+
+# grab [protoc] and the 1 line after that.
+protoc_section=$(grep -A 1 "\[protoc\]" "${repo_root}/pants.toml")
+
 # -n = quiet
 # -r = extended regex (enables \1)
 # /\1/ = only return what's in capture group 1 - the contents of the parens
-sed -nr 's/^protoc-version = "(.*)"$/\1/p' "${repo_root}/pants.toml"
+echo "${protoc_section}" | sed -nr 's/^version = "(.*)"$/\1/p'
