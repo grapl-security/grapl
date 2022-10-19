fdeeb4d540739ea6dd79db401369aeddd872c724
Max Wittek <wimax@graplsecurity.com>
2022-10-19 11:53:50 -0400

    Read protoc-version from pants.toml instead of .protoc-version

--- build-support/protoc_version.sh
+++ build-support/protoc_version.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# Extract the version of protoc that we are using from our
-# .protoc-version file.
+# Extract the version of protoc that we are using from pants.toml.
 #
 # This can be used as a source of truth any place that need access to
 # this information (e.g., build arguments for container images to
@@ -12,4 +11,13 @@ set -euo pipefail
 
 repo_root="$(git rev-parse --show-toplevel 2> /dev/null)"
 readonly repo_root
-cat "${repo_root}/.protoc-version"
+
+# Ideally, we'd use something like https://github.com/kislyuk/yq to
+# introspect `pants.toml`, but given that the entry in pants.toml for
+# the protoc version is relatively unique and easy to grep for,
+# we'll just use that.
+
+# -n = quiet
+# -r = extended regex (enables \1)
+# /\1/ = only return what's in capture group 1 - the contents of the parens
+sed -nr 's/^protoc-version = "(.*)"$/\1/p' "${repo_root}/pants.toml"
